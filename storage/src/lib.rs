08ea09e2b8f1862742d1ef446bb3086d5b88ddca
musitdev <philippe.delrieu@free.fr>
2021-07-08 16:28:17 +0000

    add tests directory

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -3,12 +3,5 @@ mod error;
 mod storage_controller;
 mod storage_worker;
 
-/*#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_sled() {
-        let tree = sled::open("./tmp/test").unwrap();
-        print!("{:?}", tree);
-        tree.flush().unwrap();
-    }
-}*/
+#[cfg(test)]
+mod tests;
