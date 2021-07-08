1931850df2588903d15c560b7c5c09db3799d693
Gregory <Gregory@Gregorys-MacBook-Air.local>
2021-07-08 16:28:17 +0000

    add basic tests for protocol

--- communication/src/protocol/tests/tools.rs
+++ communication/src/protocol/tests/tools.rs
@@ -1,26 +1,67 @@
+use super::mock_network_controller::MockNetworkController;
 use crate::common::NodeId;
 use crate::protocol::ProtocolConfig;
-use crypto::signature::{PrivateKey, SignatureEngine};
-use models::SerializationContext;
-use time::UTime;
+use crypto::{
+    hash::Hash,
+    signature::{PrivateKey, PublicKey, SignatureEngine},
+};
+use models::{Block, BlockHeader, BlockHeaderContent, SerializationContext, Slot};
 
-// generate random node ID (public key) and private key
-pub fn generate_node_keys() -> (PrivateKey, NodeId) {
-    let signature_engine = SignatureEngine::new();
-    let private_key = SignatureEngine::generate_random_private_key();
-    let self_node_id = NodeId(signature_engine.derive_public_key(&private_key));
-    (private_key, self_node_id)
+pub struct NodeInfo {
+    pub private_key: PrivateKey,
+    pub id: NodeId,
 }
 
-pub fn create_node_ids(nb_nodes: usize) -> Vec<(PrivateKey, NodeId)> {
-    let signature_engine = SignatureEngine::new();
-    (0..nb_nodes)
-        .map(|_| {
-            let private_key = SignatureEngine::generate_random_private_key();
-            let self_node_id = NodeId(signature_engine.derive_public_key(&private_key));
-            (private_key, self_node_id)
-        })
-        .collect()
+pub fn create_node(signature_engine: &SignatureEngine) -> NodeInfo {
+    let private_key = SignatureEngine::generate_random_private_key();
+    let id = NodeId(signature_engine.derive_public_key(&private_key));
+    NodeInfo { private_key, id }
+}
+
+pub async fn create_and_connect_nodes(
+    num: usize,
+    signature_engine: &SignatureEngine,
+    network_controller: &mut MockNetworkController,
+) -> Vec<NodeInfo> {
+    let mut nodes = vec![];
+    for _ in 0..num {
+        // Create a node, and connect it with protocol.
+        let info = create_node(&signature_engine);
+        network_controller.new_connection(&info.id).await;
+        nodes.push(info);
+    }
+    nodes
+}
+
+/// Creates a block for use in protocol,
+/// without paying attention to consensus related things
+/// like slot, parents, and merkle root.
+pub fn create_block(
+    private_key: &PrivateKey,
+    public_key: &PublicKey,
+    serialization_context: &SerializationContext,
+    signature_engine: &mut SignatureEngine,
+) -> Block {
+    let example_hash = Hash::hash("default_val".as_bytes());
+
+    let (_, header) = BlockHeader::new_signed(
+        signature_engine,
+        private_key,
+        BlockHeaderContent {
+            creator: public_key.clone(),
+            slot: Slot::new(0, 0),
+            parents: Vec::new(),
+            out_ledger_hash: example_hash,
+            operation_merkle_root: example_hash,
+        },
+        &serialization_context,
+    )
+    .unwrap();
+
+    Block {
+        header,
+        operations: Vec::new(),
+    }
 }
 
 // create a ProtocolConfig with typical values
