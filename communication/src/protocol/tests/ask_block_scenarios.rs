ea63169cfed39982402b442389890417147e67bb
Aurelia <adolo@massa.network>
2021-07-16 15:39:07 +0200

    Updated test_someone_knows_it

--- communication/src/protocol/tests/ask_block_scenarios.rs
+++ communication/src/protocol/tests/ask_block_scenarios.rs
@@ -82,83 +82,74 @@ async fn test_without_a_priori() {
 async fn test_someone_knows_it() {
     // start
     let protocol_config = tools::create_protocol_config();
-    let (mut network_controller, network_command_sender, network_event_receiver) =
-        MockNetworkController::new();
+    protocol_test(
+        protocol_config,
+        async move |mut network_controller,
+                    mut protocol_event_receiver,
+                    mut protocol_command_sender,
+                    protocol_manager| {
+            let node_a = tools::create_and_connect_nodes(1, &mut network_controller)
+                .await
+                .pop()
+                .unwrap();
+            let _node_b = tools::create_and_connect_nodes(1, &mut network_controller)
+                .await
+                .pop()
+                .unwrap();
+            let node_c = tools::create_and_connect_nodes(1, &mut network_controller)
+                .await
+                .pop()
+                .unwrap();
 
-    // start protocol controller
-    let (
-        mut protocol_command_sender,
-        mut protocol_event_receiver,
-        protocol_pool_event_receiver,
-        protocol_manager,
-    ) = start_protocol_controller(
-        protocol_config.clone(),
-        5u64,
-        network_command_sender,
-        network_event_receiver,
+            // 2. Create a block coming from node 0.
+            let block = tools::create_block(&node_a.private_key, &node_a.id.0);
+            let hash_1 = block.header.compute_block_id().unwrap();
+            // end set up
+
+            // node c must know about block
+            network_controller
+                .send_header(node_c.id, block.header.clone())
+                .await;
+
+            match protocol_event_receiver.wait_event().await.unwrap() {
+                ProtocolEvent::ReceivedBlockHeader { .. } => {}
+                _ => panic!("unexpected protocol event"),
+            };
+
+            // send wishlist
+            protocol_command_sender
+                .send_wishlist_delta(vec![hash_1].into_iter().collect(), HashSet::new())
+                .await
+                .unwrap();
+
+            assert_hash_asked_to_node(hash_1, node_c.id, &mut network_controller).await;
+
+            // node C replied with the block
+            network_controller.send_block(node_c.id, block).await;
+
+            // 7. Make sure protocol did not send additional ask for block commands.
+            let ask_for_block_cmd_filter = |cmd| match cmd {
+                cmd @ NetworkCommand::AskForBlocks { .. } => Some(cmd),
+                _ => None,
+            };
+
+            let got_more_commands = network_controller
+                .wait_command(100.into(), ask_for_block_cmd_filter)
+                .await;
+            assert!(
+                got_more_commands.is_none(),
+                "unexpected command {:?}",
+                got_more_commands
+            );
+            (
+                network_controller,
+                protocol_event_receiver,
+                protocol_command_sender,
+                protocol_manager,
+            )
+        },
     )
-    .await
-    .expect("could not start protocol controller");
-
-    let node_a = tools::create_and_connect_nodes(1, &mut network_controller)
-        .await
-        .pop()
-        .unwrap();
-    let _node_b = tools::create_and_connect_nodes(1, &mut network_controller)
-        .await
-        .pop()
-        .unwrap();
-    let node_c = tools::create_and_connect_nodes(1, &mut network_controller)
-        .await
-        .pop()
-        .unwrap();
-
-    // 2. Create a block coming from node 0.
-    let block = tools::create_block(&node_a.private_key, &node_a.id.0);
-    let hash_1 = block.header.compute_block_id().unwrap();
-    // end set up
-
-    // node c must know about block
-    network_controller
-        .send_header(node_c.id, block.header.clone())
-        .await;
-
-    match protocol_event_receiver.wait_event().await.unwrap() {
-        ProtocolEvent::ReceivedBlockHeader { .. } => {}
-        _ => panic!("unexpected protocol event"),
-    };
-
-    // send wishlist
-    protocol_command_sender
-        .send_wishlist_delta(vec![hash_1].into_iter().collect(), HashSet::new())
-        .await
-        .unwrap();
-
-    assert_hash_asked_to_node(hash_1, node_c.id, &mut network_controller).await;
-
-    // node C replied with the block
-    network_controller.send_block(node_c.id, block).await;
-
-    // 7. Make sure protocol did not send additional ask for block commands.
-    let ask_for_block_cmd_filter = |cmd| match cmd {
-        cmd @ NetworkCommand::AskForBlocks { .. } => Some(cmd),
-        _ => None,
-    };
-
-    let got_more_commands = network_controller
-        .wait_command(100.into(), ask_for_block_cmd_filter)
-        .await;
-    assert!(
-        got_more_commands.is_none(),
-        "unexpected command {:?}",
-        got_more_commands
-    );
-
-    // Close everything
-    protocol_manager
-        .stop(protocol_event_receiver, protocol_pool_event_receiver)
-        .await
-        .expect("Failed to shutdown protocol.");
+    .await;
 }
 
 #[tokio::test]
