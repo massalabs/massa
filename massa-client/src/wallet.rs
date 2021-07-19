1d859ea79c54dffa3725a8a6102ccac7066482df
Damir Vodenicarevic <damipator@gmail.com>
2021-07-19 15:33:02 +0200

    clarify message

--- massa-client/src/wallet.rs
+++ massa-client/src/wallet.rs
@@ -201,7 +201,7 @@ impl<'a> std::fmt::Display for WalletInfo<'a> {
                 Some(balance) => {
                     write!(f, "State: \n{}", balance)?;
                 }
-                None => writeln!(f, "State: no balance available")?,
+                None => writeln!(f, "No balance info available. Is your node running ?")?,
             }
         }
         Ok(())
