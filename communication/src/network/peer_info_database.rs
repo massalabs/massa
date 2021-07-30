7221c10180e85e7971216433f0c035c30889f221
Damir Vodenicarevic <damipator@gmail.com>
2021-07-30 10:05:38 +0200

    debug peers

--- communication/src/network/peer_info_database.rs
+++ communication/src/network/peer_info_database.rs
@@ -648,8 +648,8 @@ impl PeerInfoDatabase {
             ));
         }
         if (peer.bootstrap
-            && self.active_out_nonbootstrap_connections
-                >= self.cfg.target_out_nonbootstrap_connections)
+            && self.active_bootstrap_connections
+                >= self.cfg.target_bootstrap_connections)
             || (!peer.bootstrap
                 && self.active_out_nonbootstrap_connections
                     >= self.cfg.target_out_nonbootstrap_connections)
