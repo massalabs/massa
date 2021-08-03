dee7ddbe660766d929a5361e2f4e2fc97de3b352
Aurelia <adolo@massa.network>
2021-08-03 10:53:06 +0200

    Refactor.

--- massa-client/src/main.rs
+++ massa-client/src/main.rs
@@ -673,21 +673,25 @@ fn cmd_next_draws(data: &mut ReplData, params: &[&str]) -> Result<(), ReplError>
     let url = format!(
         "http://{}/api/v1/next_draws?{}",
         data.node_ip,
-        serde_qs::to_string(&Addresses { addrs: addrs.clone() })?
+        serde_qs::to_string(&Addresses {
+            addrs: addrs.clone()
+        })?
     );
 
     if let Some(resp) = request_data(data, &url)? {
         let resp = resp.json::<data::NextDraws>()?;
-        let addr_map = resp
-            .content()
-            .iter()
-            .fold(addrs.iter().map(|addr|(addr, Vec::new())).collect::<HashMap<_,_>>(), |mut map, (addr, slot)| {
+        let addr_map = resp.content().iter().fold(
+            addrs
+                .iter()
+                .map(|addr| (addr, Vec::new()))
+                .collect::<HashMap<_, _>>(),
+            |mut map, (addr, slot)| {
                 let entry = map.entry(addr).or_insert_with(Vec::new);
                 entry.push(slot);
                 map
-            });
+            },
+        );
         for (addr, slots) in addr_map {
-            if !slots.is_empty(){
             println!("Next selected slots of address: {}:", addr);
             for slot in slots {
                 println!(
@@ -696,7 +700,10 @@ fn cmd_next_draws(data: &mut ReplData, params: &[&str]) -> Result<(), ReplError>
                     slot.period,
                     slot.thread,
                 );
-            }} else {println!("No known selected slots of address: {}", addr);}
+            }
+            if slots.is_empty() {
+                println!("No known selected slots of address: {}", addr);
+            }
             println!();
         }
     }
