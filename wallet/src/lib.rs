c57bd54fbdd395218ee61617bb566617a8672b6d
Yvan Sraka <yvan@sraka.xyz>
2021-09-08 11:40:54 +0200

    Sort `*/Cargo.toml` dependencies

---
+++ wallet/src/lib.rs
@@ -0,0 +1,8 @@
+// TODO:
+//   * `wallet_info`: Shows wallet info
+//   * `wallet_new_privkey`: Generates a new private key and adds it to the wallet. Returns the associated address.
+//   * `send_transaction`: sends a transaction from <from_address> to <to_address> (from_address needs to be unlocked in the wallet). Returns the OperationId. Parameters: <from_address> <to_address> <amount> <fee>
+//   * `wallet_add_privkey`: Adds a list of private keys to the wallet. Returns the associated addresses. Parameters: list of private keys separated by ,  (no space).
+//   * `buy_rolls`: buy roll count for <address> (address needs to be unlocked in the wallet). Returns the OperationId. Parameters: <address>  <roll count> <fee>
+//   * `sell_rolls`: sell roll count for <address> (address needs to be unlocked in the wallet). Returns the OperationId. Parameters: <address>  <roll count> <fee>
+//   * `cmd_testnet_rewards_program`: Returns rewards id. Parameter: <staking_address> <discord_ID>
