7e59580e89da3b9a265f1944b2d5a7e508c46347
Yvan Sraka <yvan@sraka.xyz>
2021-09-13 17:39:18 +0200

    Write a draft of a minimalist JSON-RPC client
    That print a welcoming "Hello, World!" :)

--- jsonrpc-client/src/main.rs
+++ jsonrpc-client/src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use jsonrpc_core_client::transports::http;
+use jsonrpc_core_client::{RpcChannel, RpcResult, TypedClient};
+
+const URL: &str = "http://127.0.0.1:33035";
+
+struct RpcClient(TypedClient);
+
+impl From<RpcChannel> for RpcClient {
+    fn from(channel: RpcChannel) -> Self {
+        RpcClient(channel.into())
+    }
+}
+
+impl RpcClient {
+    async fn hello_world(&self) -> RpcResult<String> {
+        self.0.call_method("HelloWorld", "String", ()).await
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let client = http::connect::<RpcClient>(URL).await.unwrap();
+    println!("{}", client.hello_world().await.unwrap());
+}
