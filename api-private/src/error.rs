2cd411c3028ad93e46db2f58db01ca343ca18af6
Aurelia <adolo@massa.network>
2021-09-22 09:30:57 +0200

    Added error file.

---
+++ api-private/src/error.rs
@@ -0,0 +1,34 @@
+use consensus::ConsensusError;
+use crypto::CryptoError;
+use models::ModelsError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PrivateApiError {
+    #[error("send  channel error: {0}")]
+    SendChannelError(String),
+    #[error("receive  channel error: {0}")]
+    ReceiveChannelError(String),
+    #[error("crypto error : {0}")]
+    CryptoError(#[from] CryptoError),
+    #[error("consensus error : {0}")]
+    ConsensusError(#[from] ConsensusError),
+    #[error("models error : {0}")]
+    ModelsError(#[from] ModelsError),
+    #[error("not found")]
+    NotFound,
+    #[error("inconsistency: {0}")]
+    InconsistencyError(String),
+    #[error("missing command sender {0}")]
+    MissingCommandSender(String),
+}
+
+impl From<PrivateApiError> for jsonrpc_core::Error {
+    fn from(err: PrivateApiError) -> Self {
+        jsonrpc_core::Error {
+            code: jsonrpc_core::ErrorCode::ServerError(500),
+            message: err.to_string(),
+            data: None,
+        }
+    }
+}
