9270ec94dc2d8a1790d5bbb46f23cd825721d481
Your Name <you@example.com>
2021-11-03 11:21:31 +0800

    add module, update cargo feature

---
+++ execution/src/config.rs
@@ -0,0 +1,5 @@
+use serde::{Deserialize, Serialize};
+
+/// Execution configuration
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct ExecutionConfig {}
---
+++ execution/src/lib.rs
@@ -0,0 +1,4 @@
+mod config;
+mod controller;
+mod error;
+mod worker;
