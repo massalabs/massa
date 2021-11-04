26c6cc17f4bc0e625d0c1921b4dedf9fcaf7e8c6
Yvan Sraka <yvan@sraka.xyz>
2021-11-04 16:44:37 +0100

    Test if the client could be run and stop gracefully

---
+++ massa-client/src/tests/tools.rs
@@ -0,0 +1,18 @@
+extern crate toml_edit;
+
+use std::fs;
+use time::UTime;
+use toml_edit::{value, Document};
+
+// TODO: move me in massa-node tests to fix #361
+fn _update_genesis_timestamp() {
+    let toml = fs::read_to_string("base_config/config.toml").expect("Unable to read file");
+    let mut doc = toml.parse::<Document>().unwrap();
+    doc["consensus"]["genesis_timestamp"] =
+        value(format!("{}", UTime::now(1000 * 60).unwrap().to_millis()));
+    doc["consensus"]["end_timestamp"] = value(format!(
+        "{}",
+        UTime::now(1000 * 60 * 60).unwrap().to_millis()
+    ));
+    fs::write("config/config.toml", doc.to_string()).expect("Unable to write file");
+}
