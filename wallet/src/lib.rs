c503260345b3a270a701c4e807f86f2e303fd5e2
Aurelia <adolo@massa.network>
2021-11-09 14:52:40 +0100

    Removed unused addressState.

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -129,32 +129,6 @@ impl Wallet {
     }
 }
 
-/// Contains the private keys created in the wallet.
-#[derive(Debug)]
-pub struct WalletInfo<'a> {
-    pub wallet: &'a Wallet,
-    pub balances: AddressHashMap<AddressState>,
-}
-
-impl<'a> std::fmt::Display for WalletInfo<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        writeln!(f, "WARNING: do not share your private keys")?;
-        for (addr, (public_key, key)) in &self.wallet.keys {
-            writeln!(f)?;
-            writeln!(f, "Private key: {}", key)?;
-            writeln!(f, "Public key: {}", public_key)?;
-            writeln!(f, "Address: {}", addr)?;
-            match self.balances.get(&addr) {
-                Some(balance) => {
-                    write!(f, "State: \n{}", balance)?;
-                }
-                None => writeln!(f, "No balance info available. Is your node running?")?,
-            }
-        }
-        Ok(())
-    }
-}
-
 impl std::fmt::Display for Wallet {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         writeln!(f)?;
@@ -181,34 +155,4 @@ pub struct ConsensusConfigData {
     pub operation_validity_periods: u64,
     pub periods_per_cycle: u64,
     pub roll_price: Amount,
-}
-
-impl<'a> std::fmt::Display for AddressState {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        writeln!(f, "\tfinal balance: {}", self.final_ledger_data.balance)?;
-        writeln!(
-            f,
-            "\tcandidate balance: {}",
-            self.candidate_ledger_data.balance
-        )?;
-        writeln!(f, "\tlocked balance: {}", self.locked_balance)?;
-        writeln!(f, "\tfinal rolls: {}", self.final_rolls)?;
-        writeln!(f, "\tcandidate rolls: {}", self.candidate_rolls)?;
-        if let Some(active) = self.active_rolls {
-            writeln!(f, "\tactive rolls: {}", active)?;
-        } else {
-            writeln!(f, "\tNo active roll")?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct AddressState {
-    pub final_rolls: u64,
-    pub active_rolls: Option<u64>,
-    pub candidate_rolls: u64,
-    pub locked_balance: Amount,
-    pub candidate_ledger_data: LedgerData,
-    pub final_ledger_data: LedgerData,
 }
