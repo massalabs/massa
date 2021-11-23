f4d701e3fae9d1b200d30743b1bdb21a498147e3
Adrien Zinger <zinger.ad@gmail.com>
2021-11-23 17:27:43 +0100

    Add an integration test for the client
    Use a config path set by the env
    Really run a node and try commands

--- massa-client/src/tests/scenarios.rs
+++ massa-client/src/tests/scenarios.rs
@@ -1,10 +1,30 @@
+use super::tools;
+use assert_cmd::Command;
 use serial_test::serial;
+use std::thread;
 
 #[tokio::test]
 #[serial]
 async fn test_if_exit_gracefully() {
-    use assert_cmd::Command;
-
     let mut cmd = Command::cargo_bin("massa-client").unwrap();
+    cmd.arg("exit").assert().success();
+}
+
+const CONFIG_PATH: &str = "../massa-node/base_config/testnet.toml";
+
+#[tokio::test]
+#[serial]
+async fn test_if_wallet_info() {
+    tools::update_genesis_timestamp(CONFIG_PATH);
+    let massa_node_thread_handle = thread::spawn(|| {
+        Command::cargo_bin("massa-node")
+            .unwrap()
+            .env("MASSA_CONFIG_PATH", CONFIG_PATH)
+            .unwrap();
+    });
+    let mut cmd = Command::cargo_bin("massa-client").unwrap();
+    thread::sleep(std::time::Duration::from_secs(5));
+    cmd.arg("node_stop").assert().success();
+    massa_node_thread_handle.join().unwrap();
     cmd.arg("exit").assert().success();
 }
--- massa-node/src/settings.rs
+++ massa-node/src/settings.rs
@@ -1,7 +1,9 @@
 // Copyright (c) 2021 MASSA LABS <info@massa.net>
 
+extern crate directories;
 use bootstrap::settings::BootstrapSettings;
 use consensus::ConsensusConfig;
+use directories::ProjectDirs;
 use models::api::APISettings;
 use models::Version;
 use network::NetworkConfig;
@@ -9,23 +11,16 @@ use pool::PoolSettings;
 use protocol_exports::ProtocolSettings;
 use serde::Deserialize;
 
-const BASE_CONFIG_PATH: &str = "base_config/config.toml";
-const OVERRIDE_CONFIG_PATH: &str = "config/config.toml";
-
 lazy_static::lazy_static! {
     pub static ref SETTINGS: Settings = {
         let mut settings = config::Config::default();
-        settings
-            .merge(config::File::with_name(BASE_CONFIG_PATH))
-            .unwrap();
-        if std::path::Path::new(OVERRIDE_CONFIG_PATH).is_file() {
-            settings
-                .merge(config::File::with_name(OVERRIDE_CONFIG_PATH))
-                .unwrap();
+        let config_path = std::env::var("MASSA_CONFIG_PATH").unwrap_or_else(|_| "base_config/config.toml".to_string());
+        settings.merge(config::File::with_name(&config_path)).unwrap_or_else(|_| panic!("failed to read {} config", config_path));
+        if let Some(proj_dirs) = ProjectDirs::from("com", "MassaLabs", "Massa") { // Portable user config loading
+            let user_config_path = proj_dirs.config_dir();
+            settings.merge(config::File::with_name(user_config_path.to_str().unwrap())).unwrap_or_else(|_| panic!("failed to read {} user config", config_path));
         }
-        settings
-            .merge(config::Environment::with_prefix("MASSA_CLIENT"))
-            .unwrap();
+        settings.merge(config::Environment::with_prefix("MASSA_NODE")).unwrap();
         settings.try_into().unwrap()
     };
 }
