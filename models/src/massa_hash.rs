0f661e59e93729833e32712743bcae94146a43e7
Damir Vodenicarevic <damipator@gmail.com>
2021-11-23 15:30:34 +0100

    improve SCE

--- models/src/massa_hash.rs
+++ models/src/massa_hash.rs
@@ -2,8 +2,12 @@ use signature::{PublicKey, Signature};
 
 use serde::{Deserialize, Serialize};
 
+use crate::hhasher::PreHashed;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PubkeySig {
     pub public_key: PublicKey,
     pub signature: Signature,
 }
+
+impl PreHashed for Hash {}
