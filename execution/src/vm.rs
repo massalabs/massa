1f1725ae4fe4cd5faa11033e5dbc7d5736ce608b
Your Name <you@example.com>
2021-11-26 16:00:12 +0800

    more exec

--- execution/src/vm.rs
+++ execution/src/vm.rs
@@ -1,16 +1,24 @@
 use models::address::AddressHashMap;
+use models::Address;
 use parking_lot::Mutex;
-use std::collections::VecDeque;
 use std::sync::Arc;
-use wasmer::{imports, Function, ImportObject, Module, Store};
+use wasmer::{imports, Function, ImportObject, Instance, Module, Store, WasmerEnv};
 
 /// Example API available to wasm code, aka "syscall".
-fn foo(n: i32) -> i32 {
+fn foo(env: &ExecutionContext, n: i32) -> i32 {
     n
 }
 
 /// API allowing a contract to call another.
-fn call_address(addr: ()) {}
+fn call_address(env: &ExecutionContext, addr: Address) {
+    if let Some(module) = env.ledger.lock().get(&addr) {}
+}
+
+#[derive(WasmerEnv, Clone)]
+/// Stateful context, providing an execution context to host functions("syscalls").
+pub struct ExecutionContext {
+    ledger: Arc<Mutex<AddressHashMap<Module>>>,
+}
 
 pub struct VM {
     imports: ImportObject,
@@ -22,7 +30,7 @@ impl VM {
     pub fn new(store: Arc<Store>, ledger: Arc<Mutex<AddressHashMap<Module>>>) -> VM {
         let imports = imports! {
             "env" => {
-                "foo" => Function::new_native(&store, foo)
+                "foo" => Function::new_native_with_env(&store, ExecutionContext {ledger: ledger.clone()}, foo),
             },
         };
         VM {
@@ -33,6 +41,19 @@ impl VM {
     }
 
     pub fn run(&self, to_run: &[Module]) {
-        for module in to_run {}
+        for module in to_run {
+            self.call_module(module)
+        }
+    }
+
+    fn call_module(&self, module: &Module) {
+        let instance = Instance::new(&module, &self.imports).unwrap();
+        let program = instance
+            .exports
+            .get_function("main")
+            .unwrap()
+            .native::<(), ()>()
+            .unwrap();
+        program.call();
     }
 }
