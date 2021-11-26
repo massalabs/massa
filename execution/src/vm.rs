bee295602d953b011007bd0e50dc034a3909aa0a
Damir Vodenicarevic <damipator@gmail.com>
2021-11-27 00:37:46 +0100

    updates

--- execution/src/vm.rs
+++ execution/src/vm.rs
@@ -1,42 +1,73 @@
-use models::address::AddressHashMap;
-use models::Address;
+use crypto::hash::Hash;
+use models::hhasher::HHashMap;
+use models::{Address, Block, BlockId, Slot};
+use models::{address::AddressHashMap, Amount};
 use parking_lot::Mutex;
 use std::sync::Arc;
 use wasmer::{imports, Function, ImportObject, Instance, Module, Store, WasmerEnv};
 
+use crate::ExecutionConfig;
+
 /// Example API available to wasm code, aka "syscall".
-fn foo(env: &ExecutionContext, n: i32) -> i32 {
+fn foo(shared_env: &SharedExecutionContext, n: i32) -> i32 {
     n
 }
 
 /// API allowing a contract to call another.
-fn call_address(env: &ExecutionContext, addr: Address) {
-    if let Some(module) = env.ledger.lock().get(&addr) {}
+fn call_address(shared_env: &SharedExecutionContext, addr: Address) {
+    if let Some(module) = Arc::clone(&shared_env.0).lock().active_ledger.get(&addr) {}
+}
+
+/// execution request
+pub enum ExecutionRequest {
+    RunFinalMiss(Slot), // Runs a final miss. TODO add the address drawn for this slot
+    RunFinalBlock((BlockId, Block)), // Runs a final block
+    RunActiveMiss(Slot), // Runs an active miss. TODO add the address drawn for this slot
+    RunActiveBlock((BlockId, Block)), // Runs an active block
+    ResetToFinalState,  // Resets to final state
+    Stop,               // Stops the VM thread
+}
+
+#[derive(Debug, Clone)]
+struct SCELedgerEntry {
+    pub balance: Amount,
+    pub module: Module,
+    pub data: HHashMap<Hash, Vec<u8>>,
 }
 
 #[derive(WasmerEnv, Clone)]
 /// Stateful context, providing an execution context to host functions("syscalls").
 pub struct ExecutionContext {
-    ledger: Arc<Mutex<AddressHashMap<Module>>>,
+    pub final_ledger: AddressHashMap<SCELedgerEntry>,
+    pub active_ledger: AddressHashMap<SCELedgerEntry>,
 }
+
+#[derive(WasmerEnv, Clone)]
+pub struct SharedExecutionContext(pub Arc<Mutex<ExecutionContext>>);
 
 pub struct VM {
     imports: ImportObject,
-    store: Arc<Store>,
-    ledger: Arc<Mutex<AddressHashMap<Module>>>,
+    store: Store,
+    shared_execution_context: SharedExecutionContext,
 }
 
 impl VM {
-    pub fn new(store: Arc<Store>, ledger: Arc<Mutex<AddressHashMap<Module>>>) -> VM {
+    pub fn new() -> VM {
+        let store = Store::default();
+        let shared_execution_context =
+            SharedExecutionContext(Arc::new(Mutex::new(ExecutionContext {
+                final_ledger: Default::default(),  // TODO bootstrap
+                active_ledger: Default::default(), // TODO bootstrap
+            })));
         let imports = imports! {
             "env" => {
-                "foo" => Function::new_native_with_env(&store, ExecutionContext {ledger: ledger.clone()}, foo),
+                "foo" => Function::new_native_with_env(&store, shared_execution_context.clone(), foo),
             },
         };
         VM {
             imports,
             store,
-            ledger,
+            shared_execution_context,
         }
     }
 
