db55930ab8f81f73bc49a627a0fa11d7dcc60c51
Yvan Sraka <yvan@sraka.xyz>
2021-12-08 11:14:47 +0100

    Make `massa-node` & `massa-client` config loading behave the same way

--- massa-node/src/settings.rs
+++ massa-node/src/settings.rs
@@ -1,6 +1,5 @@
 // Copyright (c) 2021 MASSA LABS <info@massa.net>
 
-extern crate directories;
 use bootstrap::BootstrapSettings;
 use consensus::ConsensusSettings;
 use directories::ProjectDirs;
@@ -13,12 +12,12 @@ use serde::Deserialize;
 
 lazy_static::lazy_static! {
     pub static ref VERSION: Version = "TEST.5.0".parse().unwrap();
-
+    // TODO: this code is duplicated from /massa-client/settings.rs and should be part of a custom crate
     pub static ref SETTINGS: Settings = {
         let mut settings = config::Config::default();
         let config_path = std::env::var("MASSA_CONFIG_PATH").unwrap_or_else(|_| "base_config/config.toml".to_string());
-        settings.merge(config::File::with_name(&config_path)).unwrap_or_else(|_| panic!("failed to read {} config.... {}", config_path, std::env::current_dir().unwrap().as_path().to_str().unwrap()));
-        if let Some(proj_dirs) = ProjectDirs::from("com", "MassaLabs", "Massa") { // Portable user config loading
+        settings.merge(config::File::with_name(&config_path)).unwrap_or_else(|_| panic!("failed to read {} config {}", config_path, std::env::current_dir().unwrap().as_path().to_str().unwrap()));
+        if let Some(proj_dirs) = ProjectDirs::from("com", "MassaLabs", "massa-node") { // Portable user config loading
             let user_config_path = proj_dirs.config_dir();
             if user_config_path.exists() {
                 let path_str = user_config_path.to_str().unwrap();
