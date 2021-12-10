72a7023438c6927dc96d6d098f4eec69f3b2e801
Adrien Zinger <zinger.ad@gmail.com>
2021-12-10 16:46:20 +0100

    Fix unit test after stop improvement

--- massa-execution/src/tests/scenarios_mandatories.rs
+++ massa-execution/src/tests/scenarios_mandatories.rs
@@ -12,21 +12,27 @@ async fn test_execution_basic() {
 #[tokio::test]
 #[serial]
 async fn test_execution_shutdown() {
-    let (_command_sender, _event_receiver, manager) = start_controller(ExecutionConfig {}, 2)
+    let (_command_sender, event_receiver, manager) = start_controller(ExecutionConfig {}, 2)
         .await
         .expect("Failed to start execution.");
-    manager.stop().await.expect("Failed to stop execution.");
+    manager
+        .stop(event_receiver)
+        .await
+        .expect("Failed to stop execution.");
 }
 
 #[tokio::test]
 #[serial]
 async fn test_sending_command() {
-    let (mut command_sender, _event_receiver, manager) = start_controller(ExecutionConfig {}, 2)
+    let (mut command_sender, event_receiver, manager) = start_controller(ExecutionConfig {}, 2)
         .await
         .expect("Failed to start execution.");
     command_sender
         .update_blockclique(Default::default(), Default::default())
         .await
         .expect("Failed to send command");
-    manager.stop().await.expect("Failed to stop execution.");
+    manager
+        .stop(event_receiver)
+        .await
+        .expect("Failed to stop execution.");
 }
