ec40770002ce6a55b549d2ac0a43a5dd80b34e85
Yvan Sraka <yvan@sraka.xyz>
2021-12-11 13:54:47 +0100

    Document settings utility functions of consensus

--- massa-api/src/public.rs
+++ massa-api/src/public.rs
@@ -96,7 +96,7 @@ impl Endpoints for API<Public> {
         let compensation_millis = self.0.compensation_millis;
         let mut pool_command_sender = self.0.pool_command_sender.clone();
         let node_id = self.0.node_id;
-        let config = consensus_settings.config();
+        let config = consensus_settings.compact_config();
         let closure = async move || {
             let now = MassaTime::now(compensation_millis)?;
             let last_slot = get_latest_block_slot_at_timestamp(
