6baba907bd4180849ea920b4fc015d06547aed9a
Damir Vodenicarevic <damipator@gmail.com>
2021-12-17 15:40:04 +0100

    rename bootstrap_state.rs to exports.rs

--- massa-execution/src/lib.rs
+++ massa-execution/src/lib.rs
@@ -1,19 +1,19 @@
-mod bootstrap_state;
 mod config;
 mod controller;
 mod error;
+mod exports;
 mod interface_impl;
 mod sce_ledger;
 mod types;
 mod vm;
 mod worker;
 
-pub use bootstrap_state::BootstrapExecutionState;
 pub use config::ExecutionSettings;
 pub use controller::{
     start_controller, ExecutionCommandSender, ExecutionEventReceiver, ExecutionManager,
 };
 pub use error::ExecutionError;
+pub use exports::BootstrapExecutionState;
 pub use sce_ledger::{SCELedger, SCELedgerEntry};
 pub use worker::ExecutionCommand;
 pub use worker::ExecutionEvent;
