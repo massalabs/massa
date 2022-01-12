60dd706fc0f168b1871483fe888cc45c7a99b04d
Your Name <you@example.com>
2022-01-12 16:09:53 +0800

    consensus: add test for processing of execute sc operation

--- massa-consensus/src/tests/scenarios_operations_check.rs
+++ massa-consensus/src/tests/scenarios_operations_check.rs
@@ -186,3 +186,85 @@ async fn test_operations_check() {
         .unwrap();
     pool_sink.stop().await;
 }
+
+#[tokio::test]
+#[serial]
+async fn test_execution_check() {
+    let thread_count = 2;
+
+    let private_key_1 = generate_random_private_key();
+    let public_key_1 = derive_public_key(&private_key_1);
+    let address_1 = Address::from_public_key(&public_key_1);
+
+    let mut ledger = HashMap::new();
+    ledger.insert(address_1, LedgerData::new(Amount::from_str("5").unwrap()));
+
+    let ledger_file = generate_ledger_file(&ledger);
+    let staking_keys: Vec<PrivateKey> = vec![private_key_1];
+    let staking_file = tools::generate_staking_keys_file(&staking_keys);
+    let roll_counts_file = tools::generate_default_roll_counts_file(staking_keys.clone());
+    let mut cfg = tools::default_consensus_config(
+        ledger_file.path(),
+        roll_counts_file.path(),
+        staking_file.path(),
+    );
+    cfg.t0 = 1000.into();
+    cfg.future_block_processing_max_periods = 50;
+    cfg.max_future_processing_blocks = 10;
+    cfg.block_reward = Amount::from_str("1").unwrap();
+    cfg.thread_count = thread_count;
+    cfg.operation_validity_periods = 10;
+    cfg.disable_block_creation = true;
+    cfg.genesis_timestamp = cfg.genesis_timestamp.saturating_sub(10000.into());
+
+    tools::consensus_without_pool_test(
+        cfg.clone(),
+        async move |mut protocol_controller, consensus_command_sender, consensus_event_receiver| {
+            let genesis_ids = consensus_command_sender
+                .get_block_graph_status(None, None)
+                .await
+                .expect("could not get block graph status")
+                .genesis_blocks;
+
+            // Valid block A executing some bytecode and spending 2 coins.
+            let operation_1 = tools::create_executesc(
+                private_key_1,
+                public_key_1,
+                5,
+                5,
+                Default::default(),
+                1,
+                2,
+                1,
+            );
+            let (id_a, block_a, _) = create_block_with_operations(
+                &cfg,
+                Slot::new(1, 0),
+                &genesis_ids,
+                private_key_1,
+                vec![operation_1.clone()],
+            );
+            propagate_block(&mut protocol_controller, block_a, true, 150).await;
+
+            // assert the `coins` argument as been deducted from the balance of address 1.
+            let mut set = AddressHashSet::default();
+            set.insert(address_1);
+            let res = consensus_command_sender
+                .get_addresses_info(set)
+                .await
+                .unwrap()
+                .get(&address_1)
+                .unwrap()
+                .ledger_info
+                .candidate_ledger_info;
+            assert_eq!(res.balance, Amount::from_str("3").unwrap());
+
+            (
+                protocol_controller,
+                consensus_command_sender,
+                consensus_event_receiver,
+            )
+        },
+    )
+    .await;
+}
