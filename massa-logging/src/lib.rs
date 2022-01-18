72567ab4c5d6681b24569d3852c398f715920139
Yvan Sraka <yvan@sraka.xyz>
2022-01-18 15:21:04 +0100

    Quick fix `massa_trace` (simplify it a bit)

--- massa-logging/src/lib.rs
+++ massa-logging/src/lib.rs
@@ -3,14 +3,6 @@
 #[macro_export]
 macro_rules! massa_trace {
     ($evt:expr, $params:tt) => {
-        tracing::trace!(
-            "massa_trace:{}",
-            serde_json::json!({
-                // "origin": function_path!(),
-                "event": $evt,
-                "parameters": $params
-            })
-            .to_string()
-        );
+        tracing::trace!("massa_trace:{}:{}", $evt, serde_json::json!($params));
     };
 }
