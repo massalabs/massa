ead81d27a186eb63038879698ea3ba64e2ee2a5e
damip <damipator@gmail.com>
2022-01-26 15:45:05 +0100

    add debug to hashes

--- massa-hash/src/hash.rs
+++ massa-hash/src/hash.rs
@@ -5,10 +5,16 @@ use crate::settings::HASH_SIZE_BYTES;
 use bitcoin_hashes;
 use std::{convert::TryInto, str::FromStr};
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash)]
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash)]
 pub struct Hash(bitcoin_hashes::sha256::Hash);
 
 impl std::fmt::Display for Hash {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.to_bs58_check())
+    }
+}
+
+impl std::fmt::Debug for Hash {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "{}", self.to_bs58_check())
     }
