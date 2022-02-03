d6f80d669c9bbfc270fda74b16b946a0df469c96
Aurelia <adolo@massa.network>
2022-02-03 12:47:07 +0100

    Prefix node id string representation.

--- massa-models/src/node.rs
+++ massa-models/src/node.rs
@@ -3,28 +3,39 @@
 use massa_signature::PublicKey;
 use serde::{Deserialize, Serialize};
 
+use crate::ModelsError;
+
+const NODE_ID_STRING_PREFIX: &str = "NOD";
 /// NodeId wraps a public key to uniquely identify a node.
 #[derive(Clone, Copy, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct NodeId(pub PublicKey);
 
 impl std::fmt::Display for NodeId {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "{}-{}", NODE_ID_STRING_PREFIX, self.0)
     }
 }
 
 impl std::fmt::Debug for NodeId {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.0.to_bs58_check())
+        write!(f, "{}-{}", NODE_ID_STRING_PREFIX, self.0.to_bs58_check())
     }
 }
 
 impl std::str::FromStr for NodeId {
-    type Err = ();
+    type Err = ModelsError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match PublicKey::from_bs58_check(s) {
-            Ok(x) => Ok(NodeId(x)),
-            Err(_) => Err(()),
+        let v: Vec<_> = s.split('-').collect();
+        if v.len() != 2 {
+            // assume there is no prefix
+            Ok(NodeId(PublicKey::from_bs58_check(s)?))
+        } else if v[0] != NODE_ID_STRING_PREFIX {
+            Err(ModelsError::WrongPrefix(
+                NODE_ID_STRING_PREFIX.to_string(),
+                v[0].to_string(),
+            ))
+        } else {
+            Ok(NodeId(PublicKey::from_bs58_check(v[1])?))
         }
     }
 }
