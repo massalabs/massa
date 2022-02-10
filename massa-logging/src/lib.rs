ec8dfda79b52efbd9276aff5b01aa78e5b8111f0
Yvan Sraka <yvan@sraka.xyz>
2022-02-10 15:31:04 +0100

    [DEBUG] Trace the computation made at runtime

--- massa-logging/src/lib.rs
+++ massa-logging/src/lib.rs
@@ -3,6 +3,14 @@
 #[macro_export]
 macro_rules! massa_trace {
     ($evt:expr, $params:tt) => {
-        tracing::trace!("massa_trace:{}:{}", $evt, serde_json::json!($params));
+        tracing::trace!(
+            "massa_trace:{}:{}",
+            $evt,
+            // THIS IS FOR DEBUG PURPOSE ONLY
+            |string: serde_json::Value| -> serde_json::Value {
+                println!("##### TRACE WAS EVALUATED AT RUNTIME! #####");
+                string
+            }(serde_json::json!($params))
+        );
     };
 }
