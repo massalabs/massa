d8f0e3dd39083b825cbc30c909fa447dc1f25892
AurelienFT <aurelien.foucault@epitech.eu>
2022-02-14 10:06:18 +0100

    Remove unecessary error message

--- massa-client/src/repl.rs
+++ massa-client/src/repl.rs
@@ -154,8 +154,6 @@ impl Completion for CommandCompletion {
                 }
                 Some(args.join(" "))
             } else {
-                println!();
-                println!("Bad syntax for path completion.");
                 Some(args.join(" "))
             }
         } else {
