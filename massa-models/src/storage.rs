1cd6a2f0a08011be1ebccd7b358313d8c9572ddc
gterzian <2792687+gterzian@users.noreply.github.com>
2022-03-21 19:21:13 +0800

    storage: add pruning, docs

--- massa-models/src/storage.rs
+++ massa-models/src/storage.rs
@@ -6,17 +6,23 @@ use std::sync::Arc;
 
 #[derive(Debug)]
 pub struct StoredBlock {
+    /// The block.
     pub block: Block,
+    /// The serialized representation of the block.
     pub serialized: Vec<u8>,
+    /// The serialized representation of the header, if any.
+    /// Note: the header is written as part of propagation of headers.
     pub serialized_header: Option<Vec<u8>>,
 }
 
+/// A storage of block, shared by various components.
 #[derive(Clone, Default)]
 pub struct Storage {
     blocks: Arc<RwLock<Map<BlockId, Arc<RwLock<StoredBlock>>>>>,
 }
 
 impl Storage {
+    /// Store a block, along with it's serialized representation.
     pub fn store_block(&self, block_id: BlockId, block: Block, serialized: Vec<u8>) {
         let mut blocks = self.blocks.write();
         match blocks.entry(block_id) {
@@ -33,8 +39,17 @@ impl Storage {
         }
     }
 
+    /// Get a (mutable) reference to the stored block.
     pub fn retrieve_block(&self, block_id: &BlockId) -> Option<Arc<RwLock<StoredBlock>>> {
         let blocks = self.blocks.read();
         blocks.get(block_id).map(Arc::clone)
+    }
+
+    /// Remove a list of blocks from storage.
+    pub fn remove_blocks(&self, block_ids: &[BlockId]) {
+        let mut blocks = self.blocks.write();
+        for id in block_ids {
+            blocks.remove(&id);
+        }
     }
 }
