f6625003ea85c45708e229a3a9a37e7c6c8cb112
Adrien <adrien.laversanne-finot@inria.fr>
2022-03-25 22:07:01 +0100

    Change theme

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Massa Labs'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
