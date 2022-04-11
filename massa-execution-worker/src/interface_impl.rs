2661c22213d263e5822d44ded4049b773bfefd1a
Thomas Plisson <thomas.plisson@epitech.eu>
2022-04-11 11:39:53 +0200

    Update testing setup.

--- massa-execution-worker/src/interface_impl.rs
+++ massa-execution-worker/src/interface_impl.rs
@@ -21,7 +21,7 @@ use parking_lot::Mutex;
 use rand::Rng;
 use std::str::FromStr;
 use std::sync::Arc;
-use tracing::debug;
+use tracing::warn;
 
 /// helper for locking the context mutex
 macro_rules! context_guard {
@@ -64,7 +64,7 @@ impl InterfaceClone for InterfaceImpl {
 impl Interface for InterfaceImpl {
     /// prints a message in the node logs at log level 3 (debug)
     fn print(&self, message: &str) -> Result<()> {
-        debug!("SC print: {}", message);
+        warn!("SC print: {}", message);
         Ok(())
     }
 
