a4a297e5e5bd3791f4bbde7c2348ead8e7e1a6ba
Adrien <adrien.laversanne-finot@inria.fr>
2022-04-11 22:42:56 +0200

    Use massa's logo and color

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,26 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    'logo_only': True,
+    'style_nav_header_background': '#D75954',
+    'display_version': True,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "logo.svg"
