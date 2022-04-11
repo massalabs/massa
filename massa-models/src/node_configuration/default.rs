ff1d06a9a7fa0a664398d4bc27c1a13acb09b507
Damir Vodenicarevic <damipator@gmail.com>
2022-04-11 19:01:49 +0200

    update version number

--- massa-models/src/node_configuration/default.rs
+++ massa-models/src/node_configuration/default.rs
@@ -63,7 +63,7 @@ lazy_static::lazy_static! {
         if cfg!(feature = "sandbox") {
             "SAND.0.0"
         } else {
-            "TEST.9.1"
+            "TEST.9.2"
         }
         .parse()
         .unwrap()
