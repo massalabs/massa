47c9568492d51d5a05b23cd851883faa2d3e4ff8
Thomas Plisson <thomas.plisson@epitech.eu>
2022-05-04 22:52:37 +0200

    Execution ledger disk implementation ebauche.

--- massa-ledger/src/ledger.rs
+++ massa-ledger/src/ledger.rs
@@ -8,6 +8,7 @@ use crate::types::{Applicable, SetUpdateOrDelete};
 use crate::{FinalLedgerBootstrapState, LedgerConfig, LedgerError};
 use massa_hash::Hash;
 use massa_models::{Address, Amount};
+use rocksdb::DB;
 use std::collections::BTreeMap;
 
 /// Represents a final ledger associating addresses to their balances, bytecode and data.
@@ -20,7 +21,8 @@ pub struct FinalLedger {
     /// ledger configuration
     _config: LedgerConfig,
     /// ledger tree, sorted by address
-    sorted_ledger: BTreeMap<Address, LedgerEntry>,
+    sorted_ledger: DB,
+    // sorted_ledger: BTreeMap<Address, LedgerEntry>,
 }
 
 /// Allows applying `LedgerChanges` to the final ledger
@@ -32,23 +34,32 @@ impl Applicable<LedgerChanges> for FinalLedger {
                 // the incoming change sets a ledger entry to a new one
                 SetUpdateOrDelete::Set(new_entry) => {
                     // inserts/overwrites the entry with the incoming one
-                    self.sorted_ledger.insert(addr, new_entry);
+                    self.sorted_ledger.put(
+                        addr.to_bytes(),
+                        bincode::serialize(&new_entry).expect("HANDLE THIS"),
+                    );
                 }
 
                 // the incoming change updates an existing ledger entry
                 SetUpdateOrDelete::Update(entry_update) => {
                     // applies the updates to the entry
                     // if the entry does not exist, inserts a default one and applies the updates to it
-                    self.sorted_ledger
-                        .entry(addr)
-                        .or_insert_with(Default::default)
-                        .apply(entry_update);
+                    match self.sorted_ledger.get(addr.to_bytes()) {
+                        Ok(Some(value)) => (), // apply changes
+                        Ok(None) => (),        // create new default entry and updates it
+                        Err(e) => (),          // handle error, is this happening on full disk?
+                    };
+                    // self.sorted_ledger
+                    //     .get(addr)
+                    //     .unwrap()
+                    //     .or_insert_with(Default::default)
+                    //     .apply(entry_update);
                 }
 
                 // the incoming change deletes a ledger entry
                 SetUpdateOrDelete::Delete => {
                     // delete the entry, if it exists
-                    self.sorted_ledger.remove(&addr);
+                    self.sorted_ledger.delete(addr.to_bytes());
                 }
             }
         }
@@ -74,23 +85,32 @@ pub(crate) use init_file_error;
 impl FinalLedger {
     /// Initializes a new `FinalLedger` by reading its initial state from file.
     pub fn new(config: LedgerConfig) -> Result<Self, LedgerError> {
+        // open db with default options
+        // and use only single threaded mode
+        // should explore these options before review
+        let sorted_ledger = DB::open_default("_path_for_rocksdb_storage").unwrap();
+
         // load the ledger tree from file
-        let sorted_ledger = serde_json::from_str::<BTreeMap<Address, Amount>>(
+        let initial_ledger = serde_json::from_str::<BTreeMap<Address, Amount>>(
             &std::fs::read_to_string(&config.initial_sce_ledger_path)
                 .map_err(init_file_error!("loading", config))?,
         )
-        .map_err(init_file_error!("parsing", config))?
-        .into_iter()
-        .map(|(address, balance)| {
-            (
-                address,
-                LedgerEntry {
-                    parallel_balance: balance,
-                    ..Default::default()
-                },
-            )
-        })
-        .collect();
+        .map_err(init_file_error!("parsing", config))?;
+
+        // parsing from json to rust types, then types to bytes, maybe
+        // there is a better way to do this
+        for (address, amount) in &initial_ledger {
+            sorted_ledger
+                .put(
+                    address.to_bytes(),
+                    bincode::serialize(&LedgerEntry {
+                        parallel_balance: *amount,
+                        ..Default::default()
+                    })
+                    .map_err(init_file_error!("serialize", config))?,
+                )
+                .map_err(init_file_error!("insert", config))?;
+        }
 
         // generate the final ledger
         Ok(FinalLedger {
@@ -107,8 +127,20 @@ impl FinalLedger {
     /// * configuration: ledger configuration
     /// * state: bootstrap state
     pub fn from_bootstrap_state(config: LedgerConfig, state: FinalLedgerBootstrapState) -> Self {
+        // conversion code if needed for testing
+        // temporary because bootstrap streaming is not ready yet
+        // let sorted_ledger = DB::open_default("_path_for_rocksdb_storage").unwrap();
+        // for (address, entry) in &state.sorted_ledger {
+        //     sorted_ledger
+        //         .put(
+        //             address.to_bytes(),
+        //             bincode::serialize(entry).expect("WIIL BE NATURALLY REMOVED"),
+        //         )
+        //         .expect("WIIL BE NATURALLY REMOVED");
+        // }
+        // dummy temp implem since an actual one would have no purpose rn
         FinalLedger {
-            sorted_ledger: state.sorted_ledger,
+            sorted_ledger: DB::open_default("_path_for_rocksdb_storage").unwrap(),
             _config: config,
         }
     }
@@ -117,8 +149,9 @@ impl FinalLedger {
     ///
     /// TODO: This loads the whole ledger in RAM. Switch to streaming in the future
     pub fn get_bootstrap_state(&self) -> FinalLedgerBootstrapState {
+        // dummy temp implem since an actual one would have no purpose rn
         FinalLedgerBootstrapState {
-            sorted_ledger: self.sorted_ledger.clone(),
+            sorted_ledger: BTreeMap::new(),
         }
     }
 
@@ -130,7 +163,12 @@ impl FinalLedger {
     /// TODO: in the future, never manipulate full ledger entries because their datastore can be huge
     /// `https://github.com/massalabs/massa/issues/2342`
     pub fn get_full_entry(&self, addr: &Address) -> Option<LedgerEntry> {
-        self.sorted_ledger.get(addr).cloned()
+        // would probably be worth it to make a db module
+        self.sorted_ledger
+            .get(addr.to_bytes())
+            .ok()
+            .flatten()
+            .map(|bytes| bincode::deserialize(&bytes).expect("HANDLE THIS"))
     }
 
     /// Gets the parallel balance of a ledger entry
@@ -138,7 +176,9 @@ impl FinalLedger {
     /// # Returns
     /// The parallel balance, or None if the ledger entry was not found
     pub fn get_parallel_balance(&self, addr: &Address) -> Option<Amount> {
-        self.sorted_ledger.get(addr).map(|v| v.parallel_balance)
+        self.sorted_ledger
+            .get(addr.to_bytes())
+            .map(|v| v.parallel_balance)
     }
 
     /// Gets a copy of the bytecode of a ledger entry
