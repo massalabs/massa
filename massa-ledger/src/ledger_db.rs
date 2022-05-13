a6dda3079802d4af074dd3aa57835577560bbbe3
Thomas Plisson <thomas.plisson@epitech.eu>
2022-05-13 14:09:56 +0200

    Refactor db module impl because of CF's limitations.

--- massa-ledger/src/ledger_db.rs
+++ massa-ledger/src/ledger_db.rs
@@ -2,14 +2,15 @@
 
 use massa_hash::Hash;
 use massa_models::{Address, Amount};
-use rocksdb::{Options, WriteBatch, DB};
+use rocksdb::{ColumnFamilyDescriptor, Options, WriteBatch, DB};
 use std::collections::BTreeMap;
 
 use crate::{ledger_changes::LedgerEntryUpdate, LedgerEntry, SetOrDelete, SetOrKeep};
 
-const DB_PATH: &str = "../ledger_db";
+const DB_PATH: &str = "../.db";
 const BALANCE_CF: &str = "balance";
 const BYTECODE_CF: &str = "bytecode";
+const DATASTORE_CF: &str = "datastore";
 const OPEN_ERROR: &str = "critical: rocksdb open operation failed";
 const CRUD_ERROR: &str = "critical: rocksdb crud operation failed";
 const CF_ERROR: &str = "critical: rocksdb column family operation failed";
@@ -22,18 +23,30 @@ pub(crate) enum LedgerDBEntry {
 
 pub(crate) struct LedgerDB(DB);
 
+macro_rules! data_key {
+    ($addr:ident, $key:ident) => {
+        format!("{}:{}", $addr, $key).as_bytes()
+    };
+}
+
 impl LedgerDB {
     pub fn new() -> Self {
-        // options
-        let mut opts = Options::default();
-        opts.create_if_missing(true);
+        // db options
+        let mut db_opts = Options::default();
+        db_opts.create_if_missing(true);
+        db_opts.create_missing_column_families(true);
 
         // database init
-        let mut db = DB::open(&opts, DB_PATH).expect(OPEN_ERROR);
-        db.create_cf(BALANCE_CF, &Options::default())
-            .expect(CF_ERROR);
-        db.create_cf(BYTECODE_CF, &Options::default())
-            .expect(CF_ERROR);
+        let db = DB::open_cf_descriptors(
+            &db_opts,
+            DB_PATH,
+            vec![
+                ColumnFamilyDescriptor::new(BALANCE_CF, Options::default()),
+                ColumnFamilyDescriptor::new(BYTECODE_CF, Options::default()),
+                ColumnFamilyDescriptor::new(DATASTORE_CF, Options::default()),
+            ],
+        )
+        .expect(OPEN_ERROR);
 
         // return database
         LedgerDB(db)
@@ -58,17 +71,9 @@ impl LedgerDB {
         );
 
         // datastore
-        if !ledger_entry.datastore.is_empty() {
-            // note: this might be enough
-            let cf_name = addr.to_string();
-            self.0
-                .create_cf(&cf_name, &Options::default())
-                .expect(CF_ERROR);
-            let cf = self.0.cf_handle(&cf_name).expect(CF_ERROR);
-            for (hash, entry) in ledger_entry.datastore {
-                let data_key = hash.to_bytes();
-                batch.put_cf(&cf, data_key, entry);
-            }
+        let data_cf = self.0.cf_handle(DATASTORE_CF).expect(CF_ERROR);
+        for (hash, entry) in ledger_entry.datastore {
+            batch.put_cf(data_cf, data_key!(addr, hash), entry);
         }
 
         // write batch
@@ -98,23 +103,11 @@ impl LedgerDB {
         }
 
         // datastore
-        if !entry_update.datastore.is_empty() {
-            let cf_name = addr.to_string();
-            let cf = match self.0.cf_handle(&cf_name) {
-                Some(cf) => cf,
-                None => {
-                    self.0
-                        .create_cf(&cf_name, &Options::default())
-                        .expect(CF_ERROR);
-                    self.0.cf_handle(&cf_name).expect(CF_ERROR)
-                }
-            };
-            for (hash, update) in entry_update.datastore {
-                let data_key = hash.to_bytes();
-                match update {
-                    SetOrDelete::Set(entry) => batch.put_cf(&cf, data_key, entry),
-                    SetOrDelete::Delete => batch.delete_cf(&cf, data_key),
-                }
+        let data_cf = self.0.cf_handle(DATASTORE_CF).expect(CF_ERROR);
+        for (hash, update) in entry_update.datastore {
+            match update {
+                SetOrDelete::Set(entry) => batch.put_cf(&data_cf, data_key!(addr, hash), entry),
+                SetOrDelete::Delete => batch.delete_cf(&data_cf, data_key!(addr, hash)),
             }
         }
 
@@ -199,4 +192,6 @@ fn ledger_db_test() {
         ..Default::default()
     };
     db.put(&a, entry);
+    assert!(db.entry_exists(&a, LedgerDBEntry::Balance));
+    assert!(!db.entry_exists(&b, LedgerDBEntry::Balance));
 }
