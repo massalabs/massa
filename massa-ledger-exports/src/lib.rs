644aba6b4e93e1e256dddf4a937d0697861ef879
Thomas Plisson <thomas.plisson@epitech.eu>
2022-06-10 13:05:06 +0200

    Remove key export.

--- massa-ledger-exports/src/lib.rs
+++ massa-ledger-exports/src/lib.rs
@@ -9,6 +9,5 @@ pub use error::LedgerError;
 pub use ledger_changes::{
     LedgerChanges, LedgerChangesDeserializer, LedgerChangesSerializer, LedgerEntryUpdate,
 };
-// pub use ledger_db::{get_address_from_key, KeyDeserializer, KeySerializer};
 pub use ledger_entry::LedgerEntry;
 pub use types::{Applicable, SetOrDelete, SetOrKeep, SetUpdateOrDelete};
