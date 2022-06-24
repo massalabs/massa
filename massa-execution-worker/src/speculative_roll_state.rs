d981f6565ca7a4cb2f6750ebada03686e1feccf7
Thomas Plisson <thomas.plisson@epitech.eu>
2022-06-24 11:20:12 +0200

    Update spec roll state.

--- massa-execution-worker/src/speculative_roll_state.rs
+++ massa-execution-worker/src/speculative_roll_state.rs
@@ -1,11 +1,19 @@
 // Copyright (c) 2022 MASSA LABS <info@massa.net>
 
+use std::sync::Arc;
+
 use massa_pos_exports::{PoSChanges, SelectorController};
+use parking_lot::RwLock;
+
+use crate::active_history::ActiveHistory;
 
 /// Speculative state of the rolls
 pub(crate) struct SpeculativeRollState {
     /// Selector used to feed_cycle and get_selection
     selector: Box<dyn SelectorController>,
+    /// History of the outputs of recently executed slots.
+    /// Slots should be consecutive, newest at the back.
+    active_history: Arc<RwLock<ActiveHistory>>,
     /// List of changes to the state after settling roll sell/buy
     settled_changes: PoSChanges,
 }
