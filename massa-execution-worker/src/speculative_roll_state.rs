1b8f15a9d583a1e41a767a7717b4138923c40d8f
Thomas Plisson <thomas.plisson@epitech.eu>
2022-06-24 11:32:58 +0200

    SRS basic functions..

--- massa-execution-worker/src/speculative_roll_state.rs
+++ massa-execution-worker/src/speculative_roll_state.rs
@@ -15,5 +15,39 @@ pub(crate) struct SpeculativeRollState {
     /// Slots should be consecutive, newest at the back.
     active_history: Arc<RwLock<ActiveHistory>>,
     /// List of changes to the state after settling roll sell/buy
-    settled_changes: PoSChanges,
+    added_changes: PoSChanges,
+}
+
+impl SpeculativeRollState {
+    /// Creates a new `SpeculativeRollState`
+    ///
+    /// # Arguments
+    /// * `selector`: PoS draws selector controller
+    /// * `active_history`: thread-safe shared access the speculative execution history
+    pub fn new(
+        selector: Box<dyn SelectorController>,
+        active_history: Arc<RwLock<ActiveHistory>>,
+    ) -> Self {
+        SpeculativeRollState {
+            selector,
+            active_history,
+            added_changes: Default::default(),
+        }
+    }
+
+    /// Returns the changes caused to the `SpeculativeRollState` since its creation,
+    /// and resets their local value to nothing.
+    pub fn take(&mut self) -> PoSChanges {
+        std::mem::take(&mut self.added_changes)
+    }
+
+    /// Takes a snapshot (clone) of the changes caused to the `SpeculativeRollState` since its creation
+    pub fn get_snapshot(&self) -> PoSChanges {
+        self.added_changes.clone()
+    }
+
+    /// Resets the `SpeculativeRollState` to a snapshot (see `get_snapshot` method)
+    pub fn reset_to_snapshot(&mut self, snapshot: PoSChanges) {
+        self.added_changes = snapshot;
+    }
 }
