f355eb47794751e2984758b8c76befa19422c836
Thomas Plisson <thomas.plisson@epitech.eu>
2022-06-30 17:13:15 +0200

    Fmt.

--- massa-cipher/src/constants.rs
+++ massa-cipher/src/constants.rs
@@ -10,7 +10,7 @@ use pbkdf2::Params;
 pub const VERSION: u32 = 0;
 
 /// AES-GCM-SIV nonce size.
-/// 
+///
 /// A nonce is a single-use value which enables securely encrypting multiple messages under the same key.
 /// Nonces need not be random: a counter can be used so long as the values are never repeated under the same key.
 pub const NONCE_SIZE: usize = 12;
