d908f8ac9d2b2f14c5304ff8ced6f09eec4c1038
damip <damipator@gmail.com>
2022-07-13 13:55:23 +0200

    prepare test.12.1

--- massa-models/src/node_configuration/default.rs
+++ massa-models/src/node_configuration/default.rs
@@ -64,7 +64,7 @@ lazy_static::lazy_static! {
         if cfg!(feature = "sandbox") {
             "SAND.0.0"
         } else {
-            "TEST.12.0"
+            "TEST.12.1"
         }
         .parse()
         .unwrap()
