e6b9690ab10867816e0bf9703596b2e4e1064076
Thomas Plisson <thomas.plisson@epitech.eu>
2022-07-15 11:41:40 +0200

    Implement PoSChangesSerializer.

--- massa-pos-exports/src/types.rs
+++ massa-pos-exports/src/types.rs
@@ -118,7 +118,54 @@ impl PoSChangesSerializer {
 }
 
 impl Serializer<PoSChanges> for PoSChangesSerializer {
-    fn serialize(&self, _value: &PoSChanges, _buffer: &mut Vec<u8>) -> Result<(), SerializeError> {
+    fn serialize(&self, value: &PoSChanges, buffer: &mut Vec<u8>) -> Result<(), SerializeError> {
+        let _roll_changes = {
+            let entry_count: u64 = value.roll_changes.len().try_into().map_err(|err| {
+                SerializeError::GeneralError(format!("too many entries in roll_changes: {}", err))
+            })?;
+            self.u64_serializer.serialize(&entry_count, buffer)?;
+            for (addr, roll) in value.roll_changes.iter() {
+                buffer.extend(addr.to_bytes());
+                self.u64_serializer.serialize(roll, buffer)?;
+            }
+        };
+        let _production_stats = {
+            let entry_count: u64 = value.production_stats.len().try_into().map_err(|err| {
+                SerializeError::GeneralError(format!(
+                    "too many entries in production_stats: {}",
+                    err
+                ))
+            })?;
+            self.u64_serializer.serialize(&entry_count, buffer)?;
+            for (
+                addr,
+                ProductionStats {
+                    block_success_count,
+                    block_failure_count,
+                },
+            ) in value.production_stats.iter()
+            {
+                buffer.extend(addr.to_bytes());
+                self.u64_serializer.serialize(block_success_count, buffer)?;
+                self.u64_serializer.serialize(block_failure_count, buffer)?;
+            }
+        };
+        let _deferred_credits = {
+            let entry_count: u64 = value.production_stats.len().try_into().map_err(|err| {
+                SerializeError::GeneralError(format!(
+                    "too many entries in deferred_credits: {}",
+                    err
+                ))
+            })?;
+            self.u64_serializer.serialize(&entry_count, buffer)?;
+            for (slot, credits) in value.deferred_credits.iter() {
+                self.slot_serializer.serialize(slot, buffer)?;
+                for (addr, amount) in credits {
+                    buffer.extend(addr.to_bytes());
+                    self.amount_serializer.serialize(amount, buffer)?;
+                }
+            }
+        };
         Ok(())
     }
 }
