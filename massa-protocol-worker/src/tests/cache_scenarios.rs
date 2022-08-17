d555d0fd198d62aad64d8b68132c76e6415b1b0b
gterzian <2792687+gterzian@users.noreply.github.com>
2022-08-17 16:30:42 +0800

    protocol: fix cache test

--- massa-protocol-worker/src/tests/cache_scenarios.rs
+++ massa-protocol-worker/src/tests/cache_scenarios.rs
@@ -2,9 +2,10 @@
 
 // RUST_BACKTRACE=1 cargo test test_one_handshake -- --nocapture --test-threads=1
 
-use super::tools::protocol_test;
+use super::tools::{protocol_test, protocol_test_with_storage};
 use massa_models::constants::THREAD_COUNT;
 use massa_models::{self, Address, Slot};
+use massa_network_exports::{AskForBlocksInfo, NetworkCommand, ReplyForBlocksInfo};
 use massa_protocol_exports::tests::tools;
 use massa_protocol_exports::ProtocolConfig;
 use serial_test::serial;
@@ -22,32 +23,65 @@ lazy_static::lazy_static! {
 
 #[tokio::test]
 #[serial]
-#[ignore]
 async fn test_noting_block_does_not_panic_with_zero_max_node_known_blocks_size() {
     let protocol_config = &CUSTOM_PROTOCOL_CONFIG;
 
-    protocol_test(
+    protocol_test_with_storage(
         protocol_config,
         async move |mut network_controller,
                     protocol_event_receiver,
                     protocol_command_sender,
                     protocol_manager,
-                    protocol_pool_event_receiver| {
-            // Create 1 node.
-            let nodes = tools::create_and_connect_nodes(1, &mut network_controller).await;
+                    protocol_pool_event_receiver,
+                    mut storage| {
+            // Create 2 node.
+            let nodes = tools::create_and_connect_nodes(2, &mut network_controller).await;
 
             let address = Address::from_public_key(&nodes[0].id.0);
             let thread = address.get_thread(THREAD_COUNT);
 
             let operation = tools::create_operation_with_expire_period(&nodes[0].keypair, 1);
 
-            let _block = tools::create_block_with_operations(
+            let block = tools::create_block_with_operations(
                 &nodes[0].keypair,
                 Slot::new(1, thread),
                 vec![operation.clone()],
             );
 
-            // TODO: rewrite with block info.
+            // Add block to storage.
+            storage.store_block(block.clone());
+
+            // Have the second node ask for the operations.
+            let needed_ops = vec![operation.id].into_iter().collect();
+            network_controller
+                .send_ask_for_block(
+                    nodes[1].id,
+                    vec![(block.id, AskForBlocksInfo::Operations(needed_ops))],
+                )
+                .await;
+
+            // Check that protocol processes the message without panicking.
+            let cmd_filter = |cmd| match cmd {
+                NetworkCommand::SendBlockInfo { node, info } => {
+                    assert_eq!(node, nodes[1].id);
+                    Some(info)
+                }
+                _ => None,
+            };
+
+            let (block_id, info) = network_controller
+                .wait_command(100.into(), cmd_filter)
+                .await
+                .unwrap()
+                .pop()
+                .unwrap();
+
+            if let ReplyForBlocksInfo::Operations(ops) = info {
+                assert!(ops.contains(&operation.id));
+                assert_eq!(ops.len(), 1);
+            } else {
+                panic!("Unexpected block info.");
+            }
 
             (
                 network_controller,
