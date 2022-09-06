1428b1baf55f397a2b529b40f4f14df22bf309e5
gterzian <2792687+gterzian@users.noreply.github.com>
2022-09-06 15:22:51 +0800

    protocol: remove thread check

--- massa-protocol-worker/src/tests/in_block_operations_scenarios.rs
+++ massa-protocol-worker/src/tests/in_block_operations_scenarios.rs
@@ -71,12 +71,6 @@ async fn test_protocol_sends_blocks_with_operations_to_consensus() {
                 .await;
             }
 
-            // block with operation too far in the future
-            // Note: what happened with checking validity periods?
-
-            // block with an operation twice
-            // Note: what happened with checking for duplicates?
-
             // block with wrong merkle root
             {
                 let op = create_operation_with_expire_period(&keypair, 5);
@@ -142,28 +136,6 @@ async fn test_protocol_sends_blocks_with_operations_to_consensus() {
                 .await;
             }
 
-            // block with operation in wrong thread
-            {
-                let op = create_operation_with_expire_period(&keypair, 5);
-                let op_thread = op.creator_address.get_thread(protocol_config.thread_count);
-                let block_thread = if op_thread == 1 { 0 } else { 1 };
-                let block = create_block_with_operations(
-                    &creator_node.keypair,
-                    Slot::new(1, block_thread),
-                    vec![op.clone()],
-                );
-
-                send_and_propagate_block(
-                    &mut network_controller,
-                    block,
-                    false,
-                    creator_node.id,
-                    &mut protocol_event_receiver,
-                    &mut protocol_command_sender,
-                    vec![op.clone()],
-                )
-                .await;
-            }
             (
                 network_controller,
                 protocol_event_receiver,
