c0b59721da4e818525fc5db1e84a637cc2708531
Damir Vodenicarevic <damipator@gmail.com>
2022-09-13 16:57:59 +0200

    create version incompatibility

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -64,7 +64,7 @@ lazy_static::lazy_static! {
         if cfg!(feature = "sandbox") {
             "SAND.0.0"
         } else {
-            "TEST.14.0"
+            "TEST.14.1"
         }
         .parse()
         .unwrap()
