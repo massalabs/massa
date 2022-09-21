bb9258151876ddd5b53c6f59e212bd53ec6c3a00
sydhds <sydhds@massa.net>
2022-09-21 10:37:42 +0200

    Use &[u8] instead of &Vec<u8> for Interface impl

--- massa-execution-worker/src/interface_impl.rs
+++ massa-execution-worker/src/interface_impl.rs
@@ -327,12 +327,12 @@ impl Interface for InterfaceImpl {
         Ok(keys)
     }
 
-    fn has_op_key(&self, key: &Vec<u8>) -> Result<bool> {
+    fn has_op_key(&self, key: &[u8]) -> Result<bool> {
         let context = context_guard!(self);
         Ok(context.datastore.contains_key(key))
     }
 
-    fn get_op_data(&self, key: &Vec<u8>) -> Result<Vec<u8>> {
+    fn get_op_data(&self, key: &[u8]) -> Result<Vec<u8>> {
         let context = context_guard!(self);
         context.datastore
             .get(key)
