74e0c4054ba4a9e85cc5dd0876f39ef86c09c0c7
Damir Vodenicarevic <damipator@gmail.com>
2022-10-02 11:14:29 +0200

    Optimize paralell sig verifier

--- massa-protocol-worker/src/sig_verifier.rs
+++ massa-protocol-worker/src/sig_verifier.rs
@@ -10,6 +10,27 @@ use rayon::prelude::*;
 /// Efficiently verifies a batch of signatures in parallel.
 /// Returns an error if at least one of them fails to verify.
 pub fn verify_sigs_batch(ops: &[(Hash, Signature, PublicKey)]) -> Result<(), ProtocolError> {
+    // nothing to verify => OK
+    if ops.len() == 0 {
+        return Ok(());
+    }
+
+    // normal verif is fastest for 1 verif
+    if ops.len() <= 1 {
+        let (hash, signature, public_key) = ops[0];
+        return public_key
+            .verify_signature(&hash, &signature)
+            .map_err(|_err| ProtocolError::WrongSignature);
+    }
+
+    // single-core batch verif is fastest for 2 verifs
+    if ops.len() <= 2 {
+        return massa_signature::verify_signature_batch(ops)
+            .map_err(|_err| ProtocolError::WrongSignature);
+    }
+
+    // otherwise, use parallel batch verif
+
     // compute chunk size for parallelization
     let chunk_size = std::cmp::max(1, ops.len() / rayon::current_num_threads());
     // process chunks in parallel
