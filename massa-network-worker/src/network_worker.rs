4cf19bcd188ac261d26504011781135a4f3ebe9c
Thomas Plisson <thomas.plisson@epitech.eu>
2022-10-18 14:30:31 +0200

    Fix network-worker module clippy warnings.

--- massa-network-worker/src/network_worker.rs
+++ massa-network-worker/src/network_worker.rs
@@ -374,12 +374,10 @@ impl NetworkWorker {
                         });
 
                         // Note connection alive.
-                        let (ip, _) =
-                            self.active_connections
-                                .get(&new_connection_id)
-                                .ok_or_else(|| {
-                                    NetworkError::ActiveConnectionMissing(new_connection_id)
-                                })?;
+                        let (ip, _) = self
+                            .active_connections
+                            .get(&new_connection_id)
+                            .ok_or(NetworkError::ActiveConnectionMissing(new_connection_id))?;
                         self.peer_info_db.peer_alive(ip)?;
 
                         // spawn node_controller_fn
@@ -460,7 +458,7 @@ impl NetworkWorker {
         let (ip, is_outgoing) = self
             .active_connections
             .remove(&id)
-            .ok_or_else(|| NetworkError::ActiveConnectionMissing(id))?;
+            .ok_or(NetworkError::ActiveConnectionMissing(id))?;
         debug!(
             "connection closed connection_id={}, ip={}, reason={:?}",
             id, ip, reason
