7bd0efcabdb4f2887b866dae63a148b0cb4d7e1b
sydhds <sydhds@massa.net>
2022-10-18 17:10:03 +0200

    Add block header denunciation factory unit test

--- massa-factory-worker/src/tests/scenarios.rs
+++ massa-factory-worker/src/tests/scenarios.rs
@@ -65,11 +65,13 @@ fn basic_creation_with_multiple_operations() {
 
 use serial_test::serial;
 use massa_hash::Hash;
-use massa_models::block::BlockId;
+use massa_models::block::{BlockHeader, BlockHeaderSerializer, BlockId};
+use massa_models::config::THREAD_COUNT;
 use massa_models::denunciation_interest::DenunciationInterest;
-use massa_models::endorsement::{Endorsement, EndorsementHasher, EndorsementSerializer, WrappedEndorsement};
+use massa_models::endorsement::{Endorsement, EndorsementHasher, EndorsementSerializer, EndorsementSerializerLW, WrappedEndorsement};
 use massa_models::denunciation::Denunciation;
 use massa_models::slot::Slot;
+use massa_models::wrapped::Wrapped;
 
 /// Send 2 wrapped endorsements and check if a Denunciation op is in storage
 #[test]
@@ -123,6 +125,103 @@ fn test_denunciation_factory_endorsement_denunciation() {
         OperationType::Denunciation { data: de } => {
             assert!(de.is_valid());
             assert_eq!(&Denunciation::from((&wrapped_endorsement1, &wrapped_endorsement2)), de);
+        }
+        _ => {
+            panic!("Should be a Denunciation op and not: {:?}", op.content.op);
+        }
+    }
+
+    // TODO: Testnet 17: Check Pool & Propagation
+
+    // std::thread::sleep(Duration::from_secs(1));
+
+    drop(op_ids_); // release RwLockReadGuard
+    // stop everything
+    drop(test_factory);
+}
+
+/// Send block header and check if a Denunciation op is in storage
+#[test]
+#[serial]
+fn test_denunciation_factory_block_header_denunciation() {
+
+    let keypair = KeyPair::generate();
+
+    let slot = Slot::new(2, 1);
+    let parents: Vec<BlockId> = (0..THREAD_COUNT)
+        .map(|i| BlockId(Hash::compute_from(&[i])))
+        .collect();
+
+    let parents2: Vec<BlockId> = (0..THREAD_COUNT)
+        .map(|i| BlockId(Hash::compute_from(&[i+1])))
+        .collect();
+
+    let header1 = BlockHeader {
+        slot,
+        parents: parents.clone(),
+        operation_merkle_root: Hash::compute_from("mno".as_bytes()),
+        endorsements: vec![
+            Endorsement::new_wrapped(
+                Endorsement {
+                    slot: Slot::new(1, 1),
+                    index: 1,
+                    endorsed_block: BlockId(Hash::compute_from("blk1".as_bytes())),
+                },
+                EndorsementSerializerLW::new(),
+                &keypair,
+            )
+                .unwrap(),
+        ],
+    };
+
+    let wrapped_header1: Wrapped<BlockHeader, BlockId> = BlockHeader::new_wrapped(
+        header1,
+        BlockHeaderSerializer::new(),
+        &keypair
+    ).unwrap();
+
+    let header2 = BlockHeader {
+        slot,
+        parents: parents2,
+        operation_merkle_root: Hash::compute_from("mno".as_bytes()),
+        endorsements: vec![
+            Endorsement::new_wrapped(
+                Endorsement {
+                    slot: Slot::new(1, 1),
+                    index: 1,
+                    endorsed_block: BlockId(Hash::compute_from("blk1".as_bytes())),
+                },
+                EndorsementSerializerLW::new(),
+                &keypair,
+            )
+                .unwrap(),
+        ],
+    };
+
+    let wrapped_header2: Wrapped<BlockHeader, BlockId> = BlockHeader::new_wrapped(
+        header2,
+        BlockHeaderSerializer::new(),
+        &keypair
+    ).unwrap();
+
+    let test_factory = TestFactory::new(&keypair);
+
+    test_factory.de_items_tx.send(DenunciationInterest::WrappedHeader(wrapped_header1.clone())).unwrap();
+    test_factory.de_items_tx.send(DenunciationInterest::WrappedHeader(wrapped_header2.clone())).unwrap();
+
+    // Wait for denunciation factory to create the Denunciation
+    std::thread::sleep(Duration::from_secs(1));
+
+    // Get Operation from storage
+    let op_ids_ = test_factory.storage.read_operations();
+    let op_ids = op_ids_.get_operations();
+    assert_eq!(op_ids.len(), 1);
+    let op = op_ids.values().next().unwrap();
+
+    match &op.content.op {
+        OperationType::Denunciation { data: de } => {
+            assert!(de.is_valid());
+            assert_eq!(&Denunciation::from((&wrapped_header1, &wrapped_header2)), de);
         }
         _ => {
             panic!("Should be a Denunciation op and not: {:?}", op.content.op);
