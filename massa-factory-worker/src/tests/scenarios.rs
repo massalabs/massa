7629f92f013f57b3af4af13faf58fc1b8666d643
sydhds <sydhds@massa.net>
2022-10-18 12:09:54 +0200

    Add initial denunciation factory unit test

--- massa-factory-worker/src/tests/scenarios.rs
+++ massa-factory-worker/src/tests/scenarios.rs
@@ -61,3 +61,75 @@ fn basic_creation_with_multiple_operations() {
     }
     assert_eq!(block.content.operations.len(), 2);
 }
+
+use serial_test::serial;
+use massa_hash::Hash;
+use massa_models::block::BlockId;
+use massa_models::denunciation_interest::DenunciationInterest;
+use massa_models::endorsement::{Endorsement, EndorsementHasher, EndorsementSerializer, WrappedEndorsement};
+use massa_models::denunciation::Denunciation;
+use massa_models::operation::WrappedOperation;
+use massa_models::slot::Slot;
+
+///
+#[test]
+#[serial]
+fn test_denunciation_factory_endorsement_denunciation() {
+    let keypair = KeyPair::generate();
+    let mut test_factory = TestFactory::new(&keypair);
+
+    let sender_keypair = KeyPair::generate();
+
+    let slot = Slot::new(3, 7);
+    let endorsement1 = Endorsement {
+        slot,
+        index: 0,
+        endorsed_block: BlockId(Hash::compute_from("blk".as_bytes())),
+    };
+    let wrapped_endorsement1: WrappedEndorsement = Endorsement::new_wrapped_with_hasher(
+        endorsement1,
+        EndorsementSerializer::new(),
+        &sender_keypair,
+        EndorsementHasher::new(),
+    )
+        .unwrap();
+
+    let endorsement2 = Endorsement {
+        slot,
+        index: 0,
+        endorsed_block: BlockId(Hash::compute_from("blk2".as_bytes())),
+    };
+    let wrapped_endorsement2: WrappedEndorsement = Endorsement::new_wrapped_with_hasher(
+        endorsement2,
+        EndorsementSerializer::new(),
+        &sender_keypair,
+        EndorsementHasher::new(),
+    )
+        .unwrap();
+
+    test_factory.de_items_tx.send(DenunciationInterest::WrappedEndorsement(wrapped_endorsement1.clone())).unwrap();
+    test_factory.de_items_tx.send(DenunciationInterest::WrappedEndorsement(wrapped_endorsement2.clone())).unwrap();
+
+    test_factory.wait_until_next_slot();
+
+    // Get Operation from storage
+    let op_ids_ = test_factory.storage.read_operations();
+    let op_ids = op_ids_.get_operations();
+    assert_eq!(op_ids.len(), 1);
+    let op = op_ids.values().next().unwrap();
+
+    match &op.content.op {
+        OperationType::Denunciation { data: de } => {
+            assert!(de.is_valid());
+            assert_eq!(&Denunciation::from((&wrapped_endorsement1, &wrapped_endorsement2)), de);
+        }
+        _ => {
+            panic!("Should be a Denunciation op and not: {:?}", op.content.op);
+        }
+    }
+
+    drop(op_ids_); // release RwLockReadGuard
+    // stop everything
+    drop(test_factory);
+
+}
