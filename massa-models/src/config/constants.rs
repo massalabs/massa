54df925a48f2692691937fbb2d27cfc70c5dd1ba
AurelienFT <aurelien.foucault@epitech.eu>
2022-11-30 15:14:29 +0100

    Change version for testnet 18

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -64,7 +64,7 @@ lazy_static::lazy_static! {
         if cfg!(feature = "sandbox") {
             "SAND.0.0"
         } else {
-            "TEST.17.0"
+            "TEST.18.0"
         }
         .parse()
         .unwrap()
