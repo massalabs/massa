5c93e24caff5ffded38f787f2d21e1ed4a2992ab
Moncef AOUDIA <ma@massa.net>
2022-12-01 16:59:25 +0100

    feat: display human time
    resolves #3286

--- massa-bootstrap/src/server.rs
+++ massa-bootstrap/src/server.rs
@@ -1,5 +1,6 @@
 use futures::stream::FuturesUnordered;
 use futures::StreamExt;
+use humantime::format_duration;
 use massa_async_pool::AsyncMessageId;
 use massa_consensus_exports::{bootstrapable_graph::BootstrapableGraph, ConsensusController};
 use massa_final_state::FinalState;
@@ -207,7 +208,7 @@ impl BootstrapServer {
                             self.ip_hist_map.retain(|_k, v| now.duration_since(*v) <= per_ip_min_interval);
                             if self.ip_hist_map.len() > self.bootstrap_config.ip_list_max_size {
                                 // too many IPs are spamming us: clear cache
-                                warn!("high bootstrap load: at least {} different IPs attempted bootstrap in the last {}ms", self.ip_hist_map.len(), self.bootstrap_config.per_ip_min_interval);
+                                warn!("high bootstrap load: at least {} different IPs attempted bootstrap in the last {}", self.ip_hist_map.len(),format_duration(self.bootstrap_config.per_ip_min_interval.to_duration()).to_string());
                                 self.ip_hist_map.clear();
                             }
                         }
@@ -219,7 +220,7 @@ impl BootstrapServer {
                                     let mut server = BootstrapServerBinder::new(dplx, self.keypair.clone(), self.bootstrap_config.max_bytes_read_write, self.bootstrap_config.max_bootstrap_message_size, self.bootstrap_config.thread_count, self.bootstrap_config.max_datastore_key_length, self.bootstrap_config.randomness_size_bytes, self.bootstrap_config.consensus_bootstrap_part_size);
                                     let _ = match tokio::time::timeout(self.bootstrap_config.write_error_timeout.into(), server.send(BootstrapServerMessage::BootstrapError {
                                         error:
-                                        format!("Your last bootstrap on this server was {:#?} ago and you have to wait {:#?} before retrying.", occ.get().elapsed().as_secs(), per_ip_min_interval.saturating_sub(occ.get().elapsed()).as_secs())
+                                        format!("Your last bootstrap on this server was {} ago and you have to wait before.", format_duration(per_ip_min_interval.saturating_sub(occ.get().elapsed())).to_string())
                                     })).await {
                                         Err(_) => Err(std::io::Error::new(std::io::ErrorKind::TimedOut, "bootstrap error too early retry bootstrap send timed out").into()),
                                         Ok(Err(e)) => Err(e),
