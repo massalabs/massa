e97d217b806b9881d92a6eb4c80df0901a10e13d
sydhds <sydhds@massa.net>
2022-12-06 12:24:17 +0100

    Add DatastorePy and OperationPy python class

---
+++ massa-sdk-py/Readme.md
@@ -0,0 +1,35 @@
+## Setup
+
+python3 -m venv venv
+venv/bin/python -m pip install maturin
+
+## Building the sdk
+
+source venv/bin/activate
+maturin develop
+
+## Examples 1: Operation
+
+```python
+
+from massa_sdk_py import OperationPy, OperationTypePy
+op_tr = OperationTypePy.new_transaction([0]*32, 22)
+print(repr(op_tr))
+op1 = OperationPy(7, 33, op_tr)
+print(repr(op1))
+op1_ser = op1.serialize()
+op1_2 = OperationPy.deserialize(op1_ser)
+print(repr(op1_2))
+```
+
+## Examples 2: Datastore
+
+```python
+from massa_sdk_py import DatastorePy
+ds = DatastorePy(); ds.insert([1, 2, 3], [255, 254, 253])
+ds.insert([7, 8, 9], [255, 254, 253])
+ds.insert([77, 88, 99], [25, 24, 23])
+for (k, v) in ds:
+  print(k, v)
+print(repr(ds))
+```
--- massa-sdk-py/src/lib.rs
+++ massa-sdk-py/src/lib.rs
@@ -1,6 +1,16 @@
+// use pyo3::exceptions;
+use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
-use pyo3::types::PyString;
+use massa_models::address::Address;
+use massa_models::amount::Amount;
+use massa_models::datastore::Datastore;
 use massa_models::slot::Slot;
+use massa_models::datastore::DatastoreSerializer;
+use massa_models::operation::{Operation, OperationDeserializer, OperationSerializer, OperationType};
+use massa_models::config::constants;
+use massa_serialization::{DeserializeError, Deserializer, Serializer};
+
+// A custom struct (just for testing purpose)
 
 #[pyclass]
 struct MySlot {
@@ -21,6 +31,8 @@ impl MySlot {
         }
     }
 }
+
+// Slot (first example)
 
 #[pyclass]
 #[derive(Clone)]
@@ -44,47 +56,173 @@ impl SlotPy {
         "SlotPy"
     }
 
-    /*
-    fn __iter__(slf: PyRef<self>) -> PyResult<&PyAny> {
-        PyString::new(slf.py(), "hello, world").iter()
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{}", self.inner))
     }
-    */
+
 }
+
+// Datastore
+
+#[pyclass]
+struct DatastoreIter {
+    inner: std::collections::btree_map::IntoIter<Vec<u8>, Vec<u8>>
+}
+
+#[pymethods]
+impl DatastoreIter {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<(Vec<u8>, Vec<u8>)> {
+        slf.inner.next()
+    }
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct DatastorePy {
+    pub inner: Datastore,
+}
+
+#[pymethods]
+impl DatastorePy {
+
+    #[new]
+    pub fn new() -> Self {
+        Self {
+            inner: Default::default()
+        }
+    }
+
+    pub fn insert(&mut self, key: Vec<u8>, value: Vec<u8>) {
+        self.inner.insert(key, value);
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.inner))
+    }
+
+    fn __iter__(slf: PyRef<'_, Self>) -> PyResult<Py<DatastoreIter>> {
+        let iter = DatastoreIter {
+            inner: slf.inner.clone().into_iter()
+        };
+        Py::new(slf.py(), iter)
+    }
+
+    pub fn serialize(&self) -> PyResult<Vec<u8>> {
+        let serializer = DatastoreSerializer::new();
+        let mut buffer = Vec::new();
+        serializer
+            .serialize(&self.inner, &mut buffer)
+            .map_err(|e|
+                PyRuntimeError::new_err(e.to_string())
+            )?;
+        Ok(buffer)
+    }
+}
+
+// Operation (enough to build an transaction op and serialize / deserialize)
+
+#[pyclass]
+#[derive(Clone)]
+pub struct OperationTypePy {
+    pub inner: OperationType,
+}
+
+#[pymethods]
+impl OperationTypePy {
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.inner))
+    }
+
+    #[staticmethod]
+    fn new_transaction(recipient_address: Vec<u8>, amount: u64) -> Self {
+        // FIXME: here we use Vec<u8> as &[u8; 32] is not easily translated by PyO3
+        //        But at the end, we would have AddressPy avail to deal with this
+        Self {
+            inner: OperationType::Transaction {
+                recipient_address: Address::from_bytes(&recipient_address[..].try_into().unwrap()),
+                amount: Amount::from_raw(amount),
+            }
+        }
+    }
+}
+
+#[pyclass]
+pub struct OperationPy {
+    pub inner: Operation,
+}
+
+#[pymethods]
+impl OperationPy {
+    #[new]
+    pub fn new(fee: u64, expire_period: u64, op_type: OperationTypePy) -> Self {
+        Self {
+            inner: Operation {
+                fee: Amount::from_raw(fee),
+                expire_period,
+                op: op_type.inner
+            }
+        }
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.inner))
+    }
+
+    pub fn serialize(&self) -> PyResult<Vec<u8>> {
+        let serializer = OperationSerializer::new();
+        let mut buffer = Vec::new();
+        serializer
+            .serialize(&self.inner, &mut buffer)
+            .map_err(|e|
+                PyRuntimeError::new_err(e.to_string())
+            )?;
+        Ok(buffer)
+    }
+
+    #[staticmethod]
+    pub fn deserialize(buffer: Vec<u8>) -> PyResult<OperationPy> {
+        let deserializer = OperationDeserializer::new(
+            constants::MAX_DATASTORE_VALUE_LENGTH,
+            constants::MAX_FUNCTION_NAME_LENGTH,
+            constants::MAX_PARAMETERS_SIZE,
+            constants::MAX_OPERATION_DATASTORE_ENTRY_COUNT,
+            constants::MAX_OPERATION_DATASTORE_KEY_LENGTH,
+            constants::MAX_OPERATION_DATASTORE_VALUE_LENGTH);
+
+        let (_bytes_left, operation) = deserializer
+            .deserialize::<DeserializeError>(&buffer)
+            .map_err(|e|
+                PyRuntimeError::new_err(e.to_string())
+            )?;
+        Ok(OperationPy {
+            inner: operation
+        })
+    }
+}
+
 
 
 /*
-#[pyproto]
-impl<'a> PyObjectProtocol for SlotPy {
-    fn __str__(&self) -> PyResult<&'static str> {
-        Ok("SlotPy")
-    }
-
-    fn __repr__(&'a self) -> PyResult<String> {
-        Ok(format!("Slot py: {} {}", self.inner.period, self.inner.thread))
-    }
-
-    fn __getattr__(&'a self, name: &str) -> PyResult<String> {
-        let out: String = match name {
-            "period" => self.inner.period.into(),
-            "thread" => self.inner.thread.into(),
-            &_ => "INVALID FIELD".to_owned(),
-        };
-        Ok(out)
-    }
-}
-*/
-
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
+*/
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn massa_sdk_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_class::<MySlot>()?;
+    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    // m.add_class::<MySlot>()?;
     m.add_class::<SlotPy>()?;
+    m.add_class::<DatastorePy>()?;
+    m.add_class::<OperationPy>()?;
+    m.add_class::<OperationTypePy>()?;
     Ok(())
 }
