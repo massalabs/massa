4e4e762d7a14ea9b9d2e9b6f20e74425e36c1438
Moncef AOUDIA <ma@massa.net>
2022-12-23 11:18:47 +0100

    fix: wrong testnet version

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -64,7 +64,7 @@ lazy_static::lazy_static! {
         if cfg!(feature = "sandbox") {
             "SAND.0.0"
         } else {
-            "TEST.17.2"
+            "TEST.18.0"
         }
         .parse()
         .unwrap()
