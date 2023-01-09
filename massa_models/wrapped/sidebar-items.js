6020f5ef6f429432420238840cd4f157c3c972a4
Ben-PH <Ben-PH@users.noreply.github.com>
2023-01-09 11:40:45 +0000

    Deploying to gh-pages from @ massalabs/massa@52540ce0e1254bbe1031178c9dbb37a37929f07d 🚀

--- massa_models/wrapped/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":[["Wrapped","Wrapped structure T where U is the associated id"],["WrappedDeserializer","Deserializer for Wrapped structure"],["WrappedSerializer","Serializer for `Wrapped` structure"]],"trait":[["Id","Used by signed structure"],["WrappedContent","Trait that define a structure that can be wrapped."]]};
