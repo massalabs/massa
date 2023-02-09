8a55ecf463bd4ca2a4de2728a1508f5a331fe340
AurelienFT <aurelien.foucault@epitech.eu>
2023-02-09 10:01:54 +0100

    Update new timestamp

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -45,7 +45,7 @@ lazy_static::lazy_static! {
                 .saturating_add(MassaTime::from_millis(1000 * 10))
         )
     } else {
-        1675346400000.into()  // Thursday, February 02, 2022 14:00:00 AM UTC
+        1675951200000.into()  // Thursday, February 09, 2022 02:00:00 PM UTC
     };
 
     /// TESTNET: time when the blockclique is ended.
@@ -62,9 +62,9 @@ lazy_static::lazy_static! {
     /// node version
     pub static ref VERSION: Version = {
         if cfg!(feature = "sandbox") {
-            "SAND.20.0"
+            "SAND.0.0"
         } else {
-            "TEST.20.0"
+            "TEST.19.2"
         }
         .parse()
         .unwrap()
