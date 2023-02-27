77c56e0a74def168ceafc46dd738cda1dca1cec7
Ben PHL <benphawke@gmail.com>
2023-02-27 16:47:29 +0100

    Make establisher `fn get_connector` non-async

--- massa-bootstrap/src/establisher.rs
+++ massa-bootstrap/src/establisher.rs
@@ -103,7 +103,7 @@ pub mod types {
         ///
         /// # Argument
         /// * `timeout_duration`: timeout duration in milliseconds
-        pub async fn get_connector(
+        pub fn get_connector(
             &mut self,
             timeout_duration: MassaTime,
         ) -> io::Result<DefaultConnector> {
