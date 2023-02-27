b77875c3e65510669469189bd64b8e862273e719
Ben PHL <benphawke@gmail.com>
2023-02-27 19:33:05 +0100

    Set the mockers to use a TcpStream as a duplex

--- massa-bootstrap/src/tests/mock_establisher.rs
+++ massa-bootstrap/src/tests/mock_establisher.rs
@@ -1,15 +1,14 @@
 // Copyright (c) 2022 MASSA LABS <info@massa.net>
 
-use massa_models::config::{CHANNEL_SIZE, MAX_DUPLEX_BUFFER_SIZE};
+use massa_models::config::CHANNEL_SIZE;
 use massa_time::MassaTime;
 use socket2 as _;
 use std::io;
 use std::net::SocketAddr;
-use tokio::io::DuplexStream;
 use tokio::sync::{mpsc, oneshot};
 use tokio::time::timeout;
 
-pub type Duplex = DuplexStream;
+pub type Duplex = tokio::net::TcpStream;
 
 pub fn new() -> (MockEstablisher, MockEstablisherInterface) {
     let (connection_listener_tx, connection_listener_rx) =
@@ -32,18 +31,24 @@ pub fn new() -> (MockEstablisher, MockEstablisherInterface) {
 
 #[derive(Debug)]
 pub struct MockListener {
-    connection_listener_rx: crossbeam::channel::Receiver<(SocketAddr, oneshot::Sender<Duplex>)>, // (controller, mock)
+    connection_listener_rx:
+        crossbeam::channel::Receiver<(SocketAddr, oneshot::Sender<tokio::net::TcpStream>)>, // (controller, mock)
 }
 
 impl MockListener {
-    pub async fn accept(&mut self) -> std::io::Result<(Duplex, SocketAddr)> {
-        let (addr, sender) = self.connection_listener_rx.recv().map_err(|_| {
+    pub async fn accept(&mut self) -> std::io::Result<(tokio::net::TcpStream, SocketAddr)> {
+        let (_addr, sender) = self.connection_listener_rx.recv().map_err(|_| {
             io::Error::new(
                 io::ErrorKind::Other,
                 "MockListener accept channel from Establisher closed".to_string(),
             )
         })?;
-        let (duplex_controller, duplex_mock) = tokio::io::duplex(MAX_DUPLEX_BUFFER_SIZE);
+        let duplex_controller = tokio::net::TcpListener::bind("localhost:0").await.unwrap();
+        let duplex_mock = tokio::net::TcpStream::connect(duplex_controller.local_addr().unwrap())
+            .await
+            .unwrap();
+        let duplex_controller = duplex_controller.accept().await.unwrap();
+
         sender.send(duplex_mock).map_err(|_| {
             io::Error::new(
                 io::ErrorKind::Other,
@@ -51,27 +56,32 @@ impl MockListener {
             )
         })?;
 
-        Ok((duplex_controller, addr))
+        Ok(duplex_controller)
     }
 }
 
 #[derive(Debug)]
 pub struct MockConnector {
-    connection_connector_tx: mpsc::Sender<(Duplex, SocketAddr, oneshot::Sender<bool>)>,
+    connection_connector_tx:
+        mpsc::Sender<(tokio::net::TcpStream, SocketAddr, oneshot::Sender<bool>)>,
     timeout_duration: MassaTime,
 }
 
 impl MockConnector {
-    pub async fn connect(&mut self, addr: SocketAddr) -> std::io::Result<Duplex> {
-        // task the controller connection if exist.
-        let (duplex_controller, duplex_mock) = tokio::io::duplex(MAX_DUPLEX_BUFFER_SIZE);
+    pub async fn connect(&mut self, addr: SocketAddr) -> std::io::Result<tokio::net::TcpStream> {
+        let duplex_mock = tokio::net::TcpListener::bind(addr).await.unwrap();
+        let duplex_controller = tokio::net::TcpStream::connect(addr).await.unwrap();
+        let duplex_mock = duplex_mock.accept().await.unwrap();
+
+        // // task the controller connection if exist.
+        // let (duplex_controller, duplex_mock) = tokio::io::duplex(MAX_DUPLEX_BUFFER_SIZE);
         // to see if the connection is accepted
         let (accept_tx, accept_rx) = oneshot::channel::<bool>();
 
         // send new connection to mock
         timeout(self.timeout_duration.to_duration(), async move {
             self.connection_connector_tx
-                .send((duplex_mock, addr, accept_tx))
+                .send((duplex_mock.0, addr, accept_tx))
                 .await
                 .map_err(|_err| {
                     io::Error::new(
