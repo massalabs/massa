d11d35fc384dbf30d37f35e5f07ae4410de5e74b
Thomas Plisson <thomas.plisson@epitech.eu>
2023-03-03 11:30:14 +0100

    Add upcomming ModuleInfo type

--- massa-sc-module-cache/src/types.rs
+++ massa-sc-module-cache/src/types.rs
@@ -1,3 +1,10 @@
 use massa_sc_runtime::RuntimeModule;
 
 pub type ModuleInfo = (RuntimeModule, Option<u64>);
+
+/// NOTE: this will replace ModuleInfo
+pub enum ModuleInfoBis {
+    Invalid,
+    Module(RuntimeModule),
+    ModuleAndDelta((RuntimeModule, u64)),
+}
