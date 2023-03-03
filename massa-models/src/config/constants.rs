5afb39c45e4fb03cbf2b4907c49464fa8ea85bd3
AurelienFT <aurelien.foucault@epitech.eu>
2023-03-03 15:51:34 +0100

    Revert "Prepare testnet21 branch."
    This reverts commit f3e0b7fa382499e16fac0a9987d9dfbd2933e201.

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -64,7 +64,7 @@ lazy_static::lazy_static! {
         if cfg!(feature = "sandbox") {
             "SAND.20.0"
         } else {
-            "TEST.21.0"
+            "TEST.20.0"
         }
         .parse()
         .unwrap()
