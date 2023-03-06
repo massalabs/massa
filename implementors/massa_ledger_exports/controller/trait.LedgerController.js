1194bc91757d907ff4d7b269110f807c0cc73ca9
modship <modship@users.noreply.github.com>
2023-03-06 21:39:33 +0000

    Deploying to gh-pages from @ massalabs/massa@d2c862921e0460313d6d4ef4e3ff3146a65b150c 🚀

--- implementors/massa_ledger_exports/controller/trait.LedgerController.js
+++ implementors/massa_ledger_exports/controller/trait.LedgerController.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_ledger_worker":[["impl <a class=\"trait\" href=\"massa_ledger_exports/controller/trait.LedgerController.html\" title=\"trait massa_ledger_exports::controller::LedgerController\">LedgerController</a> for <a class=\"struct\" href=\"massa_ledger_worker/struct.FinalLedger.html\" title=\"struct massa_ledger_worker::FinalLedger\">FinalLedger</a>"]]
+"massa_ledger_worker":[["impl LedgerController for <a class=\"struct\" href=\"massa_ledger_worker/struct.FinalLedger.html\" title=\"struct massa_ledger_worker::FinalLedger\">FinalLedger</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
