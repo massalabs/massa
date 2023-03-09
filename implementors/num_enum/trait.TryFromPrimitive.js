f4f9d47f8411e057d4c1af47d4258312a80e888d
sydhds <sydhds@users.noreply.github.com>
2023-03-09 09:32:23 +0000

    Deploying to gh-pages from @ massalabs/massa@acae69aa7c285e79a2995ada13aae4de042877b4 🚀

---
+++ implementors/num_enum/trait.TryFromPrimitive.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_versioning_worker":[["impl TryFromPrimitive for <a class=\"enum\" href=\"massa_versioning_worker/versioning/enum.ComponentStateTypeId.html\" title=\"enum massa_versioning_worker::versioning::ComponentStateTypeId\">ComponentStateTypeId</a>"],["impl TryFromPrimitive for <a class=\"enum\" href=\"massa_versioning_worker/versioning/enum.MipComponent.html\" title=\"enum massa_versioning_worker::versioning::MipComponent\">MipComponent</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
