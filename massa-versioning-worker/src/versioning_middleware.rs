55e8207ec33629b6e2738ddf0e6059b7f82f7d97
sydhds <sydhds@gmail.com>
2023-03-21 10:43:17 +0100

    Fix versioning middleware unit test

--- massa-versioning-worker/src/versioning_middleware.rs
+++ massa-versioning-worker/src/versioning_middleware.rs
@@ -68,7 +68,6 @@ impl VersioningMiddleware {
             };
 
             state.on_advance(&advance_msg.clone());
-
             debug!("New state: vi: {:?}, state: {:?}", vi, state);
         }
 
@@ -78,10 +77,29 @@ impl VersioningMiddleware {
 
 #[cfg(test)]
 mod test {
-    use crate::versioning::{MipComponent, MipInfo, MipState};
-    use core::time::Duration;
-
     use super::*;
+
+    use core::time::Duration;
+    use std::str::FromStr;
+
+    use crate::versioning::{ComponentState, MipComponent, MipInfo, MipState, MipStoreRaw};
+
+
+    // test only
+    impl MipStoreRaw {
+        /// A helper function to query MipStoreRaw
+        fn get_state_for(&self, mi: &MipInfo) -> Option<&MipState> {
+            self.0.get(mi)
+        }
+    }
+
+    impl MipStore {
+        /// A helper function to query MipStore
+        fn get_state_for(&self, mi: &MipInfo) -> MipState {
+            let store = self.0.read();
+            store.get_state_for(mi).unwrap().clone()
+        }
+    }
 
     fn get_a_version_info(start: MassaTime, timeout: MassaTime) -> MipInfo {
         // A helper function to provide a  default MipInfo
@@ -112,45 +130,58 @@ mod test {
         let mut vm = VersioningMiddleware::new(5, vs.clone());
 
         // The MIP-0001 hasn't Started yet, so we do not announce it.
+        assert_eq!(vs.get_state_for(&vi).inner, ComponentState::defined());
         assert_eq!(vs.get_network_version_current(), 0);
         assert_eq!(vs.get_network_version_to_announce(), 0);
 
         vm.new_block(0);
+        // Need to wait a least 1ms between each block otherwise MipState filtered the advance msg
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(0);
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(0);
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(0);
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(0);
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(0);
 
         // The MIP-0001 hasn't Started yet, so we do not announce it.
+        assert_eq!(vs.get_state_for(&vi).inner, ComponentState::defined());
         assert_eq!(vs.get_network_version_current(), 0);
         assert_eq!(vs.get_network_version_to_announce(), 0);
 
-        tokio::time::sleep(Duration::from_secs(3)).await;
+        tokio::time::sleep(Duration::from_secs(2)).await;
 
         vm.new_block(0);
 
         // The MIP-0001 has Started, we start announcing it.
+        assert_eq!(vs.get_state_for(&vi).inner, ComponentState::started(Amount::zero()));
         assert_eq!(vs.get_network_version_current(), 0);
         assert_eq!(vs.get_network_version_to_announce(), 1);
 
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(1);
+        assert_eq!(vs.get_state_for(&vi).inner, ComponentState::started(Amount::from_str("20.0").unwrap()));
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(1);
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(1);
+        tokio::time::sleep(Duration::from_millis(1)).await;
         vm.new_block(1);
-        vm.new_block(1);
+        tokio::time::sleep(Duration::from_millis(1)).await;
 
         // Enough people voted for MIP-0001, but Timeout has not occurred, so we still announce it and don't consider it active
+        assert_eq!(vs.get_state_for(&vi).inner, ComponentState::locked_in());
         assert_eq!(vs.get_network_version_current(), 0);
         assert_eq!(vs.get_network_version_to_announce(), 1);
 
-        tokio::time::sleep(Duration::from_secs(2)).await;
-
-        vm.new_block(1);
-        vm.new_block(1);
+        tokio::time::sleep(Duration::from_millis(2200)).await;
         vm.new_block(1);
 
         // Timeout occurred, so we don't announce MIP-0001 anymore, and consider it active
+        assert_eq!(vs.get_state_for(&vi).inner, ComponentState::active());
         assert_eq!(vs.get_network_version_current(), 1);
         assert_eq!(vs.get_network_version_to_announce(), 0);
     }
