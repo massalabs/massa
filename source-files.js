0bb1d005d2d9b8634533a60ae34ddea9857b1979
Ben-PH <Ben-PH@users.noreply.github.com>
2023-03-22 14:28:25 +0000

    Deploying to gh-pages from @ massalabs/massa@e2289d79add82f1a9388df47d3f4acaf0be5718e 🚀

--- source-files.js
+++ source-files.js
@@ -4,7 +4,7 @@ var sourcesIndex = JSON.parse('{\
 "massa_async_pool":["",[],["changes.rs","config.rs","lib.rs","message.rs","pool.rs"]],\
 "massa_bootstrap":["",[["server",[],["white_black_list.rs"]]],["client.rs","client_binder.rs","error.rs","establisher.rs","lib.rs","messages.rs","server.rs","server_binder.rs","settings.rs","tools.rs"]],\
 "massa_cipher":["",[],["constants.rs","decrypt.rs","encrypt.rs","error.rs","lib.rs"]],\
-"massa_client":["",[],["cmds.rs","display.rs","main.rs","repl.rs","settings.rs"]],\
+"massa_client":["",[],["cmds.rs","main.rs","repl.rs","settings.rs"]],\
 "massa_consensus_exports":["",[],["block_graph_export.rs","block_status.rs","bootstrapable_graph.rs","channels.rs","controller_trait.rs","error.rs","events.rs","export_active_block.rs","lib.rs","settings.rs"]],\
 "massa_consensus_worker":["",[["state",[],["graph.rs","mod.rs","process.rs","process_commands.rs","prune.rs","stats.rs","tick.rs","verifications.rs"]],["worker",[],["init.rs","main_loop.rs","mod.rs"]]],["commands.rs","controller.rs","lib.rs","manager.rs"]],\
 "massa_executed_ops":["",[],["config.rs","executed_ops.rs","lib.rs","ops_changes.rs"]],\
