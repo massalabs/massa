d0dcc5f9a90e50f06c46988c1ce78dbcc0c78e51
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2023-03-23 11:18:19 +0000

    Deploying to gh-pages from @ massalabs/massa@5c39ee1f11baa1869f53a288fc8291b87d8f3fd6 🚀

--- massa_models/denunciation/sidebar-items.js
+++ massa_models/denunciation/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DenunciationError","DenunciationTypeId"],"struct":["BlockHeaderDenunciationDeserializer","BlockHeaderDenunciationSerializer","DenunciationDeserializer","DenunciationSerializer","EndorsementDenunciationDeserializer","EndorsementDenunciationSerializer"]};
+window.SIDEBAR_ITEMS = {"enum":["DenunciationError"]};
