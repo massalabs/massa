42709da17c6f5b5c9bc41823bf3415008218fe75
Moncef AOUDIA <ma@massa.net>
2023-03-24 15:36:31 +0100

    refactor: new blocks

--- massa-grpc/src/stream/new_blocks.rs
+++ massa-grpc/src/stream/new_blocks.rs
@@ -1,14 +1,15 @@
-use crate::error::GrpcError;
+use crate::error::{GrpcError, match_for_io_error};
 use crate::service::MassaGrpcService;
 use futures_util::StreamExt;
 use massa_models::block::SecureShareBlock;
 use massa_proto::massa::api::v1::{self as grpc};
 use massa_proto::massa::api::v1::{NewBlocksStreamRequest, NewBlocksStreamResponse};
+use std::io::ErrorKind;
 use std::pin::Pin;
 use tokio::select;
 use tonic::codegen::futures_core;
 use tonic::{Request, Streaming};
-use tracing::log::error;
+use tracing::log::{error, warn};
 
 /// type declaration for NewBlocksStream
 pub type NewBlocksStream = Pin<
@@ -28,56 +29,60 @@ pub(crate) async fn new_blocks(
     let mut subscriber = grpc.consensus_channels.block_sender.subscribe();
 
     tokio::spawn(async move {
-        // let mut request_id = request.id;
+            let mut request_id = String::new();
+            loop {
+                select! {
+                     event = subscriber.recv() => {
+                        match event {
+                            Ok(share_block) => {
+                                let massa_block = share_block as SecureShareBlock;
+                                let header = massa_block.content.header.into();
+                                let operations = massa_block.content.operations.into_iter().map(|operation| operation.to_string()).collect();
 
-        // todo request id
-
-        loop {
-            select! {
-                 event = subscriber.recv() => {
-
-                    match event {
-                        Ok(share_block) => {
-                            let massa_block = share_block as SecureShareBlock;
-
-                            let header = massa_block.content.header.into();
-                            let operations = massa_block.content.operations.into_iter().map(|operation| operation.to_string()).collect();
-
-                            if let Err(e) = tx.send(Ok(NewBlocksStreamResponse {
-                                    id: "todo".to_string(),
-                                    block: Some(grpc::Block {
-                                        header: Some(header),
-                                        operations
-                                    })
-                            })).await {
-                                error!("failed to send operation : {}", e);
-                                break;
-                            }
-                        },
-                        Err(e) => {error!("error on receive block : {}", e)}
-                    }
-                },
-                res = in_stream.next() => {
-                    match res {
-                        Some(res) => {
-                            match res {
-                                Ok(_data) => {
-                                    // nothing to do
-                                },
-                                Err(e) => {
-                                    error!("{}", e);
+                                if let Err(e) = tx.send(Ok(NewBlocksStreamResponse {
+                                        id: request_id.clone(),
+                                        block: Some(grpc::Block {
+                                            header: Some(header),
+                                            operations
+                                        })
+                                })).await {
+                                    error!("failed to send new block : {}", e);
                                     break;
                                 }
-                            }
-                        },
-                        None => {
-                            // client disconnected
-                            break;
-                        },
+                            },
+                            Err(e) => {error!("error on receive new block : {}", e)}
+                        }
+                    },
+                    res = in_stream.next() => {
+                        match res {
+                            Some(res) => {
+                                match res {
+                                    Ok(data) => {
+                                        request_id = data.id
+                                    },
+                                    Err(err) => {
+                                        if let Some(io_err) = match_for_io_error(&err) {
+                                            if io_err.kind() == ErrorKind::BrokenPipe {
+                                                warn!("client disconnected, broken pipe: {}", io_err);
+                                                break;
+                                            }
+                                        }
+                                        error!("{}", err);
+                                        if let Err(e) = tx.send(Err(err)).await {
+                                            error!("failed to send back new_blocks error response: {}", e);
+                                            break;
+                                        }
+                                    }
+                                }
+                            },
+                            None => {
+                                // client disconnected
+                                break;
+                            },
+                        }
                     }
                 }
             }
-        }
     });
 
     let out_stream = tokio_stream::wrappers::ReceiverStream::new(rx);
