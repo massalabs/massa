6b08bc8947751ff7d1cd49c92b07cda609aa2e5c
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2023-03-27 14:10:39 +0000

    Deploying to gh-pages from @ massalabs/massa@02fc5c94f3ad1756ea1d716b00dc0101b5e5d3bf 🚀

--- massa_versioning_exports/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["VersioningCommand","VersioningManagementCommand","VersioningMiddlewareError"],"struct":["VersioningCommandSender","VersioningConfig","VersioningManager","VersioningReceivers","VersioningSenders"]};
