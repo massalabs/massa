f920f59aec7cfca16c6fd83d433242bf3b2ecafc
aoudiamoncef <aoudiamoncef@users.noreply.github.com>
2023-03-28 10:07:00 +0000

    Deploying to gh-pages from @ massalabs/massa@d24a2116cddfae5ae713eaa3ccc13d2178fee2a0 🚀

---
+++ implementors/massa_proto/massa/api/v1/grpc_server/trait.Grpc.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_grpc":[["impl Grpc for <a class=\"struct\" href=\"massa_grpc/service/struct.MassaGrpcService.html\" title=\"struct massa_grpc::service::MassaGrpcService\">MassaGrpcService</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
