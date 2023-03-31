8c15257a70620568f5df795142bbe04f696fc4cd
modship <modship@users.noreply.github.com>
2023-03-31 08:39:18 +0000

    Deploying to gh-pages from @ massalabs/massa@2b523c82fbccf0a236adb7491b3914351f4c6130 🚀

---
+++ implementors/massa_proto/massa/api/v1/grpc_server/trait.Grpc.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_grpc":[["impl Grpc for <a class=\"struct\" href=\"massa_grpc/server/struct.MassaGrpc.html\" title=\"struct massa_grpc::server::MassaGrpc\">MassaGrpc</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
