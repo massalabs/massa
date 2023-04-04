60a0180d9fcc82622ac36aa0a6f31651cdcf14ef
Leo-Besancon <leoleo38@live.fr>
2023-04-04 15:54:52 +0200

    fixed stats feature flags

--- massa-consensus-worker/src/state/stats.rs
+++ massa-consensus-worker/src/state/stats.rs
@@ -49,6 +49,8 @@ impl ConsensusState {
         Ok(())
     }
 
+
+    #[cfg(not(feature = "sandbox"))]
     /// Helper function for stats_tick. Checks if there are any final blocks is coming from protocol
     /// if none => we are probably desync
     /// Ignore if we are before the last_start_period
