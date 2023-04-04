2dbe31f9a9d0bbda7e7018abf8cb729f546571d3
Eitu33 <89928840+Eitu33@users.noreply.github.com>
2023-04-04 11:25:24 +0200

    Module cache v2 (#3725)
    * Create massa_sc_module_cache
    
    * Change ModuleCache documentation and add HDCache placeholder functions
    
    * Create LRUCache and implement load_module
    
    * Impl save_module_from_bytecode and correct mistakes
    
    * Separate init costs handling and update save_module_from_bytecode
    
    * Add remove_module
    
    * Use latest runtime changes, fix behaviours and add comments
    
    * Add HDCache
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * Fix internal api
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * Upate init_costs handling and add ModuleInfo
    
    * Fix missing occurences of ModuleInfo
    
    * temporary cache usage update on execution, not expected behaviour
    
    * fixup! Add HDCache
    
    * REMOVE ME change dep for massa-sc-runtime
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * update load_module, use set_init_cost, add update_module
    
    * Minor changes
    
    * fix get()
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * Revert "REMOVE ME change dep for massa-sc-runtime"
    
    This reverts commit 76276e38ce2070567e591eec4d29d6f1c6683da0.
    
    * Make insert en remove atomic, fix internal interface, make tests pass
    
    /!\ tests are ok if run 1 by 1 but fail if run in parallel
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * Add upcomming ModuleInfo type
    
    * Limit number of entries in the db, adjust API
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * Select entries to remove randomly
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * ModuleInfo NOM Ser & Deser
    
    * Wip transition to ModuleInfoBis
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * No SP cache implementation
    
    * Use latest impl in execution
    
    * Intermediate commit for hd cache update
    
    * Cache update on settle slot
    
    * max block gas as first input limit for compilation
    
    * Update keys format in hd cache, metadata value serializers
    
    * Rename metadata fields
    
    * Rename controller functions
    
    * Re impl set_init_cost and remove cf usage
    
    * Impl get
    
    * Update save_module, implement set_invalid, change lru error handling
    
    * Update fix impl details and start changing error handling
    
    * Update error handling
    
    * Rename cache module
    
    * Update hd cache get behaviour
    
    * Update load_module behaviour
    
    * test update
    
    * Hd cache tests update
    
    * Add test_missing_module
    
    * Fix hd cache get behaviour
    
    * Fix invalid module deser error
    
    * Cache config
    
    * Fill basic cache config
    
    * tmp dir on test cache creation
    
    * Complete config setup
    
    * Update ExecutionConfig test default value & disable test_hash_sha256
    
    * Fix on ledger change cache update error
    
    * Update todos
    
    * Minor clippy fix
    
    * Update controller error handling, hd cache snip behaviour, massa-node settings input
    
    * Doc
    
    * Compilation costs and doc
    
    * Fix hd cache tests
    
    * Debit compilation costs
    
    * Debit cost on tmp module execution
    
    * Test compilation costs payment
    
    * Update compilation costs to associate it with gas
    
    * Revert previous gas costs tests and add not_enough_compilation_gas
    
    * Fix clippy warnings
    
    * Fix node settings
    
    * Cache controller logs
    
    * Remove unecessary module cloning
    
    * Use latest runtime changes in cache controller
    
    * Fix parent branch failing test_hash_sha256
    
    * Review update + Revert parent branch test fixes
    
    * Save init costs on VM execution error and update error handling
    
    * Improve hd cache snip doc
    
    * Add context to the VM related execution errors
    
    * Fix not_enough_compilation_gas test
    
    * Fix vesting tests
    
    * Logs and safety check
    
    * Update runtime version
    
    ---------
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    Co-authored-by: Jean-François Morcillo <jfm@massa.net>

---
+++ massa-module-cache/src/config.rs
@@ -0,0 +1,19 @@
+use massa_sc_runtime::GasCosts;
+use std::path::PathBuf;
+
+pub struct ModuleCacheConfig {
+    /// Path to the hard drive cache storage
+    pub hd_cache_path: PathBuf,
+    /// Gas costs used to:
+    /// * setup `massa-sc-runtime` metering on compilation
+    /// * debit compilation costs
+    pub gas_costs: GasCosts,
+    /// Default gas for compilation
+    pub compilation_gas: u64,
+    /// Maximum number of entries we want to keep in the LRU cache
+    pub lru_cache_size: u32,
+    /// Maximum number of entries we want to keep in the HD cache
+    pub hd_cache_size: usize,
+    /// Amount of entries removed when `hd_cache_size` is reached
+    pub snip_amount: usize,
+}
---
+++ massa-module-cache/src/hd_cache.rs
@@ -0,0 +1,310 @@
+use crate::types::{
+    ModuleInfo, ModuleMetadata, ModuleMetadataDeserializer, ModuleMetadataSerializer,
+};
+use core::panic;
+use massa_hash::Hash;
+use massa_sc_runtime::{GasCosts, RuntimeModule};
+use massa_serialization::{DeserializeError, Deserializer, Serializer};
+use rand::RngCore;
+use rocksdb::{Direction, IteratorMode, WriteBatch, DB};
+use std::path::PathBuf;
+use tracing::debug;
+
+const OPEN_ERROR: &str = "critical: rocksdb open operation failed";
+const CRUD_ERROR: &str = "critical: rocksdb crud operation failed";
+const DATA_SER_ERROR: &str = "critical: metadata serialization failed";
+const DATA_DESER_ERROR: &str = "critical: metadata deserialization failed";
+const MOD_SER_ERROR: &str = "critical: module serialization failed";
+const MOD_DESER_ERROR: &str = "critical: module deserialization failed";
+const MODULE_IDENT: u8 = 0u8;
+const DATA_IDENT: u8 = 1u8;
+
+/// Module key formatting macro
+#[macro_export]
+macro_rules! module_key {
+    ($bc_hash:expr) => {
+        [&$bc_hash.to_bytes()[..], &[MODULE_IDENT]].concat()
+    };
+}
+
+/// Delta key formatting macro
+#[macro_export]
+macro_rules! metadata_key {
+    ($bc_hash:expr) => {
+        [&$bc_hash.to_bytes()[..], &[DATA_IDENT]].concat()
+    };
+}
+
+pub(crate) struct HDCache {
+    /// RocksDB database
+    db: DB,
+    /// How many entries are in the db. Count is initialized at creation time by iterating
+    /// over all the entries in the db then it is maintained in memory
+    entry_count: usize,
+    /// Maximum number of entries we want to keep in the db.
+    /// When this maximum is reached `snip_amount` entries are removed
+    max_entry_count: usize,
+    /// How many entries are removed when `entry_count` reaches `max_entry_count`
+    snip_amount: usize,
+    /// Module metadata serializer
+    meta_ser: ModuleMetadataSerializer,
+    /// Module metadata deserializer
+    meta_deser: ModuleMetadataDeserializer,
+}
+
+impl HDCache {
+    /// Create a new HDCache
+    ///
+    /// # Arguments
+    /// * path: where to store the db
+    /// * max_entry_count: maximum number of entries we want to keep in the db
+    /// * amount_to_remove: how many entries are removed when `entry_count` reaches `max_entry_count`
+    pub fn new(path: PathBuf, max_entry_count: usize, snip_amount: usize) -> Self {
+        let db = DB::open_default(path).expect(OPEN_ERROR);
+        let entry_count = db.iterator(IteratorMode::Start).count();
+
+        Self {
+            db,
+            entry_count,
+            max_entry_count,
+            snip_amount,
+            meta_ser: ModuleMetadataSerializer::new(),
+            meta_deser: ModuleMetadataDeserializer::new(),
+        }
+    }
+
+    /// Insert a new module in the cache
+    pub fn insert(&mut self, hash: Hash, module_info: ModuleInfo) {
+        if self.entry_count >= self.max_entry_count {
+            self.snip();
+        }
+
+        let mut ser_metadata = Vec::new();
+        let ser_module = match module_info {
+            ModuleInfo::Invalid => {
+                self.meta_ser
+                    .serialize(&ModuleMetadata::Invalid, &mut ser_metadata)
+                    .expect(DATA_SER_ERROR);
+                Vec::new()
+            }
+            ModuleInfo::Module(module) => {
+                self.meta_ser
+                    .serialize(&ModuleMetadata::NotExecuted, &mut ser_metadata)
+                    .expect(DATA_SER_ERROR);
+                module.serialize().expect(MOD_SER_ERROR)
+            }
+            ModuleInfo::ModuleAndDelta((module, delta)) => {
+                self.meta_ser
+                    .serialize(&ModuleMetadata::Delta(delta), &mut ser_metadata)
+                    .expect(DATA_SER_ERROR);
+                module.serialize().expect(MOD_SER_ERROR)
+            }
+        };
+
+        let mut batch = WriteBatch::default();
+        batch.put(module_key!(hash), ser_module);
+        batch.put(metadata_key!(hash), ser_metadata);
+        self.db.write(batch).expect(CRUD_ERROR);
+
+        self.entry_count = self.entry_count.saturating_add(1);
+
+        debug!("(HD insert) entry_count is: {}", self.entry_count);
+    }
+
+    /// Sets the initialization cost of a given module separately
+    ///
+    /// # Arguments
+    /// * `hash`: hash associated to the module for which we want to set the cost
+    /// * `init_cost`: the new cost associated to the module
+    pub fn set_init_cost(&self, hash: Hash, init_cost: u64) {
+        let mut ser_metadata = Vec::new();
+        self.meta_ser
+            .serialize(&ModuleMetadata::Delta(init_cost), &mut ser_metadata)
+            .expect(DATA_SER_ERROR);
+        self.db
+            .put(metadata_key!(hash), ser_metadata)
+            .expect(CRUD_ERROR);
+    }
+
+    /// Sets a given module as invalid
+    pub fn set_invalid(&self, hash: Hash) {
+        let mut ser_metadata = Vec::new();
+        self.meta_ser
+            .serialize(&ModuleMetadata::Invalid, &mut ser_metadata)
+            .expect(DATA_SER_ERROR);
+        self.db
+            .put(metadata_key!(hash), ser_metadata)
+            .expect(CRUD_ERROR);
+    }
+
+    /// Retrieve a module
+    pub fn get(&self, hash: Hash, limit: u64, gas_costs: GasCosts) -> Option<ModuleInfo> {
+        let mut iterator = self
+            .db
+            .iterator(IteratorMode::From(&module_key!(hash), Direction::Forward));
+
+        if let (
+            Some(Ok((key_1, ser_module))),
+            Some(Ok((key_2, ser_metadata))),
+        ) = (iterator.next(), iterator.next())
+            && *key_1 == module_key!(hash)
+            && *key_2 == metadata_key!(hash)
+        {
+            let module = RuntimeModule::deserialize(&ser_module, limit, gas_costs).expect(MOD_DESER_ERROR);
+            let (_, metadata) = self
+                .meta_deser
+                .deserialize::<DeserializeError>(&ser_metadata)
+                .expect(DATA_DESER_ERROR);
+            let result = match metadata {
+                ModuleMetadata::Invalid => ModuleInfo::Invalid,
+                ModuleMetadata::NotExecuted => ModuleInfo::Module(module),
+                ModuleMetadata::Delta(delta) => ModuleInfo::ModuleAndDelta((module, delta)),
+            };
+            Some(result)
+        } else {
+            None
+        }
+    }
+
+    /// Try to remove as much as `self.amount_to_snip` entries from the db
+    fn snip(&mut self) {
+        let mut iter = self.db.raw_iterator();
+        let mut batch = WriteBatch::default();
+        let mut snipped_count: usize = 0;
+
+        while snipped_count < self.snip_amount {
+            // generate a random key
+            let mut rbytes = [0u8; 16];
+            rand::thread_rng().fill_bytes(&mut rbytes);
+            let key = *Hash::compute_from(&rbytes).to_bytes();
+
+            // take the upper existing key
+            iter.seek_for_prev(key);
+
+            // check iterator validity
+            if !iter.valid() {
+                continue;
+            }
+
+            // unwrap justified by above conditional statement.
+            // seeking the previous key of a randombly generated one
+            // will always end up on a metadata key.
+            let metadata_key = iter.key().unwrap();
+            batch.delete(metadata_key);
+            iter.prev();
+            let module_key = iter.key().unwrap();
+            batch.delete(module_key);
+
+            // increase snipped_count
+            snipped_count += 1;
+        }
+
+        // safety check
+        if batch.len() / 2 != snipped_count {
+            panic!("snipped_count incoherence");
+        }
+
+        // delete the key and reduce entry_count
+        self.db.write(batch).expect(CRUD_ERROR);
+        self.entry_count -= snipped_count;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use massa_hash::Hash;
+    use massa_sc_runtime::{Compiler, GasCosts, RuntimeModule};
+    use rand::thread_rng;
+    use serial_test::serial;
+    use tempfile::TempDir;
+
+    fn make_default_module_info() -> ModuleInfo {
+        let bytecode: Vec<u8> = vec![
+            0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x06, 0x01, 0x60, 0x01, 0x7f,
+            0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07, 0x0b, 0x01, 0x07, 0x61, 0x64, 0x64, 0x5f,
+            0x6f, 0x6e, 0x65, 0x00, 0x00, 0x0a, 0x09, 0x01, 0x07, 0x00, 0x20, 0x00, 0x41, 0x01,
+            0x6a, 0x0b, 0x00, 0x1a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x01, 0x0a, 0x01, 0x00, 0x07,
+            0x61, 0x64, 0x64, 0x5f, 0x6f, 0x6e, 0x65, 0x02, 0x07, 0x01, 0x00, 0x01, 0x00, 0x02,
+            0x70, 0x30,
+        ];
+        ModuleInfo::Module(
+            RuntimeModule::new(&bytecode, 10, GasCosts::default(), Compiler::CL).unwrap(),
+        )
+    }
+
+    fn setup() -> HDCache {
+        let tmp_path = TempDir::new().unwrap().path().to_path_buf();
+        HDCache::new(tmp_path, 1000, 10)
+    }
+
+    #[test]
+    #[serial]
+    fn test_basic_crud() {
+        let mut cache = setup();
+        let hash = Hash::compute_from(b"test_hash");
+        let module = make_default_module_info();
+
+        let limit = 1;
+        let init_cost = 100;
+        let gas_costs = GasCosts::default();
+
+        cache.insert(hash, module.clone());
+        let cached_module_v1 = cache.get(hash, limit, gas_costs.clone()).unwrap();
+        assert!(matches!(cached_module_v1, ModuleInfo::Module(_)));
+
+        cache.set_init_cost(hash, init_cost);
+        let cached_module_v2 = cache.get(hash, limit, gas_costs.clone()).unwrap();
+        assert!(matches!(cached_module_v2, ModuleInfo::ModuleAndDelta(_)));
+
+        cache.set_invalid(hash);
+        let cached_module_v3 = cache.get(hash, limit, gas_costs).unwrap();
+        assert!(matches!(cached_module_v3, ModuleInfo::Invalid));
+    }
+
+    #[test]
+    #[serial]
+    fn test_insert_more_than_max_entry() {
+        let mut cache = setup();
+        let module = make_default_module_info();
+
+        // fill the db: add cache.max_entry_count entries
+        for count in 0..cache.max_entry_count {
+            let key = Hash::compute_from(count.to_string().as_bytes());
+            cache.insert(key, module.clone());
+        }
+        assert_eq!(cache.entry_count, cache.max_entry_count);
+
+        // insert one more entry
+        let key = Hash::compute_from(cache.max_entry_count.to_string().as_bytes());
+        cache.insert(key, module.clone());
+        assert_eq!(
+            cache.entry_count,
+            cache.max_entry_count - cache.snip_amount + 1
+        );
+        dbg!(cache.entry_count);
+    }
+
+    #[test]
+    #[serial]
+    fn test_missing_module() {
+        let mut cache = setup();
+        let module = make_default_module_info();
+
+        let limit = 1;
+        let gas_costs = GasCosts::default();
+
+        for count in 0..cache.max_entry_count {
+            let key = Hash::compute_from(count.to_string().as_bytes());
+            cache.insert(key, module.clone());
+        }
+
+        for _ in 0..cache.max_entry_count {
+            let mut rbytes = [0u8; 16];
+            thread_rng().fill_bytes(&mut rbytes);
+            let get_key = Hash::compute_from(&rbytes);
+            let cached_module = cache.get(get_key, limit, gas_costs.clone());
+            assert!(cached_module.is_none());
+        }
+    }
+}
