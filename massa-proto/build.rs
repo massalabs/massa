3bfacdad0c34c408888080f85e15cadcd7e5d967
Moncef AOUDIA <22281426+aoudiamoncef@users.noreply.github.com>
2023-04-04 18:59:38 +0200

    Disable tonic build by default (#3764)

--- massa-proto/build.rs
+++ massa-proto/build.rs
@@ -1,37 +1,53 @@
 // Copyright (c) 2023 MASSA LABS <info@massa.net>
 
-use glob::glob;
-use std::path::PathBuf;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //TODO add download external protos files instead of doing it manually
-    let protos = find_protos("proto/massa/");
+    #[cfg(feature = "build-tonic")]
+    tonic::build()?;
 
-    tonic_build::configure()
-        .build_server(true)
-        .build_transport(true)
-        .build_client(true)
-        .type_attribute(
-            ".google.api.HttpRule",
-            "#[cfg(not(doctest))]\n\
-             #[allow(dead_code)]\n\
-             pub struct HttpRuleComment{}\n\
-             /// HACK: see docs in [`HttpRuleComment`] ignored in doctest pass",
-        )
-        .file_descriptor_set_path("src/api.bin")
-        .include_file("_includes.rs")
-        .out_dir("src/")
-        .compile(
-            &protos,
-            &["proto/massa/api/v1/", "proto/third-party"], // specify the root location to search proto dependencies
-        )?;
+    #[cfg(not(feature = "build-tonic"))]
+    println!("cargo:warning=build-tonic feature is disabled, you can update the generated code from protobuf files by running: cargo check --features build-tonic");
 
     Ok(())
 }
 
-fn find_protos(dir_path: &str) -> Vec<PathBuf> {
-    glob(&format!("{dir_path}/**/*.proto"))
-        .unwrap()
-        .flatten()
-        .collect()
+#[cfg(feature = "build-tonic")]
+mod tonic {
+    use glob::glob;
+    use std::path::PathBuf;
+
+    pub fn build() -> Result<(), Box<dyn std::error::Error>> {
+        //TODO add download external protos files instead of doing it manually
+        let protos = find_protos("proto/massa/");
+
+        let mut prost_config = prost_build::Config::default();
+        prost_config.protoc_arg("--experimental_allow_proto3_optional");
+
+        tonic_build::configure()
+            .build_server(true)
+            .build_transport(true)
+            .build_client(true)
+            .type_attribute(
+                ".google.api.HttpRule",
+                "#[cfg(not(doctest))]\n\
+                 #[allow(dead_code)]\n\
+                 pub struct HttpRuleComment{}\n\
+                 /// HACK: see docs in [`HttpRuleComment`] ignored in doctest pass",
+            )
+            .file_descriptor_set_path("src/api.bin")
+            .include_file("_includes.rs")
+            .out_dir("src/")
+            .compile_with_config(
+                prost_config,
+                &protos,
+                &["proto/massa/api/v1/", "proto/third-party"], // specify the root location to search proto dependencies
+            )
+            .map_err(|e| e.into())
+    }
+
+    fn find_protos(dir_path: &str) -> Vec<PathBuf> {
+        glob(&format!("{dir_path}/**/*.proto"))
+            .unwrap()
+            .flatten()
+            .collect()
+    }
 }
