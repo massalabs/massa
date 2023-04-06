1a7c8001fffe390b033bf9e30968b2b376767ea6
Leo-Besancon <leoleo38@live.fr>
2023-04-06 09:11:00 +0200

    Update downtime timestamps
    10AM > 11AM on 6th of april

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -21,9 +21,9 @@ use massa_time::MassaTime;
 use num::rational::Ratio;
 
 /// Start of the downtime simulation
-pub const DOWNTIME_START_TIMESTAMP: MassaTime = MassaTime::from_millis(1680699600000); // 05/04/2023 3PM CET
+pub const DOWNTIME_START_TIMESTAMP: MassaTime = MassaTime::from_millis(1680768000000); // 06/04/2023 10AM CET
 /// End of the downtime simulation
-pub const DOWNTIME_END_TIMESTAMP: MassaTime = MassaTime::from_millis(1680703200000); // 05/04/2023 4PM CET
+pub const DOWNTIME_END_TIMESTAMP: MassaTime = MassaTime::from_millis(1680771600000); // 06/04/2023 11AM CET
 
 /// Limit on the number of peers we advertise to others.
 pub const MAX_ADVERTISE_LENGTH: u32 = 10000;
