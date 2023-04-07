a561b1371ac099274eaf862c6f9f2177a6e0f434
AurelienFT <aurelien.foucault@epitech.eu>
2023-04-07 11:43:00 +0200

    Testnet 22

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -75,7 +75,7 @@ lazy_static::lazy_static! {
         if cfg!(feature = "sandbox") {
             "SAND.20.0"
         } else {
-            "TEST.21.0"
+            "TEST.22.0"
         }
         .parse()
         .unwrap()
