eff0e2d94a7250899e0e0fea0188d593885ca047
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2023-04-24 18:07:26 +0000

    Deploying to gh-pages from @ massalabs/massa@32fd1459fac332ab7b6bee0a5c29bab52bff1b11 🚀

--- massa_async_pool/changes/sidebar-items.js
+++ massa_async_pool/changes/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["VARIANTS"],"enum":["Change","ChangeId","__Field"],"struct":["AsyncPoolChanges","AsyncPoolChangesDeserializer","AsyncPoolChangesSerializer","__FieldVisitor","__Visitor"]};
+window.SIDEBAR_ITEMS = {"struct":["AsyncPoolChanges","AsyncPoolChangesDeserializer","AsyncPoolChangesSerializer","__Visitor"]};
