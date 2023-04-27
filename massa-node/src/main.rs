3727ff80854256c7905449281018f52eed33f1b7
Moncef AOUDIA <22281426+aoudiamoncef@users.noreply.github.com>
2023-04-27 11:48:56 +0200

    Implement `GetScExecutionEvents` in gRPC API (#3868)

--- massa-node/src/main.rs
+++ massa-node/src/main.rs
@@ -764,6 +764,7 @@ async fn launch(
             last_start_period: final_state.read().last_start_period,
             max_denunciations_per_block_header: MAX_DENUNCIATIONS_PER_BLOCK_HEADER,
             max_block_ids_per_request: SETTINGS.grpc.max_block_ids_per_request,
+            max_operation_ids_per_request: SETTINGS.grpc.max_operation_ids_per_request,
         };
 
         let grpc_api = MassaGrpc {
