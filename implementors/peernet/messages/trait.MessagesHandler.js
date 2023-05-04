bb1ae5fa422e92aea9d473fd273cfffe4714d3aa
AurelienFT <AurelienFT@users.noreply.github.com>
2023-05-04 08:19:11 +0000

    Deploying to gh-pages from @ massalabs/massa@b241538e8317236a16f37b79964980dc1bb239f8 🚀

--- implementors/peernet/messages/trait.MessagesHandler.js
+++ implementors/peernet/messages/trait.MessagesHandler.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_protocol_worker":[["impl MessagesHandler for <a class=\"struct\" href=\"massa_protocol_worker/messages/struct.MessagesHandler.html\" title=\"struct massa_protocol_worker::messages::MessagesHandler\">MessagesHandler</a>"],["impl MessagesHandler for <a class=\"struct\" href=\"massa_protocol_worker/handlers/peer_handler/tester/struct.TesterMessagesHandler.html\" title=\"struct massa_protocol_worker::handlers::peer_handler::tester::TesterMessagesHandler\">TesterMessagesHandler</a>"]]
+"massa_protocol_worker":[["impl MessagesHandler for <a class=\"struct\" href=\"massa_protocol_worker/messages/struct.MessagesHandler.html\" title=\"struct massa_protocol_worker::messages::MessagesHandler\">MessagesHandler</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
