705241c3313b5176fee1937a5b9d6271cd4cc8c4
aoudiamoncef <aoudiamoncef@users.noreply.github.com>
2023-05-11 08:16:25 +0000

    Deploying to gh-pages from @ massalabs/massa@6ba245cfb86313159fc97a5b260675e5a30f867f 🚀

--- implementors/num_enum/trait.FromPrimitive.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"massa_versioning_worker":[["impl FromPrimitive for <a class=\"enum\" href=\"massa_versioning_worker/versioning/enum.MipComponent.html\" title=\"enum massa_versioning_worker::versioning::MipComponent\">MipComponent</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- massa_bootstrap/bindings/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["client","server"],"trait":["BindingReadExact"]};
