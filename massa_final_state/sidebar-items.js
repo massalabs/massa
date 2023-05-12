96a70e7617bd52f0b157fad92050fa25ddea8c88
aoudiamoncef <aoudiamoncef@users.noreply.github.com>
2023-05-12 14:56:32 +0000

    Deploying to gh-pages from @ massalabs/massa@bc2b4c53eb039d461ac0937c327844e8fd33485d 🚀

--- massa_final_state/sidebar-items.js
+++ massa_final_state/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["FinalStateError"],"mod":["config","error","final_state","state_changes"],"struct":["FinalState","FinalStateConfig","StateChanges","StateChangesDeserializer","StateChangesSerializer"]};
+window.SIDEBAR_ITEMS = {"enum":["FinalStateError"],"mod":["config","error","final_state","mapping_grpc","state_changes"],"struct":["FinalState","FinalStateConfig","StateChanges","StateChangesDeserializer","StateChangesSerializer"]};
