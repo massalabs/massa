6afd3afe7a68b22953e59c89687923cd07ba882e
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2023-05-17 12:03:21 +0000

    Deploying to gh-pages from @ massalabs/massa@0f07f18c3e92213950a73e79aec24ef8aaff941c 🚀

---
+++ implementors/monotree/database/trait.Database.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_db":[["impl Database for <a class=\"struct\" href=\"massa_db/massa_db/struct.MassaDBForMonotree.html\" title=\"struct massa_db::massa_db::MassaDBForMonotree\">MassaDBForMonotree</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ massa_db/massa_db/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["MassaDBConfig","MassaDBForMonotree","RawMassaDB","RawMassaDBForMonotree","StreamBatch"],"type":["DBBatch","Key","MassaDB","Value"]};
