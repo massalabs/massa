33b7ebd84b8dae6fe3a805c067567dc12bd327bc
Leo-Besancon <leoleo38@live.fr>
2023-05-17 15:25:51 +0200

    Added network restart documentation in massa-final-state crate lib.rs

--- massa-final-state/src/lib.rs
+++ massa-final-state/src/lib.rs
@@ -32,6 +32,57 @@
 //!
 //! When the crate feature `testing` is enabled, tooling useful for testing purposes is exported.
 //! See `test_exports/mod.rs` for details.
+//!
+//! # Network restart documentation
+//!
+//! ## Goals of the network restart
+//! If the blockchain crashes (corrupted / attacked ledger, all nodes crash, etc.) and we want to keep the same main parameters of the network (same `GENESIS_TIMESTAMP`, same ledger, same final_state, etc.), then we can restart the network.
+//!
+//! **ONE** node should restart from a snapshot (which is just the RocksDB ledger, read as usual), and the other nodes should bootstrap from it.
+//!
+//! ## Command line
+//!
+//! ```sh
+//! cargo run --release -- --restart-from-snapshot-at-period 200
+//! ```
+//!
+//! Means: the node will restart from the ledger and final_state on disk (usual path in the config). Block production will start once the period given in args is reached (here, 200).
+//!
+//! ## Scenario
+//!
+//! 1. At period 40, the network crashes.
+//! 2. We restart one node N0, at the time of period 80, with `cargo run --release -- --restart-from-snapshot-at-period 200`
+//! 3. We start one other node N1, at the time of period 100, with `cargo run --release`
+//! 4. The node N1 will bootstrap from N0. No blocks are produced yet.
+//! 5. At the time of period 200, block production starts again.
+//!
+//! ## Additional notes
+//!
+//! ### Why is block production delayed?
+//!
+//! In order to give time to all nodes to rejoin the network after a crash and bootstrap. If we don't give them the time, their rolls would be sold because most stakers would have a lot of block miss.
+//!
+//! ### In sandbox
+//!
+//! Sandbox feature can be enabled. For instance, here is a test scenario:
+//!
+//! 1. Run the node as usual: `cargo run --release --features sandbox`
+//! 2. Make transaction, buy rolls, etc.
+//! 3. Shut down the node at slot S_0.
+//! 4. Restart the network: `cargo run --release --features sandbox --restart-from-snapshot-at-period S_1`
+//!
+//! Here, the network will restart, and the network will start producing blocks again 10 seconds after launch.
+//!
+//! **/!\ This means that the genesis timestamp will be different between runs, but it should not matter in most cases.**
+//!
+//! ### Backups
+//!
+//! By default, the network restarts from the state associated with the last final slot before the shutdown.
+//! However, we may sometimes want to recover from an earlier state (e.g. if an attacker stole 50% of all Massa, we want to restart with the state before the attack.
+//! We use RocksDB checkpoint system to save the state at regular interval (see the `PERIODS_BETWEEN_BACKUPS` constant in `massa > massa-models > src > config > constants.rs`)
+//! Backups for `Slot {period, thread}` are stored in `massa > massa-node > storage > ledger > rocks_db_backup > backup_[period]_[thread]`
+//! Backups are hard links of the rocks_db, so the overhead of storing them should be minimal.
+//! To recover from a backup, simply replace the contents of the rocks_db folder by the contents of the target backup folder.
 
 #![warn(missing_docs)]
 #![warn(unused_crate_dependencies)]
