299cf43da0733434f658d9cab31fe21af38bcf11
modship <yeskinokay@gmail.com>
2023-05-31 15:02:45 +0200

    add doc and nb files updated

--- massa-xtask/src/update_package_versions.rs
+++ massa-xtask/src/update_package_versions.rs
@@ -4,10 +4,12 @@ use std::path::Path;
 use toml_edit::{Document, Formatted, Item, Value};
 use walkdir::WalkDir;
 
-fn update_workspace_packages_version(
+/// check the version of the packages in the workspace
+fn check_workspace_packages_version(
     new_version: String,
     workspace_path: &Path,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<i32, Box<dyn std::error::Error>> {
+    let mut nb_files_updated = 0;
     // search for Cargo.toml files in the workspace
     for entry in WalkDir::new(workspace_path)
         .into_iter()
@@ -15,19 +17,29 @@ fn update_workspace_packages_version(
     {
         if entry.file_name() == "Cargo.toml" {
             // if the Cargo.toml file is found, check the version and update it if necessary
-            check_package_version(new_version.clone(), entry.path())?;
+            if check_package_version(new_version.clone(), entry.path())? {
+                // if version has been updated
+                nb_files_updated += 1;
+            }
         }
     }
 
-    Ok(())
+    Ok(nb_files_updated)
 }
 
+/// check the version of the package in the Cargo.toml file
+///
+/// Update it if version is different from the new version and if the package is a massa package
+///
+/// return Ok(true) if the version has been updated
 fn check_package_version(
     new_version: String,
     cargo_toml_path: &Path,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<bool, Box<dyn std::error::Error>> {
     let cargo_toml_content = fs::read_to_string(cargo_toml_path)?;
     let mut doc = cargo_toml_content.parse::<Document>()?;
+
+    let mut update = false;
 
     if let Some(package) = doc["package"].as_table_mut() {
         if package["name"].to_string().contains("massa") {
@@ -39,16 +51,19 @@ fn check_package_version(
                     println!(
                         "Updating version of package {} from {} to {}",
                         package["name"], actual_version, new_version
-                    )
+                    );
+                    update = true;
                 }
             }
         }
     }
 
-    let updated_cargo_toml_content = doc.to_string();
-    fs::write(cargo_toml_path, updated_cargo_toml_content)?;
+    if update {
+        let updated_cargo_toml_content = doc.to_string();
+        fs::write(cargo_toml_path, updated_cargo_toml_content)?;
+    }
 
-    Ok(())
+    Ok(update)
 }
 
 pub(crate) fn update_package_versions() {
@@ -66,7 +81,8 @@ pub(crate) fn update_package_versions() {
 
     let workspace_path = Path::new("./");
 
-    if let Err(e) = update_workspace_packages_version(to_string, workspace_path) {
-        panic!("Error updating workspace packages version: {}", e);
+    match check_workspace_packages_version(to_string, workspace_path) {
+        Err(e) => panic!("Error updating workspace packages version: {}", e),
+        Ok(nb_files_updated) => println!("{} files updated", nb_files_updated),
     }
 }
