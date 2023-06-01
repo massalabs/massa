c67ef2fe6a70ffb81abd4055f3c736a3b5946638
sydhds <sydhds@users.noreply.github.com>
2023-06-01 10:11:40 +0000

    Deploying to gh-pages from @ massalabs/massa@50564895563f5b83d0910331b4b823179a3d9e3d 🚀

--- massa_versioning/sidebar-items.js
+++ massa_versioning/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["address_factory","grpc_mapping","keypair_factory","versioning","versioning_factory","versioning_ser_der"]};
+window.SIDEBAR_ITEMS = {"mod":["address_factory","grpc_mapping","keypair_factory","mips","versioning","versioning_factory","versioning_ser_der"]};
