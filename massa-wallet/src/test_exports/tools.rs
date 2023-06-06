5476ac8173aabbac9091115b8127c7881a6491d3
AurelienFT <aurelien.foucault@epitech.eu>
2023-06-06 14:30:40 +0200

    Fix tests

--- massa-wallet/src/test_exports/tools.rs
+++ massa-wallet/src/test_exports/tools.rs
@@ -1,27 +1,18 @@
-use std::io::Write;
-
-use massa_cipher::encrypt;
 use massa_models::{address::Address, prehash::PreHashMap};
 use massa_signature::KeyPair;
-use tempfile::NamedTempFile;
+use tempfile::TempDir;
 
 use crate::Wallet;
 
 /// Creates a temporary file and a temporary wallet.
-pub fn create_test_wallet(default_accounts: Option<PreHashMap<Address, KeyPair>>) -> Wallet {
-    let wallet_file = NamedTempFile::new().expect("cannot create temp file");
+pub fn create_test_wallet(
+    default_accounts: Option<PreHashMap<Address, KeyPair>>,
+) -> (TempDir, Wallet) {
     let accounts = default_accounts.unwrap_or_default();
-    wallet_file
-        .as_file()
-        .write_all(
-            &encrypt(
-                "test",
-                serde_json::to_string::<PreHashMap<Address, KeyPair>>(&accounts)
-                    .unwrap()
-                    .as_bytes(),
-            )
-            .unwrap(),
-        )
+    let folder = TempDir::new().expect("cannot create temp dir");
+    let mut wallet = Wallet::new(folder.path().to_path_buf(), "test".to_string()).unwrap();
+    wallet
+        .add_keypairs(accounts.values().cloned().collect())
         .unwrap();
-    Wallet::new(wallet_file.path().to_path_buf(), "test".to_string()).unwrap()
+    (folder, wallet)
 }
