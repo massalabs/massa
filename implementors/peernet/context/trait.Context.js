1e5c084daecb9e1ee25b6748de111a0aebdf7c43
modship <modship@users.noreply.github.com>
2023-06-09 10:34:15 +0000

    Deploying to gh-pages from @ massalabs/massa@6e63ad88a9bc9c37fc9f6d949a6a0fd506e9fb4f 🚀

--- implementors/peernet/context/trait.Context.js
+++ implementors/peernet/context/trait.Context.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_protocol_worker":[["impl Context&lt;<a class=\"struct\" href=\"massa_protocol_exports/peer_id/struct.PeerId.html\" title=\"struct massa_protocol_exports::peer_id::PeerId\">PeerId</a>&gt; for <a class=\"struct\" href=\"massa_protocol_worker/context/struct.Context.html\" title=\"struct massa_protocol_worker::context::Context\">Context</a>"]]
+"massa_protocol_worker":[["impl Context&lt;PeerId&gt; for <a class=\"struct\" href=\"massa_protocol_worker/context/struct.Context.html\" title=\"struct massa_protocol_worker::context::Context\">Context</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ massa_metrics/server/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["bind_metrics","serve_req"]};
