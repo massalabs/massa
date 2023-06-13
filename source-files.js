c16c4d755a0377f8d403f26f9683018563c117ee
AurelienFT <AurelienFT@users.noreply.github.com>
2023-06-13 07:45:48 +0000

    Deploying to gh-pages from @ massalabs/massa@6fa2f1d8563c9f6ec571cb1dc8c73a0e378fda35 🚀

--- source-files.js
+++ source-files.js
@@ -3,6 +3,7 @@ var sourcesIndex = JSON.parse('{\
 "massa_api_exports":["",[],["address.rs","block.rs","config.rs","datastore.rs","endorsement.rs","error.rs","execution.rs","ledger.rs","lib.rs","node.rs","operation.rs","page.rs","rolls.rs","slot.rs"]],\
 "massa_async_pool":["",[],["changes.rs","config.rs","lib.rs","mapping_grpc.rs","message.rs","pool.rs"]],\
 "massa_bootstrap":["",[["bindings",[],["client.rs","server.rs"]],["server",[],["white_black_list.rs"]]],["bindings.rs","client.rs","error.rs","lib.rs","listener.rs","messages.rs","server.rs","settings.rs","tools.rs"]],\
+"massa_channel":["",[],["lib.rs","receiver.rs","sender.rs"]],\
 "massa_cipher":["",[],["constants.rs","decrypt.rs","encrypt.rs","error.rs","lib.rs"]],\
 "massa_client":["",[],["cmds.rs","display.rs","main.rs","repl.rs","settings.rs"]],\
 "massa_consensus_exports":["",[],["block_graph_export.rs","block_status.rs","bootstrapable_graph.rs","channels.rs","controller_trait.rs","error.rs","events.rs","export_active_block.rs","lib.rs","settings.rs"]],\
@@ -20,6 +21,7 @@ var sourcesIndex = JSON.parse('{\
 "massa_ledger_exports":["",[],["config.rs","controller.rs","error.rs","key.rs","ledger_changes.rs","ledger_entry.rs","lib.rs","mapping_grpc.rs","types.rs"]],\
 "massa_ledger_worker":["",[],["ledger.rs","ledger_db.rs","lib.rs"]],\
 "massa_logging":["",[],["lib.rs"]],\
+"massa_metrics":["",[],["lib.rs","server.rs"]],\
 "massa_models":["",[["config",[],["compact_config.rs","constants.rs","massa_settings.rs","mod.rs"]]],["active_block.rs","address.rs","amount.rs","block.rs","block_header.rs","block_id.rs","bytecode.rs","clique.rs","composite.rs","datastore.rs","denunciation.rs","endorsement.rs","error.rs","execution.rs","ledger.rs","lib.rs","mapping_grpc.rs","node.rs","operation.rs","output_event.rs","prehash.rs","rolls.rs","secure_share.rs","serialization.rs","slot.rs","stats.rs","streaming_step.rs","timeslots.rs","version.rs"]],\
 "massa_module_cache":["",[],["config.rs","controller.rs","error.rs","hd_cache.rs","lib.rs","lru_cache.rs","types.rs"]],\
 "massa_node":["",[],["main.rs","settings.rs"]],\
