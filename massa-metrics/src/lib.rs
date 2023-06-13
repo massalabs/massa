4b1e87df45152c8dffdbbe2a2cde378e8c393b44
modship <yeskinokay@gmail.com>
2023-06-13 11:44:21 +0200

    fix vec alloc
    (cherry picked from commit 56387c6d2be205575f51def6b5dbff59a9ad0669)

--- massa-metrics/src/lib.rs
+++ massa-metrics/src/lib.rs
@@ -97,7 +97,19 @@ impl MassaMetrics {
         // TODO unwrap
 
         let mut consensus_vec = vec![];
+        for i in 0..nb_thread {
+            let gauge = Gauge::new(
+                format!("consensus_thread_{}", i),
+                "consensus thread actual period",
+            )
+            .expect("Failed to create gauge");
+            #[cfg(not(feature = "testing"))]
+            {
+                let _ = prometheus::register(Box::new(gauge.clone()));
+            }
 
+            consensus_vec.push(gauge);
+        }
         // active cursor
         let active_cursor_thread =
             IntGauge::new("active_cursor_thread", "execution active cursor thread").unwrap();
@@ -223,20 +235,6 @@ impl MassaMetrics {
         .unwrap();
 
         if enabled {
-            for i in 0..nb_thread {
-                let gauge = Gauge::new(
-                    format!("consensus_thread_{}", i),
-                    "consensus thread actual period",
-                )
-                .expect("Failed to create gauge");
-                #[cfg(not(feature = "testing"))]
-                {
-                    let _ = prometheus::register(Box::new(gauge.clone()));
-                }
-
-                consensus_vec.push(gauge);
-            }
-
             // TODO addr from config
             #[cfg(not(feature = "testing"))]
             {
@@ -345,7 +343,9 @@ impl MassaMetrics {
     }
 
     pub fn set_consensus_period(&self, thread: usize, period: u64) {
-        self.consensus_vec[thread].set(period as f64);
+        if let Some(g) = self.consensus_vec.get(thread) {
+            g.set(period as f64);
+        }
     }
 
     pub fn set_consensus_state(
