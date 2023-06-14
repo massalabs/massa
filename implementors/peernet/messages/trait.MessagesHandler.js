985392f36c57689b3d53a2aaa5dc7d7353ddb394
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2023-06-14 14:32:12 +0000

    Deploying to gh-pages from @ massalabs/massa@c0704845563e487421c59b17e6b6eafed0da426c 🚀

--- implementors/peernet/messages/trait.MessagesHandler.js
+++ implementors/peernet/messages/trait.MessagesHandler.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_protocol_worker":[["impl MessagesHandler&lt;PeerId&gt; for <a class=\"struct\" href=\"massa_protocol_worker/messages/struct.MessagesHandler.html\" title=\"struct massa_protocol_worker::messages::MessagesHandler\">MessagesHandler</a>"]]
+"massa_protocol_worker":[["impl MessagesHandler&lt;<a class=\"struct\" href=\"massa_protocol_exports/peer_id/struct.PeerId.html\" title=\"struct massa_protocol_exports::peer_id::PeerId\">PeerId</a>&gt; for <a class=\"struct\" href=\"massa_protocol_worker/messages/struct.MessagesHandler.html\" title=\"struct massa_protocol_worker::messages::MessagesHandler\">MessagesHandler</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
