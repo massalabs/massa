8251d2ffb2e64d8d9b6546bdcb8fded5992999f0
Ben-PH <Ben-PH@users.noreply.github.com>
2023-06-15 11:16:38 +0000

    Deploying to gh-pages from @ massalabs/massa@7b1279a71338c64904c5d6aaa4381e2e5ec49ae1 🚀

--- implementors/massa_proto_rs/massa/api/v1/massa_service_server/trait.MassaService.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"massa_grpc":[["impl MassaService for <a class=\"struct\" href=\"massa_grpc/server/struct.MassaGrpc.html\" title=\"struct massa_grpc::server::MassaGrpc\">MassaGrpc</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
