52ebc68330ba83b6c89d65a331451eaa362387ad
AurelienFT <aurelien.foucault@epitech.eu>
2023-06-16 13:11:05 +0200

    Update to activation delay of 40 cycles

--- massa-node/src/main.rs
+++ massa-node/src/main.rs
@@ -303,7 +303,7 @@ async fn launch(
                 components: BTreeMap::from([(MipComponent::FinalStateHashKind, 1)]),
                 start: mip_0002_start,
                 timeout: mip_0002_timeout,
-                activation_delay: T0.saturating_mul(PERIODS_PER_CYCLE.saturating_add(1)),
+                activation_delay: T0.saturating_mul(PERIODS_PER_CYCLE.saturating_add(1)).saturating_mul(40),
             },
             MipState::new(mip_0002_defined_start),
         ),
