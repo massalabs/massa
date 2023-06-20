fab552c1dc4ed0536c18c1ab4753e67575e817bc
litchipi <litchipi@users.noreply.github.com>
2023-06-20 15:03:54 +0000

    Deploying to gh-pages from @ massalabs/massa@7fed3e13fe75ac8511275ac17d7fffbbc5802b18 🚀

--- implementors/massa_proto/massa/api/v1/massa_service_server/trait.MassaService.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"massa_grpc":[["impl <a class=\"trait\" href=\"massa_proto/massa/api/v1/massa_service_server/trait.MassaService.html\" title=\"trait massa_proto::massa::api::v1::massa_service_server::MassaService\">MassaService</a> for <a class=\"struct\" href=\"massa_grpc/server/struct.MassaGrpc.html\" title=\"struct massa_grpc::server::MassaGrpc\">MassaGrpc</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
