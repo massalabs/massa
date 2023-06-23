e5f29bc3ee80932190a8ee51491e7cf49e48070e
modship <modship@users.noreply.github.com>
2023-06-23 14:14:23 +0000

    Deploying to gh-pages from @ massalabs/massa@9d06849583d9b3f6f9900e10ab75688f0b43bda8 🚀

--- massa_metrics/sidebar-items.js
+++ massa_metrics/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["dec_endorsements_counter","dec_operations_counter","inc_endorsements_counter","inc_operations_counter","set_blocks_counter","set_connections"],"mod":["server"],"struct":["BLOCKS_COUNTER","ENDORSEMENTS_COUNTER","IN_CONNECTIONS","MassaMetrics","OPERATIONS_COUNTER","OUT_CONNECTIONS"]};
+window.SIDEBAR_ITEMS = {"fn":["set_blocks_counter","set_endorsements_counter","set_operations_counter"],"mod":["server","survey"],"struct":["BLOCKS_COUNTER","ENDORSEMENTS_COUNTER","MassaMetrics","OPERATIONS_COUNTER"]};
