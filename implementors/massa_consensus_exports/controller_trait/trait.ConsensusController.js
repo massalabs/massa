891f41486ee4f0379e90d0e7da3dc87fb876e425
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2023-06-26 16:20:49 +0000

    Deploying to gh-pages from @ massalabs/massa@c9128442f1ccf84bbfdcd3fc9afeee2c5dbc59e7 🚀

--- implementors/massa_consensus_exports/controller_trait/trait.ConsensusController.js
+++ implementors/massa_consensus_exports/controller_trait/trait.ConsensusController.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_consensus_worker":[["impl ConsensusController for <a class=\"struct\" href=\"massa_consensus_worker/controller/struct.ConsensusControllerImpl.html\" title=\"struct massa_consensus_worker::controller::ConsensusControllerImpl\">ConsensusControllerImpl</a>"]]
+"massa_consensus_worker":[["impl <a class=\"trait\" href=\"massa_consensus_exports/controller_trait/trait.ConsensusController.html\" title=\"trait massa_consensus_exports::controller_trait::ConsensusController\">ConsensusController</a> for <a class=\"struct\" href=\"massa_consensus_worker/controller/struct.ConsensusControllerImpl.html\" title=\"struct massa_consensus_worker::controller::ConsensusControllerImpl\">ConsensusControllerImpl</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
