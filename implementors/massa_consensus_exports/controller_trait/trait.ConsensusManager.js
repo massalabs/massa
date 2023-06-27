16620fe81c26ae58a577b5d8f5308c504f6bb312
modship <modship@users.noreply.github.com>
2023-06-27 08:20:04 +0000

    Deploying to gh-pages from @ massalabs/massa@45dd50721d511481a4786ecd02bb82582d9d241b 🚀

--- implementors/massa_consensus_exports/controller_trait/trait.ConsensusManager.js
+++ implementors/massa_consensus_exports/controller_trait/trait.ConsensusManager.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_consensus_worker":[["impl ConsensusManager for <a class=\"struct\" href=\"massa_consensus_worker/manager/struct.ConsensusManagerImpl.html\" title=\"struct massa_consensus_worker::manager::ConsensusManagerImpl\">ConsensusManagerImpl</a>"]]
+"massa_consensus_worker":[["impl <a class=\"trait\" href=\"massa_consensus_exports/controller_trait/trait.ConsensusManager.html\" title=\"trait massa_consensus_exports::controller_trait::ConsensusManager\">ConsensusManager</a> for <a class=\"struct\" href=\"massa_consensus_worker/manager/struct.ConsensusManagerImpl.html\" title=\"struct massa_consensus_worker::manager::ConsensusManagerImpl\">ConsensusManagerImpl</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
