ce62c7c292e77eb63d5552dde4a07e92f5790dd6
AurelienFT <AurelienFT@users.noreply.github.com>
2023-06-29 11:17:22 +0000

    Deploying to gh-pages from @ massalabs/massa@3778916d259faf2f61e85ac77f73ec665acad9ee 🚀

--- massa_models/block_id/sidebar-items.js
+++ massa_models/block_id/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["BLOCKID_PREFIX","BLOCKID_VERSION","BLOCK_ID_SIZE_BYTES"],"struct":["BlockId","BlockIdDeserializer","BlockIdSerializer","BlockInfo"]};
+window.SIDEBAR_ITEMS = {"constant":["BLOCKID_PREFIX","BLOCKID_VERSION","BLOCK_ID_SIZE_BYTES"],"struct":["BlockId","BlockIdDeserializer","BlockIdSerializer"]};
