c5c35e19026cefbef8d673f0c5c97a831eae9ad8
AurelienFT <AurelienFT@users.noreply.github.com>
2023-06-30 14:45:16 +0000

    Deploying to gh-pages from @ massalabs/massa@569d6b97c2e3d33893834bab6e9e31b5ad30276a 🚀

--- massa_consensus_worker/state/sidebar-items.js
+++ massa_consensus_worker/state/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["clique_computation","graph","process","process_commands","prune","stats","tick","verifications"],"struct":["ConsensusState"]};
+window.SIDEBAR_ITEMS = {"mod":["blocks_state","clique_computation","graph","process","process_commands","prune","stats","tick","verifications"],"struct":["ConsensusState"]};
