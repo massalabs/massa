8fc41c704a214fe89aa4d1f0f59053a1298e885e
aoudiamoncef <aoudiamoncef@users.noreply.github.com>
2023-07-11 15:00:11 +0000

    Deploying to gh-pages from @ massalabs/massa@21a5e932963bff77352f8752779953b7984c55ad 🚀

---
+++ implementors/massa_proto_rs/massa/api/v1/private_service_server/trait.PrivateService.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_grpc":[["impl PrivateService for <a class=\"struct\" href=\"massa_grpc/server/struct.MassaPrivateGrpc.html\" title=\"struct massa_grpc::server::MassaPrivateGrpc\">MassaPrivateGrpc</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
