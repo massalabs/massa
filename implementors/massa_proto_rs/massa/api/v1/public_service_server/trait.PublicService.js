db2c42b876bebea8d3a12cdbd44ae03c96c8e8fd
modship <modship@users.noreply.github.com>
2023-07-13 14:58:33 +0000

    Deploying to gh-pages from @ massalabs/massa@1f2fba06c81e7819edfe0ac62680efa905d42042 🚀

--- implementors/massa_proto_rs/massa/api/v1/public_service_server/trait.PublicService.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"massa_grpc":[["impl PublicService for <a class=\"struct\" href=\"massa_grpc/server/struct.MassaPublicGrpc.html\" title=\"struct massa_grpc::server::MassaPublicGrpc\">MassaPublicGrpc</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
