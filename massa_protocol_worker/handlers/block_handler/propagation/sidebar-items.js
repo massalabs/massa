47ad411eef72163c5b9e4ec0d20dae59417412e6
aoudiamoncef <aoudiamoncef@users.noreply.github.com>
2023-07-26 10:39:20 +0000

    Deploying to gh-pages from @ massalabs/massa@82de507a684c312d4351960760ac0a9d353eef6c 🚀

--- massa_protocol_worker/handlers/block_handler/propagation/sidebar-items.js
+++ massa_protocol_worker/handlers/block_handler/propagation/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["start_propagation_thread"],"struct":["BlockPropagationData","PropagationThread"]};
+window.SIDEBAR_ITEMS = {"fn":["start_propagation_thread"],"struct":["PropagationThread"]};
--- massa_protocol_worker/handlers/block_handler/retrieval/sidebar-items.js
+++ massa_protocol_worker/handlers/block_handler/retrieval/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["start_retrieval_thread"],"struct":["BlockInfo","RetrievalThread"]};
+window.SIDEBAR_ITEMS = {"fn":["start_retrieval_thread"],"static":["BLOCK_HEADER"],"struct":["BlockInfo","RetrievalThread"]};
