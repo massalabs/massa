a709822bcbb109999b26e41cb5260d6222e37b74
Damir Vodenicarevic <damipator@gmail.com>
2023-08-11 11:00:56 +0200

    fix

--- massa-bootstrap/src/tools.rs
+++ massa-bootstrap/src/tools.rs
@@ -11,3 +11,25 @@ pub(crate) fn normalize_ip(ip: IpAddr) -> IpAddr {
     }
     .to_canonical()
 }
+
+/// Convert a mio stream to std
+/// Adapted from Tokio
+pub(crate) fn mio_stream_to_std(mio_socket: mio::net::TcpStream) -> std::net::TcpStream {
+    #[cfg(unix)]
+    {
+        use std::os::unix::io::{FromRawFd, IntoRawFd};
+        unsafe { std::net::TcpStream::from_raw_fd(mio_socket.into_raw_fd()) }
+    }
+
+    #[cfg(windows)]
+    {
+        use std::os::windows::io::{FromRawSocket, IntoRawSocket};
+        unsafe { std::net::TcpStream::from_raw_socket(mio_socket.into_raw_socket()) }
+    }
+
+    #[cfg(target_os = "wasi")]
+    {
+        use std::os::wasi::io::{FromRawFd, IntoRawFd};
+        unsafe { std::net::TcpStream::from_raw_fd(io.into_raw_fd()) }
+    }
+}
