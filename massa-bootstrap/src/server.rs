0fc0668da0b87489d1eb3579ff2eb7675575439e
sydhds <sydhds@gmail.com>
2023-08-24 16:28:55 +0200

    Remove let binding

--- massa-bootstrap/src/server.rs
+++ massa-bootstrap/src/server.rs
@@ -116,8 +116,7 @@ impl BootstrapManager {
 
         // when the runtime is dropped at the end of this stop, the listener is auto-aborted
 
-        let _res1 = self
-            .update_handle
+        self.update_handle
             .join()
             .expect("in BootstrapManager::stop() joining on updater thread")?;
 
