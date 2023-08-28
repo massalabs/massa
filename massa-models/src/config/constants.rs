6ed1de2747f5df0cdd76d60eaab61dc2a9709823
Litchi Pi <litchi.pi@proton.me>
2023-08-28 09:22:26 +0200

    Divide storage costs of massa storage by 10
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -104,9 +104,9 @@ pub const ROLL_PRICE: Amount = Amount::const_init(100, 0);
 /// Block reward is given for each block creation
 pub const BLOCK_REWARD: Amount = Amount::const_init(102, 2);
 /// Cost to store one byte in the ledger
-pub const LEDGER_COST_PER_BYTE: Amount = Amount::const_init(1, 3);
+pub const LEDGER_COST_PER_BYTE: Amount = Amount::const_init(1, 4);
 /// Cost for a base entry default 0.01 MASSA
-pub const LEDGER_ENTRY_BASE_COST: Amount = Amount::const_init(1, 2);
+pub const LEDGER_ENTRY_BASE_COST: Amount = Amount::const_init(1, 3);
 /// Cost for a base entry datastore 10 bytes constant to avoid paying more for longer keys
 pub const LEDGER_ENTRY_DATASTORE_BASE_SIZE: usize = 10;
 /// Time between the periods in the same thread.
