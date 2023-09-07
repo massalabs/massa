7ca5ef23d192baaed935c4598fde2236cdbb7e3c
Leo-Besancon <lb@massa.net>
2023-09-07 08:57:55 +0200

    Revert "Reset to zero the hash trail instead of deleting it (#4385)"
    This reverts commit de428f9ea8061a9445a8367770e7dea7998fed0b.

--- massa-final-state/src/final_state.rs
+++ massa-final-state/src/final_state.rs
@@ -119,8 +119,11 @@ impl FinalState {
         };
 
         if reset_final_state {
-            // reset the execution trail hash
-            final_state.init_execution_trail_hash();
+            // delete the execution trail hash
+            final_state
+                .db
+                .write()
+                .delete_prefix(EXECUTION_TRAIL_HASH_PREFIX, STATE_CF, None);
             final_state.async_pool.reset();
             final_state.pos_state.reset();
             final_state.executed_ops.reset();
