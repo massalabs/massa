27f2a3c65ad9319a6828bb8173133109870bd188
AurelienFT <AurelienFT@users.noreply.github.com>
2023-09-19 13:02:20 +0000

    Deploying to gh-pages from @ massalabs/massa@9f746f8dead240dfbcd5addaa5d98ff2a08e7cc3 🚀

---
+++ implementors/crypto_common/trait.OutputSizeUser.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_hash":[["impl OutputSizeUser for <a class=\"struct\" href=\"massa_hash/hash/struct.SmtHasher.html\" title=\"struct massa_hash::hash::SmtHasher\">SmtHasher</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- massa_hash/hash/sidebar-items.js
+++ massa_hash/hash/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Hash","HashDeserializer","HashSerializer"]};
+window.SIDEBAR_ITEMS = {"struct":["Hash","HashDeserializer","HashSerializer","SmtHasher"]};
