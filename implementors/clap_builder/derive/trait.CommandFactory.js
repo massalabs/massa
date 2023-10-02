1a71f71f38d5bcef235e622b075b0398c08bc8e5
sydhds <sydhds@users.noreply.github.com>
2023-10-02 14:15:35 +0000

    Deploying to gh-pages from @ massalabs/massa@fdce0802412861efdb6fea04aecde47bb26058ba 🚀

--- implementors/clap_builder/derive/trait.CommandFactory.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {
-"massa_client":[["impl CommandFactory for <a class=\"struct\" href=\"massa_client/struct.Args.html\" title=\"struct massa_client::Args\">Args</a>"]],
-"massa_node":[["impl CommandFactory for <a class=\"struct\" href=\"massa_node/struct.Args.html\" title=\"struct massa_node::Args\">Args</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
