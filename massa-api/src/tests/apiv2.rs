afdcebde947e14c940fc17982736c4331ec6ecde
modship <yeskinokay@gmail.com>
2023-10-13 10:36:54 +0200

    subscribe_new_filled_blocks

--- massa-api/src/tests/apiv2.rs
+++ massa-api/src/tests/apiv2.rs
@@ -10,8 +10,12 @@ use jsonrpsee::{
 use massa_consensus_exports::test_exports::MockConsensusControllerImpl;
 use massa_grpc::tests::mock::MockExecutionCtrl;
 use massa_models::{
-    address::Address, block::SecureShareBlock, block_header::BlockHeader, block_id::BlockId,
-    config::VERSION, secure_share::SecureShare,
+    address::Address,
+    block::{FilledBlock, SecureShareBlock},
+    block_header::BlockHeader,
+    block_id::BlockId,
+    config::VERSION,
+    secure_share::SecureShare,
 };
 use massa_protocol_exports::test_exports::tools::create_block;
 use massa_signature::KeyPair;
@@ -222,6 +226,60 @@ async fn subscribe_new_blocks_headers() {
     assert!(result.is_some());
     let value = result.unwrap().unwrap().clone();
     assert_eq!(value["id"].as_str().unwrap(), &block.id.to_string());
+
+    api_handle.stop().await;
+}
+
+#[tokio::test]
+async fn subscribe_new_filled_blocks() {
+    let addr: SocketAddr = "[::]:5035".parse().unwrap();
+    let (mut api_server, api_config) = get_apiv2_server(&addr);
+
+    let uri = Url::parse(&format!(
+        "ws://localhost:{}",
+        addr.to_string().split(':').into_iter().last().unwrap()
+    ))
+    .unwrap();
+    let (tx, _rx) = tokio::sync::broadcast::channel::<FilledBlock>(10);
+
+    api_server.0.consensus_channels.filled_block_sender = tx.clone();
+
+    let api_handle = api_server
+        .serve(&addr, &api_config)
+        .await
+        .expect("failed to start MASSA API V2");
+    let block = create_block(&KeyPair::generate(0).unwrap());
+
+    let client1 = WsClientBuilder::default().build(&uri).await.unwrap();
+    let mut sub1: Subscription<Value> = client1
+        .subscribe(
+            "subscribe_new_filled_blocks",
+            rpc_params![],
+            "unsubscribe_hello",
+        )
+        .await
+        .unwrap();
+
+    let filled = FilledBlock {
+        header: block.content.header,
+        operations: vec![],
+    };
+
+    tokio::spawn(async move {
+        tokio::time::sleep(Duration::from_millis(200)).await;
+        let _ = tx.send(filled).unwrap();
+    });
+
+    let result = tokio::time::timeout(Duration::from_secs(4), sub1.next())
+        .await
+        .unwrap();
+
+    assert!(result.is_some());
+    let value = result.unwrap().unwrap().clone();
+    assert_eq!(
+        value["header"]["id"].as_str().unwrap(),
+        &block.id.to_string()
+    );
 
     api_handle.stop().await;
 }
