45f0d8425a227655f4ea18b7eca0d81138372214
litchipi <litchipi@users.noreply.github.com>
2023-10-24 07:30:07 +0000

    Deploying to gh-pages from @ massalabs/massa@3f84f68bbed97d1b71e99f17307b03183a3dd6ed 🚀

--- massa_protocol_worker/sidebar-items.js
+++ massa_protocol_worker/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["create_protocol_controller","start_protocol_controller"],"mod":["connectivity","context","controller","handlers","ip","manager","messages","sig_verifier","worker","wrap_network","wrap_peer_db"]};
+window.SIDEBAR_ITEMS = {"fn":["create_protocol_controller","start_protocol_controller"],"mod":["connectivity","context","controller","handlers","ip","manager","messages","sig_verifier","worker","wrap_network"]};
