42f614294003a4ea91769ce919a79298faf6e1b7
Leo-Besancon <lb@massa.net>
2023-11-06 16:24:10 +0100

    Decrease timeouts, decrease message_size

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -210,9 +210,9 @@ pub const MAX_BOOTSTRAP_POS_CYCLES: u32 = 5;
 /// Max async pool changes
 pub const MAX_BOOTSTRAP_ASYNC_POOL_CHANGES: u64 = 100_000;
 /// Max bytes in final states parts
-pub const MAX_BOOTSTRAP_FINAL_STATE_PARTS_SIZE: u32 = 1_000_000_000;
+pub const MAX_BOOTSTRAP_FINAL_STATE_PARTS_SIZE: u32 = 100_000_000;
 /// Max bytes in final states parts
-pub const MAX_BOOTSTRAP_VERSIONING_ELEMENTS_SIZE: u32 = 100_000_000;
+pub const MAX_BOOTSTRAP_VERSIONING_ELEMENTS_SIZE: u32 = 10_000_000;
 /// Max size of the IP list
 pub const IP_LIST_MAX_SIZE: usize = 10000;
 /// Size of the random bytes array used for the bootstrap, safe to import
