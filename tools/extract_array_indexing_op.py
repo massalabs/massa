79b869b56a8ac5ea79ea1db5d94aef513c254c5e
sydhds <sydhds@gmail.com>
2023-11-27 16:07:32 +0100

    Add more exclude patterns

--- tools/extract_array_indexing_op.py
+++ tools/extract_array_indexing_op.py
@@ -8,9 +8,11 @@ def index_to_coordinates(s, index):
     sp = s[:index+1].splitlines(keepends=True)
     return len(sp), len(sp[-1])
 
+
 to_exclude = [
-    "[test]", # unit test
-    "[serial]", # unit test
+    "[test]",  # unit test
+    "[ignore]",
+    "[serial]",
     "[inline]", # optim :-D
     "[must_use]",
     "[dependencies]", # toml found in rust code ...
@@ -19,12 +21,23 @@ to_exclude = [
     "[from]", # thiserror syntax
     "[u8]", # u8 array type
     "[..]", # index all
-    "#[serde_as]" # Serde
+    "[serde_as]", # Serde
+    "[tokio::test]",  # async unit test
+    "[async_trait]",  # async trait crate
+    "[async_trait::async_trait]",  # async trait crate
+    "[macro_export]",  # macro definition
+    "[DeprecatedByNewRuntime]",  # in docstring
+    "[tonic::async_trait]",  # tonic re-export?
+    "[default]"  # Default trait
 ]
 
 if __name__ == "__main__":
 
     filepath = sys.argv[1]
+
+    if filepath.startswith("./tests"):
+        sys.exit(0)
+
     print(f"Opening file: {filepath}...")
     match_count = 0
     with open(filepath) as fp:
@@ -36,7 +49,6 @@ if __name__ == "__main__":
 
             line_number, col = index_to_coordinates(file_content, m.span()[0])
             print(f"Found match {m} at line {line_number}")
-
 
             match_count += 1
 
