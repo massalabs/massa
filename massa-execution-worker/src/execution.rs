779dd3fd2ba8b97c82877018962d0b1b26ac81bb
Leo-Besancon <lb@massa.net>
2023-12-01 10:19:10 +0100

    fmt

--- massa-execution-worker/src/execution.rs
+++ massa-execution-worker/src/execution.rs
@@ -628,7 +628,6 @@ impl ExecutionState {
                 true => panic!("You are being slashed at slot {} for double-staking using address {}. The node is stopping to prevent any further loss. Block header denunciation of block at slot {:?}. Denunciation's public key: {:?}", block_slot, addr_denounced, denunciation.get_slot(), denunciation.get_public_key()),
                 false => panic!("You are being slashed at slot {} for double-staking using address {}. The node is stopping to prevent any further loss. Endorsement denunciation of endorsement at slot {:?} and index {:?}. Denunciation's public key: {:?}", block_slot, addr_denounced, denunciation.get_slot(), denunciation.get_index(), denunciation.get_public_key())
             }
-            
         }
 
         Ok(())
