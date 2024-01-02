8c54549a79a92555bf1edc2bbd3265657ca14d0c
Leo-Besancon <lb@massa.net>
2024-01-02 16:04:28 +0100

    Update constants.rs

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -51,7 +51,7 @@ lazy_static::lazy_static! {
             )
         )
     } else {
-        MassaTime::from_millis(1702639800000) // Friday, December 15, 2023 11:30:00 UTC
+        MassaTime::from_millis(1704208200000) // Friday, December 15, 2023 11:30:00 UTC
     };
 
     /// TESTNET: time when the blockclique is ended.
