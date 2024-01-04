db06601f2d4714fe58a15c03b26eaa37f1749903
Leo-Besancon <lb@massa.net>
2024-01-04 12:34:36 +0100

    Update build.rs

--- massa-node/build.rs
+++ massa-node/build.rs
@@ -11,12 +11,15 @@ fn main() {
     let cargo_dir = &Path::new(&manifest_dir);
     let target_initial_setup_path = cargo_dir.join("genesis-ledger").join("node_initial_setup");
     let node_config_path = cargo_dir.join("base_config");
+    let cargo_dir_abs = cargo_dir.canonicalize().expect("Failed to get Cargo dir");
+    let workspace_dir = cargo_dir_abs.parent().expect("Failed to get workspace dir");
     
     if !target_initial_setup_path.exists() {
+        env::set_current_dir(&workspace_dir).expect("cd failed");
         Command::new("git")
-                .arg("submodule update --init")
-                .output()
-                .expect("git submodule update failed");
+            .args(["submodule", "update", "--init"])
+            .output()
+            .expect("git submodule update failed");
     }
 
     if !target_initial_setup_path.exists() {
