4de02b4ad94694e475f17fccf455fc691f9a882e
sydhds <sydhds@users.noreply.github.com>
2024-01-11 16:09:41 +0000

    Deploying to gh-pages from @ massalabs/massa@391a99efe34cc135a080d90ccd1d3b4c7bcf5fad 🚀

---
+++ implementors/zeroize/trait.Zeroize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_wallet":[["impl Zeroize for <a class=\"struct\" href=\"massa_wallet/struct.Wallet.html\" title=\"struct massa_wallet::Wallet\">Wallet</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
