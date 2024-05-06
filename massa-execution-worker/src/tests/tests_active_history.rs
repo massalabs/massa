dea9fdbe372eb59527f7634cc73b5d0a6303216e
sydhds <sydhds@gmail.com>
2024-05-06 15:15:22 +0200

    Cargo clippy fixes

--- massa-execution-worker/src/tests/tests_active_history.rs
+++ massa-execution-worker/src/tests/tests_active_history.rs
@@ -58,6 +58,9 @@ fn test_active_history_deferred_credits() {
         slot_trace: Default::default(),
         #[cfg(feature = "dump-block")]
         storage: None,
+        deferred_credits_execution: Default::default(),
+        cancel_async_message_execution: Default::default(),
+        auto_sell_execution: Default::default(),
     };
 
     let active_history = ActiveHistory(VecDeque::from([exec_output_1]));
