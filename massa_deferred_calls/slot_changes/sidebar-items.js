86c268256cb7fa90a16a26b46138838aaaad677c
modship <modship@users.noreply.github.com>
2024-09-27 12:35:23 +0000

    Deploying to gh-pages from @ massalabs/massa@78f3f9e2b387fb99c57e714efa2b30b2ecb8811a 🚀

---
+++ massa_deferred_calls/slot_changes/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["DeferredRegistrySlotChanges","DeferredRegistrySlotChangesDeserializer","DeferredRegistrySlotChangesSerializer"]};
--- type.impl/std/primitive.array.js
+++
@@ -1,3 +0,0 @@
-(function() {var type_impls = {
-"massa_cipher":[]
-};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
