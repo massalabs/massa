a60f278bf43de3d80b55783c2853f1a27edd44b6
sydhds <sydhds@users.noreply.github.com>
2024-09-27 15:14:01 +0000

    Deploying to gh-pages from @ massalabs/massa@ac8a3d8ae70e805b2e8ead990f3ff9916697fdf9 🚀

--- massa_deferred_calls/slot_changes/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["DeferredRegistrySlotChanges","DeferredRegistrySlotChangesDeserializer","DeferredRegistrySlotChangesSerializer"]};
---
+++ trait.impl/nom/error/trait.ParseError.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_serialization":[["impl&lt;'a&gt; ParseError&lt;&amp;'a [<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.81.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"massa_serialization/struct.DeserializeError.html\" title=\"struct massa_serialization::DeserializeError\">DeserializeError</a>&lt;'a&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
