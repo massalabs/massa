09ce4a25ca1b26b29b540491221119b509271356
sydhds <sydhds@users.noreply.github.com>
2024-09-30 14:08:49 +0000

    Deploying to gh-pages from @ massalabs/massa@f383d6e3bb7b888e3de9a20ec5950fce2b807056 🚀

---
+++ search.desc/massa_xtask/massa_xtask-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("massa_xtask", 0, "to use it task: cargo xtask &lt;task_name&gt; example: cargo …\ncheck the version of the package in the Cargo.toml file\ncheck the version of the packages in the workspace")
