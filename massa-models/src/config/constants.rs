a9321dbb8e8b4d9cef18032e7a2878f3d9bab61b
Léo Besançon <lb@massa.net>
2024-10-21 16:32:33 +0200

    prepare labnet

--- massa-models/src/config/constants.rs
+++ massa-models/src/config/constants.rs
@@ -41,42 +41,17 @@ lazy_static::lazy_static! {
     /// Time in milliseconds when the blockclique started.
     /// In sandbox mode, the value depends on starting time and on the --restart-from-snapshot-at-period argument in CLI,
     /// so that the network starts or restarts 10 seconds after launch
-    pub static ref GENESIS_TIMESTAMP: MassaTime = if cfg!(feature = "sandbox") {
-        std::env::var("GENESIS_TIMESTAMP").map(|timestamp| MassaTime::from_millis(timestamp.parse::<u64>().unwrap())).unwrap_or_else(|_|
-            MassaTime::now()
-                .saturating_sub(
-                    T0.checked_mul(get_period_from_args()).unwrap()
-                )
-                .saturating_add(MassaTime::from_millis(1000 * 10)
-            )
-        )
-    } else {
-        MassaTime::from_millis(1705312800000) // Monday, January 15, 2024 10:00:00 AM UTC
-    };
+    pub static ref GENESIS_TIMESTAMP: MassaTime = MassaTime::from_millis(1729521273764);
 
     /// TESTNET: time when the blockclique is ended.
-    pub static ref END_TIMESTAMP: Option<MassaTime> =
-    #[allow(clippy::if_same_then_else)]
-    if cfg!(feature = "sandbox") {
-        None
-    } else {
-        None
-    };
+    pub static ref END_TIMESTAMP: Option<MassaTime> = None;
     /// `KeyPair` to sign genesis blocks.
     pub static ref GENESIS_KEY: KeyPair = KeyPair::from_str("S1UxdCJv5ckDK8z87E5Jq5fEfSVLi2cTHgtpfZy7iURs3KpPns8")
         .unwrap();
     /// number of cycle misses (strictly) above which stakers are deactivated
     pub static ref POS_MISS_RATE_DEACTIVATION_THRESHOLD: Ratio<u64> = Ratio::new(7, 10);
     /// node version
-    pub static ref VERSION: Version = {
-        if cfg!(feature = "sandbox") {
-            "SAND.2.3"
-        } else {
-            "MAIN.2.3"
-        }
-        .parse()
-        .unwrap()
-    };
+    pub static ref VERSION: Version = "LABN.7.7".parse().unwrap();
     /// node chain id (to avoid replay attacks)
     pub static ref CHAINID: u64 = {
         // MASM (MainNet):           77658377
