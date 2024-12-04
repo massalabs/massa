0656238d1f89c9412a814a0af65b40b215d91cb8
modship <modship@users.noreply.github.com>
2024-12-04 09:06:19 +0000

    Deploying to gh-pages from @ massalabs/massa@9bc359011f9e5377e6ba89f5fbcf595f74626f44 🚀

---
+++ massa_deferred_calls/call/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["DeferredCall","DeferredCallDeserializer","DeferredCallSerializer"]};
--- massa_ledger_exports/error/sidebar-items.js
+++ massa_ledger_exports/error/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["_DERIVE_Display_FOR_LedgerError"],"enum":["LedgerError"]};
+window.SIDEBAR_ITEMS = {"enum":["LedgerError"]};
