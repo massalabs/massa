6102f0edeb4989a2fe05b10aed4d483cf5c0ea21
modship <yeskinokay@gmail.com>
2024-12-04 11:07:13 +0100

    Test: Fix test_fetch_message (deferred_calls merge)

--- massa-execution-worker/src/active_history.rs
+++ massa-execution-worker/src/active_history.rs
@@ -443,6 +443,7 @@ mod test {
             executed_ops_changes: Default::default(),
             executed_denunciations_changes: Default::default(),
             execution_trail_hash_change: Default::default(),
+            deferred_call_changes: Default::default(),
         };
         let state_changes_2 = StateChanges {
             ledger_changes: Default::default(),
@@ -451,6 +452,7 @@ mod test {
             executed_ops_changes: Default::default(),
             executed_denunciations_changes: Default::default(),
             execution_trail_hash_change: Default::default(),
+            deferred_call_changes: Default::default(),
         };
 
         let exec_output_1 = ExecutionOutput {
