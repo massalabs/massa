cd9e713526ec99338e283add535533ad606c7fae
Leo-Besancon <lb@massa.net>
2025-01-15 12:15:05 +0100

    Fix features for massa-sc-runtime

--- massa-xtask/src/main.rs
+++ massa-xtask/src/main.rs
@@ -1,6 +1,8 @@
+#[cfg(feature = "gas_calibration")]
 mod check_gas_cost_definitions;
 mod update_package_versions;
 
+#[cfg(feature = "gas_calibration")]
 use crate::check_gas_cost_definitions::check_gas_cost_definitions;
 use crate::update_package_versions::update_package_versions;
 use std::env;
@@ -13,6 +15,7 @@ fn main() {
     match task.as_deref() {
         // We can add more tasks here
         Some("update_package_versions") => update_package_versions(),
+        #[cfg(feature = "gas_calibration")]
         Some("check_gas_cost_definitions") => check_gas_cost_definitions().unwrap(),
         _ => panic!("Unknown task"),
     }
