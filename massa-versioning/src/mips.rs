c1c20ff890ae1d85454f14ae15d09fa2144479f4
Leo-Besancon <lb@massa.net>
2025-03-19 16:12:58 +0100

    cargo clippy fixes

--- massa-versioning/src/mips.rs
+++ massa-versioning/src/mips.rs
@@ -18,12 +18,12 @@ pub fn get_mip_list() -> [(MipInfo, MipState); 1] {
                 (MipComponent::FinalState, 1),
             ]),
             // Note: All bootstrap servers should have been updated to the latest version before the start time
-            start: MassaTime::from_utc_ymd_hms(2025, 01, 13, 14, 0, 0).unwrap(), // Monday, January 13, 2025 2:00:00 PM UTC
+            start: MassaTime::from_utc_ymd_hms(2025, 1, 13, 14, 0, 0).unwrap(), // Monday, January 13, 2025 2:00:00 PM UTC
             // Give 1 week for the MIP to be accepted
-            timeout: MassaTime::from_utc_ymd_hms(2025, 01, 20, 14, 0, 0).unwrap(), // Monday, January 20, 2025 2:00:00 PM UTC
-            activation_delay: MassaTime::from_millis(1 * 60 * 60 * 1000), // The MIP will be activated 1 hour after the LockedIn state
+            timeout: MassaTime::from_utc_ymd_hms(2025, 1, 20, 14, 0, 0).unwrap(), // Monday, January 20, 2025 2:00:00 PM UTC
+            activation_delay: MassaTime::from_millis(60 * 60 * 1000), // The MIP will be activated 1 hour after the LockedIn state
         },
-        MipState::new(MassaTime::from_utc_ymd_hms(2025, 01, 10, 10, 0, 0).unwrap()), // Friday, January 10, 2025 10:00:00 AM UTC
+        MipState::new(MassaTime::from_utc_ymd_hms(2025, 1, 10, 10, 0, 0).unwrap()), // Friday, January 10, 2025 10:00:00 AM UTC
     )];
 
     // debug!("MIP list: {:?}", mip_list);
