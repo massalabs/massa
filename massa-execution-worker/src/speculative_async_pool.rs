c1b55714924102a0410e3e63672ff420e29f485f
Leo-Besancon <lb@massa.net>
2025-03-27 08:21:39 +0100

    Revert "add comment"
    This reverts commit 4f00cc508041d8ec5307a9a9e233e03126e73b65.

--- massa-execution-worker/src/speculative_async_pool.rs
+++ massa-execution-worker/src/speculative_async_pool.rs
@@ -342,8 +342,6 @@ impl SpeculativeAsyncPool {
         msgs
     }
 
-    /// In this version, we need to ensure the order of the messages is preserved when we fetch them
-    /// (to avoid a non-deterministic behavior in the execution).
     fn fetch_msgs_v1(
         &mut self,
         wanted_ids: Vec<&AsyncMessageId>,
