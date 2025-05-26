448398d9cf049a14e5c6d94cd4b16e561851c8bd
Leo-Besancon <lb@massa.net>
2025-05-26 15:51:05 +0200

    Update grpc new_slot_abi_call_stacks

--- massa-grpc/src/stream/new_slot_abi_call_stacks.rs
+++ massa-grpc/src/stream/new_slot_abi_call_stacks.rs
@@ -13,7 +13,7 @@ use crate::public::into_element;
 #[cfg(not(feature = "execution-trace"))]
 use massa_models::slot::Slot;
 #[cfg(feature = "execution-trace")]
-use massa_proto_rs::massa::api::v1::{AscabiCallStack, OperationAbiCallStack};
+use massa_proto_rs::massa::api::v1::{AscabiCallStack, OperationAbiCallStack, DeferredCallAbiCallStack};
 #[cfg(not(feature = "execution-trace"))]
 #[derive(Clone)]
 struct SlotAbiCallStack {
@@ -92,6 +92,15 @@ pub(crate) async fn new_slot_abi_call_stacks(
                                         }
                                     )
                                 }
+                                for (deferred_call_id, deferred_call_call_stack) in massa_slot_execution_trace.deferred_call_stacks.iter() {
+                                    ret.deferred_call_stacks.push(
+                                        DeferredCallAbiCallStack {
+                                            deferred_call_id: deferred_call_id.to_string(),
+                                            call_stack: deferred_call_call_stack.iter().map(into_element).collect()
+                                        }
+                                    )
+                                }
+                                
                             }
 
                             if let Err(e) = tx.send(Ok(ret)).await {
