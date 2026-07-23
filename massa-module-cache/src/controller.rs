413b3bb962d91011c5fa6e2161377eb1f810651a
Peterjah <ps@massa.net>
2026-07-23 11:27:03 +0200

    filter out readonly execute from cache-import block

--- massa-module-cache/src/controller.rs
+++ massa-module-cache/src/controller.rs
@@ -217,4 +217,9 @@ impl ModuleCache {
     pub fn get_module_lru_cache_memory_usage(&self) -> usize {
         self.lru_cache.cache.memory_usage()
     }
+
+    /// Returns the number of modules currently held in the in-RAM LRU cache
+    pub fn lru_cache_len(&self) -> usize {
+        self.lru_cache.cache.len()
+    }
 }
