88582cb149a999494d633db6356c224a13902886
peterjah <peterjah@users.noreply.github.com>
2026-08-20 09:23:28 +0000

    Deploying to gh-pages from @ massalabs/massa@413a9ce777b9ed0e775961b3783adf64b4230239 🚀

--- massa_protocol_worker/handlers/operation_handler/retrieval/sidebar-items.js
+++ massa_protocol_worker/handlers/operation_handler/retrieval/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["THREAD_NAME"],"enum":["PeerAskState"],"fn":["note_operations_from_peer","start_retrieval_thread"],"struct":["AskedOperation","OperationBatchItem","RetrievalThread"]};
+window.SIDEBAR_ITEMS = {"constant":["THREAD_NAME"],"fn":["note_operations_from_peer","start_retrieval_thread"],"struct":["OperationBatchItem","RetrievalThread"]};
