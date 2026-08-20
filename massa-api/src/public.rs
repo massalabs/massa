4286b531d80bcbd543a3b0e6af779a4671a5dea1
Peterjah <ps@massa.net>
2026-08-20 11:30:23 +0200

    [F42] propagate the configured datastore key query cap from RPC/gRPC parsing to the scan

--- massa-api/src/public.rs
+++ massa-api/src/public.rs
@@ -1629,7 +1629,7 @@ fn get_address_datastore_keys_to_state_query_item(
         (Some(k), false) => std::ops::Bound::Excluded(k),
     };
 
-    let (prefix, start_key, end_key) = cleanup_datastore_key_range_query(
+    let (prefix, start_key, end_key, count) = cleanup_datastore_key_range_query(
         &value.prefix,
         start_key,
         end_key,
@@ -1650,7 +1650,7 @@ fn get_address_datastore_keys_to_state_query_item(
             prefix,
             start_key,
             end_key,
-            count: value.count,
+            count,
         })
     } else {
         Ok(ExecutionQueryRequestItem::AddressDatastoreKeysCandidate {
@@ -1658,7 +1658,7 @@ fn get_address_datastore_keys_to_state_query_item(
             prefix,
             start_key,
             end_key,
-            count: value.count,
+            count,
         })
     }
 }
