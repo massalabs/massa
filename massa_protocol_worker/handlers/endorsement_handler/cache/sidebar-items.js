b93d4d30ff4cfc443e201d1e62a56591dbd47367
peterjah <peterjah@users.noreply.github.com>
2026-08-24 14:21:10 +0000

    Deploying to gh-pages from @ massalabs/massa@56f8fc7e89e28152a723ad26ca1e86c59d037393 🚀

--- massa_protocol_worker/handlers/endorsement_handler/cache/sidebar-items.js
+++ massa_protocol_worker/handlers/endorsement_handler/cache/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["MAX_ENDORSEMENTS_PER_DRAW"],"struct":["EndorsementCache"],"type":["SharedEndorsementCache"]};
+window.SIDEBAR_ITEMS = {"struct":["EndorsementCache"],"type":["SharedEndorsementCache"]};
