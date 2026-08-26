ae6ab0123a10f7973ccf8dfa0d31e3eceab9823c
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2026-08-26 11:05:22 +0000

    Deploying to gh-pages from @ massalabs/massa@84bb172f59fb41bb3e0a65985cd46d544b15d850 🚀

--- massa_protocol_worker/handlers/peer_handler/sidebar-items.js
+++ massa_protocol_worker/handlers/peer_handler/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["THREAD_NAME"],"fn":["filter_peers_listeners"],"mod":["announcement","messages","models","tester"],"struct":["MassaHandshake","PeerManagementHandler"]};
+window.SIDEBAR_ITEMS = {"constant":["THREAD_NAME"],"mod":["announcement","messages","models","tester"],"struct":["MassaHandshake","PeerManagementHandler"]};
