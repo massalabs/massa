e46ee00e900a3d05e2d61f5c3936e8817609ee9e
Leo-Besancon <leoleo38@live.fr>
2026-09-04 10:25:15 +0200

    fix build after rebase

--- massa-deferred-calls/src/tests/mod.rs
+++ massa-deferred-calls/src/tests/mod.rs
@@ -148,6 +148,8 @@ fn uninitialized_slot_base_fee_uses_min_gas_cost() {
         max_history_length: 100,
         max_final_state_elements_size: 100,
         max_versioning_elements_size: 100,
+        max_final_state_elements_count: MAX_BOOTSTRAP_FINAL_STATE_ELEMENTS_COUNT as usize,
+        max_versioning_elements_count: MAX_BOOTSTRAP_VERSIONING_ELEMENTS_COUNT as usize,
         thread_count: THREAD_COUNT,
         max_ledger_backups: 100,
         enable_metrics: false,
--- massa-execution-worker/src/speculative_deferred_calls.rs
+++ massa-execution-worker/src/speculative_deferred_calls.rs
@@ -673,6 +673,8 @@ mod tests {
             max_history_length: 10,
             max_final_state_elements_size: 100_000,
             max_versioning_elements_size: 100_000,
+            max_final_state_elements_count: MAX_BOOTSTRAP_FINAL_STATE_ELEMENTS_COUNT as usize,
+            max_versioning_elements_count: MAX_BOOTSTRAP_VERSIONING_ELEMENTS_COUNT as usize,
             thread_count: THREAD_COUNT,
             max_ledger_backups: 10,
             enable_metrics: false,
