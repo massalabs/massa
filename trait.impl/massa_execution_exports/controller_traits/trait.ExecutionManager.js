f15a37a871892c8c87e3845dd575faa0060b8c7d
peterjah <peterjah@users.noreply.github.com>
2026-09-10 14:21:36 +0000

    Deploying to gh-pages from @ massalabs/massa@288e9c8e41dc7a476d3b5f8a1535fd3f71d50056 🚀

--- trait.impl/massa_execution_exports/controller_traits/trait.ExecutionManager.js
+++ trait.impl/massa_execution_exports/controller_traits/trait.ExecutionManager.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_execution_worker":[["impl ExecutionManager for <a class=\"struct\" href=\"massa_execution_worker/controller/struct.ExecutionManagerImpl.html\" title=\"struct massa_execution_worker::controller::ExecutionManagerImpl\">ExecutionManagerImpl</a>"]]
+"massa_execution_worker":[["impl <a class=\"trait\" href=\"massa_execution_exports/controller_traits/trait.ExecutionManager.html\" title=\"trait massa_execution_exports::controller_traits::ExecutionManager\">ExecutionManager</a> for <a class=\"struct\" href=\"massa_execution_worker/controller/struct.ExecutionManagerImpl.html\" title=\"struct massa_execution_worker::controller::ExecutionManagerImpl\">ExecutionManagerImpl</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
