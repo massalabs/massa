cbf36877753c2435420d11e7e2e776594c9e0c41
bilboquet <bilboquet@users.noreply.github.com>
2026-09-15 15:39:54 +0000

    Deploying to gh-pages from @ massalabs/massa@c4577f563b1ef471e532d164e95681e3d65d4e28 🚀

--- trait.impl/massa_execution_exports/controller_traits/trait.ExecutionController.js
+++ trait.impl/massa_execution_exports/controller_traits/trait.ExecutionController.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_execution_worker":[["impl ExecutionController for <a class=\"struct\" href=\"massa_execution_worker/controller/struct.ExecutionControllerImpl.html\" title=\"struct massa_execution_worker::controller::ExecutionControllerImpl\">ExecutionControllerImpl</a>"]]
+"massa_execution_worker":[["impl <a class=\"trait\" href=\"massa_execution_exports/controller_traits/trait.ExecutionController.html\" title=\"trait massa_execution_exports::controller_traits::ExecutionController\">ExecutionController</a> for <a class=\"struct\" href=\"massa_execution_worker/controller/struct.ExecutionControllerImpl.html\" title=\"struct massa_execution_worker::controller::ExecutionControllerImpl\">ExecutionControllerImpl</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
