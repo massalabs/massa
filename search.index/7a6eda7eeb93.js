ba5c6411e7182098d0a1bd451c86058546bd9923
bilboquet <bilboquet@users.noreply.github.com>
2026-09-16 15:49:12 +0000

    Deploying to gh-pages from @ massalabs/massa@3243c072ebe5540bc8ef4eb09cc0b5164b914102 🚀

---
+++ search.index/7a6eda7eeb93.js
@@ -0,0 +1,1 @@
+rn_("tQBCAABVQVakVqVWplanVqhWbJY/lz+YP5k/mj+bP5w/nT+eP58/FVMWU0FDAABnWnhaeVp6Wnta+wJpchsCoNAAAFfccng=")
--- trait.impl/massa_execution_exports/controller_traits/trait.ExecutionController.js
+++ trait.impl/massa_execution_exports/controller_traits/trait.ExecutionController.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["massa_execution_worker",[["impl ExecutionController for <a class=\"struct\" href=\"massa_execution_worker/controller/struct.ExecutionControllerImpl.html\" title=\"struct massa_execution_worker::controller::ExecutionControllerImpl\">ExecutionControllerImpl</a>"]]]]);
+    var implementors = Object.fromEntries([["massa_execution_worker",[["impl <a class=\"trait\" href=\"massa_execution_exports/controller_traits/trait.ExecutionController.html\" title=\"trait massa_execution_exports::controller_traits::ExecutionController\">ExecutionController</a> for <a class=\"struct\" href=\"massa_execution_worker/controller/struct.ExecutionControllerImpl.html\" title=\"struct massa_execution_worker::controller::ExecutionControllerImpl\">ExecutionControllerImpl</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[265]}
+//{"start":57,"fragment_lengths":[450]}
