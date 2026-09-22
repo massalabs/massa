4ea275eee4497c21840c7d25b1f3c464dc4c56e6
bilboquet <bilboquet@users.noreply.github.com>
2026-09-22 05:08:54 +0000

    Deploying to gh-pages from @ massalabs/massa@c2da8675d22f557d2f18df3d4fdf26db01297fe1 🚀

--- trait.impl/massa_pos_exports/controller_traits/trait.SelectorManager.js
+++ trait.impl/massa_pos_exports/controller_traits/trait.SelectorManager.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["massa_pos_worker",[["impl <a class=\"trait\" href=\"massa_pos_exports/controller_traits/trait.SelectorManager.html\" title=\"trait massa_pos_exports::controller_traits::SelectorManager\">SelectorManager</a> for <a class=\"struct\" href=\"massa_pos_worker/controller/struct.SelectorManagerImpl.html\" title=\"struct massa_pos_worker::controller::SelectorManagerImpl\">SelectorManagerImpl</a>"]]]]);
+    var implementors = Object.fromEntries([["massa_pos_worker",[["impl SelectorManager for <a class=\"struct\" href=\"massa_pos_worker/controller/struct.SelectorManagerImpl.html\" title=\"struct massa_pos_worker::controller::SelectorManagerImpl\">SelectorManagerImpl</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[396]}
+//{"start":57,"fragment_lengths":[231]}
